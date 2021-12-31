3d4e324bf5eb4cf487ab3f2fec084c7a9d731d61
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-12-31 13:12:19 +0530

    Time: 8178 ms (5.05%), Space: 14.8 MB (16.07%) - LeetHub

---
+++ substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,43 @@
+class Solution:
+    
+    def freq(self, a):
+        d = {}
+        for i in a:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        return d
+        
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        
+        f = self.freq(words)
+        words.sort()
+        lwords = [len(i) for i in words]
+        tlen = sum(lwords)
+        lb = min(lwords)
+        ub = max(lwords)
+        ans = []
+        for i in range(len(s)):
+            b = s[i:i+tlen]
+            idx = 0
+            check = []
+            while idx <= tlen:  
+                Flag = False
+                #print(idx)
+                for j in range(lb, ub + 1):
+                    c = b[idx:idx + j]
+                    if c in words:
+                        check.append(c)
+                        idx += j
+                        Flag = True
+                        break
+                if not Flag:
+                    break
+            if sorted(check) == words and self.freq(check) == f:
+                ans.append(i)
+        return ans
+                
+               
+        
+        
