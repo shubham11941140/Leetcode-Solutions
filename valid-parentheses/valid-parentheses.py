42445bbb47e64fd326a6f31bc937ac1aacb65cfe
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-02 10:27:00 +0530

    Time: 40 ms (20.99%), Space: 14.4 MB (7.65%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        while s:
+            t = s
+            s = s.replace('()', '')
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+            if s == t:
+                break
+        
+        return s == ''
+        
