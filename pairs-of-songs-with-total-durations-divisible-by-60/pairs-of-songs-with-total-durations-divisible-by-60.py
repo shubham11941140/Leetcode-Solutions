351a74c2b60d15a2b8cb29cb41ec0d7c2e5608c8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-02 10:17:00 +0530

    Time: 314 ms (15.84%), Space: 17.2 MB (98.04%) - LeetHub

---
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,35 @@
+class Solution:
+    
+    def freq(self, a):
+        d = {}
+        for i in a:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        return d
+    
+    def con(self, a):
+        return (a * (a - 1)) // 2
+    
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        n = len(time)
+        for i in range(n):
+            time[i] %= 60
+        
+        d = self.freq(time)
+        ans = 0  
+        for i in range(61):
+            if i not in d:
+                d[i] = 0
+        
+        ans = 0
+        for i in range(1, 30):
+            rem = 60 - i
+            val = d[i] * d[rem]
+            ans += val
+        ans += (self.con(d[0]) + self.con(d[30]))        
+        return ans
+            
+                    
+        
