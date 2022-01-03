ba1a80221cd82e87c86da3c915975fa5e44681b9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-03 09:25:33 +0530

    Time: 1513 ms (5.00%), Space: 72.2 MB (10.08%) - LeetHub

---
+++ first-missing-positive/first-missing-positive.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def remneg(self, a):
+        return [i for i in a if i > 0]
+    
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = self.remneg(nums)
+        if not nums:
+            return 1
+        m = min(nums)
+        if m >= 2:
+            return 1
+        else:
+            f = sorted(list(set(nums)))
+            l = len(f)
+            i = 0
+            #print(f)
+            while i + 1 == f[i]:
+                i += 1
+                if i == l:
+                    return l + 1
+            return i + 1
+                    
+        
