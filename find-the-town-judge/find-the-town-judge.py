5130fd4cdcdeef7bbcd121761a7936248021c5bb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-03 09:13:21 +0530

    Time: 704 ms (96.64%), Space: 19 MB (65.18%) - LeetHub

--- find-the-town-judge/find-the-town-judge.py
+++ find-the-town-judge/find-the-town-judge.py
@@ -3,19 +3,9 @@ class Solution:
         adj = [[] for i in range(n)]
         flag = False
         for i in trust:
-            u = i[0] - 1
-            v = i[1] - 1
-            #adj[u].append(v)
-            adj[v].append(u)
-        for i in range(n):
-            t = adj[i]
-            if len(t) == n - 1:
-                c = 0
-                for j in range(n):
-                    if i != j:
-                        if i not in adj[j]:
-                            c += 1
-                if c == n - 1:
+            adj[i[1] - 1].append(i[0] - 1)
+        for i in range(n): 
+            if len(adj[i]) == n - 1 and len([j for j in range(n) if i != j and i not in adj[j]]) == n - 1:
                     flag = True
                     ans = i + 1
         return ans if flag else -1
