411301f04f94f185ed9e16c08249c178c3a4e9fe
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-04 14:08:43 +0530

    Time: 101 ms (29.30%), Space: 14.1 MB (92.40%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.py
@@ -0,0 +1,8 @@
+from math import log
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return 3 ** round(log(n, 3)) == n
+        
+        
