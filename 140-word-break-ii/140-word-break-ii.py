0fd56004c1369334ab89e3c06e06badb38910203
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-08 17:22:43 +0530

    Time: 439 ms (5.06%), Space: 25.6 MB (8.54%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    
+    def space(self, s, idx, a):
+        if idx == len(s):
+            a.append(s)
+            return
+        self.space(s, idx + 1, a)
+        self.space(s[:idx] + " " + s[idx:], idx + 2, a)
+            
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        a = []
+        self.space(s, 0, a)
+        ans = []
+        for i in a:
+            k = i.split(' ')
+            l = len(k)
+            for j in k:
+                if j in wordDict:
+                    l -= 1
+            if not l:
+                ans.append(i)
+        return ans
+                    
+        
+        
