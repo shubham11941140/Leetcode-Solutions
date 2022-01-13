bd65d0156ef90c0918549b1c84ba0370abb561a5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-13 11:56:30 +0530

    Time: 67 ms (5.77%), Space: 14.3 MB (78.35%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -19,60 +19,19 @@ class Solution:
     def flip(self, root1, r):
         if not root1:
             return
-        #if root1 and not r:            r = TreeNode()
-        #r = TreeNode()
         r.val = root1.val
-        print(24, r.val)
         if root1.right and not r.left:
             r.left = TreeNode()
             self.flip(root1.right, r.left)
         if root1.left and not r.right:
             r.right = TreeNode()
             self.flip(root1.left, r.right)
-
-    
-    def tra(self, root):
-        if not root:
-            return
-        self.tra(root.left)
-        print(root.val)
-        self.tra(root.right)
         
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        # Flip tree and check same
-        # Recursively
-        self.tra(root)
         r = TreeNode()
-        print(r.val)
-        self.flip(root, r)
-        print(43)
-        self.tra(r)
-        
+        self.flip(root, r)        
         return self.same(root, r)
-    
-        adj = [[] for i in range(2000)]
-        q = []
-        q.append((root, 0))
-        while q:
-            root, level = q.pop(0)
-            if root is None:
-                root = TreeNode(-200)
-            adj[level].append(root.val)
-            q.append((root.left, level + 1))
-            q.append((root.right, level + 1))
-            #print(level, adj[level])
-            if adj[level - 1] != adj[level - 1][::-1]:
-                return False
-            if len(set(adj[level - 1])) == 1 and adj[level - 1][0] == -200:
-                break
-                
-        for arr in adj:
-            if not arr:
-                break
-            elif arr != arr[::-1]:
-                return False
-        return True
+
                 
             
 
