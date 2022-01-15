d1ca25bee9b81602f05c4d8535ad2bdadfd061cd
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-15 12:51:12 +0530

    Time: 299 ms (25.87%), Space: 14.6 MB (11.27%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    def compute(self, nums, n, target):
+        ans = 10 ** 10
+        final = 10 ** 10
+        for i in range(n):
+            val = target - nums[i]
+            left = i + 1
+            right = n - 1  
+            while left < right:
+                t = nums[left] + nums[right]
+                print(nums[i] + t)
+                if t == val:
+                    return target
+                elif t < val:
+                    #ans = max(ans, nums[i] + t)
+                    left += 1
+                elif t > val:     
+                    #ans = min(ans, nums[i] + t)
+                    right -= 1
+                if abs(t-val) < ans:
+                    final = nums[i] + t
+                    ans = abs(t-val)
+                    print(final, ans)
+        return final
+    
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        return self.compute(nums, len(nums), target)
+        
