d3e2310e48b1ddec07c8d2662436d5fa4bc6d350
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-15 12:37:59 +0530

    Time: 9963 ms (5.01%), Space: 320.1 MB (5.01%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,33 @@
+class Solution:
+        
+    def compute(self, nums, n, ans):
+        for i in range(n):
+            val = -(nums[i])
+            left = i + 1
+            right = n - 1  
+            while left < right:
+                t = nums[left] + nums[right]
+                if t == val:
+                    if left != i and right != i:
+                        b = sorted([nums[i], nums[left], nums[right]])
+                        ans.append(b)
+                    left += 1
+                    right -= 1
+                elif t < val:
+                    left += 1
+                elif t > val:
+                    right -= 1
+        return ans
+        
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  
+        ans = []
+        n = len(nums)
+        t = self.compute(nums, n, ans)
+        r = []
+        for i in t:
+            if i not in r:
+                r.append(i)
+        return r           
+                        
+        
