47b49d61d864412a0df218a381e33d015e51be58
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-19 19:53:43 +0530

    Time: 400 ms (5.08%), Space: 14.3 MB (48.79%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    
+    def rep(self, n):
+        return sum([pow(int(i), 2) for i in str(n)])
+    
+    def isHappy(self, n: int) -> bool:
+        for _ in range(1000):
+            if n == 1:
+                return True
+            n = self.rep(n)
+        return False
+        
+        
