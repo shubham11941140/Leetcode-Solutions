a6976a018dd40d758763b8ab2e4cb2f601059673
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-20 11:26:49 +0530

    Time: 1090 ms (5.03%), Space: 15.6 MB (60.66%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -8,20 +8,13 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left = 0
         right = 10 * (sum(piles) + 1)
-        k = 0
         while left <= right:
-            k += 1
-            if k == 10000:
-                break
             mid = (left + right) // 2            
             calc = self.satisfy(piles, mid)
-            print(calc, h, "MID", mid)
             if calc <= h:
-                print(20)
                 if mid > 1 and self.satisfy(piles, mid - 1) <= h:
                     right = mid
                 else:
-                    print("HERE")
                     return mid
             elif calc > h:
                 left = mid + 1
