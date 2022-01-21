4bb33736aa813276453cc84649b3db85f8ba6757
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-21 12:21:16 +0530

    Time: 9062 ms (14.06%), Space: 15.4 MB (22.81%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,23 +1,14 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        print(n)
-        # DP solution
         dp = [0 for i in range(n)]
-        m = max(nums)
-        print(m)
-        
+        m = max(nums)       
         for i in range(1, n):
-            #for j in range(i): 
-            # Put max condition optimization
             if i >= m + 10:
                 check = [dp[j] for j in range(i - m - 10, i) if (i - j) <= nums[j]]
             else:
                 check = [dp[j] for j in range(i) if (i - j) <= nums[j]]
-            #print(check)
-            #if not check:                break
-            dp[i] = 1 + min(check)
-        
+            dp[i] = 1 + min(check)        
         return dp[n - 1]
 
         
