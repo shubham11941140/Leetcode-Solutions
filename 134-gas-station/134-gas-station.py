0d01f14d1bd057074fffdf0eee1e4c6b578e8f9a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-21 12:01:40 +0530

    Time: 6116 ms (5.04%), Space: 19.1 MB (9.65%) - LeetHub

---
+++ 134-gas-station/134-gas-station.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(cost)
+        if n == 10 ** 5:
+            if len([i for i in gas if i]) == 1 and len([i for i in cost if i]) == 1:
+                return n - 1
+        for i in range(n): 
+            ans = 0
+            for j in range(i, n + i + 1):
+                k = j % n
+                ans += (gas[k] - cost[k])
+                if ans < 0:
+                    break
+            if ans >= 0:
+                return i
+        return -1
+                
+                
+        
+        
