9c5a280ea2804962f400d77cd810b00dc5f2b2f7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-22 12:34:57 +0530

    Time: 276 ms (5.20%), Space: 21.9 MB (7.24%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -4,39 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    
-    
-    # 2 DFS to solve this
-    # FIND E1
-    def dfs1(self, root, curmax, e1):        
-        rl = self.dfs1(root.left, curmax, e1) if root.left else -5000
-        rr = self.dfs1(root.right, curmax, e1) if root.right else -5000
-        print("RRRL", rl, rr)
-        if rl == -5000 and rr == -5000:
-            cursum = root.val
-        else:
-            cursum = root.val + max(rl, rr)
-            print("CURSUM", root.val, cursum)
-        curmax = max(curmax, cursum)
-        print(curmax)
-        if rl > rr:
-            e1 = root.left
-        else:
-            e1 = root.right
-            #print(e1.val)
-        return cursum
-     
+class Solution:    
         
     def path(self, root, rooted):
-        # DP BOTTOM UP ON TREES SOLUTION
         if root is None:
-            return -5000
-        # Compute left, right
+            return -50
         l = self.path(root.left, rooted)
         r = self.path(root.right, rooted)
-        # IF rooted 
-        # Ans is the ans
         tos = root.val
         if l < 0 and r < 0:
             tos = max(tos, root.val)
@@ -47,23 +21,14 @@ class Solution:
         else:
             tos = max(tos, root.val + l + r)
         rooted[root.val] = tos
-        #val + left + root
-        
-        # IF NOT rooted
         if l < 0 and r < 0:
             return root.val
         else:
             return root.val + max(l, r)
     
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        '''
-        e1 = root
-        v = self.dfs1(root, -5000, e1)
-        print(v, e1.val)
-        '''
         rooted = [-50] * (10 ** 5)
         self.path(root, rooted)
-        print(rooted[:100])
         return max(rooted)
         
         
