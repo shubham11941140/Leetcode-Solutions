ea25bfbb15a7d9e7c4608b84857ac12b571c87f0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-23 10:37:37 +0530

    Time: 4768 ms (39.78%), Space: 14.4 MB (72.57%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.py
@@ -0,0 +1,16 @@
+from math import ceil, sqrt
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        gen = [(i*i) for i in range(1, 110)]
+        k = len(gen)
+        dp = [10 ** 5 for i in range(n + 1)]
+        dp[0] = 0
+        for i in range(1, n + 1):
+            for j in range(k):
+                val = i - gen[j]
+                if val < 0:
+                    break
+                dp[i] = min(dp[i], dp[val] + 1)
+        return dp[n]
+        
