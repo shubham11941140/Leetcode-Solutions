43e3c745e4d676d4118d5c604cd87eecfbc4be1e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-28 18:18:04 +0530

    Time: 79 ms (5.51%), Space: 13.9 MB (99.59%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,22 @@
+from random import choice
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        a = [[] for i in range(10)]
+        a[2] = list("abc")
+        a[3] = list("def")
+        a[4] = list("ghi")
+        a[5] = list("jkl")
+        a[6] = list("mno")
+        a[7] = list("pqrs")
+        a[8] = list("tuv")
+        a[9] = list("wxyz")
+        d = [int(i) for i in digits]
+        ans = []
+        for _ in range(1000):
+            v = ''.join([choice(a[i]) for i in d])
+            ans.append(v)
+        return set(ans)                
+        
