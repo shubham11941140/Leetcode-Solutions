0017f1d8b181ed10f78e52280bbf048921100796
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-02 11:40:35 +0530

    Time: 2773 ms (7.81%), Space: 15.7 MB (10.05%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -3,22 +3,17 @@ class Solution:
         n = len(s)
         k = len(p)
         p = sorted(list(p))
-        if n < 100 and k < 100:
+        if max(n, k) < 1000:
             return [i for i in range(n) if sorted(s[i:i + k]) == p]
-        ans = []
-        print(n, k)
-        
+        ans = []        
         i = 0
         while i < n - k + 1:            
-            if sorted(s[i:i + k]) == p:
+            if sorted(s[i:i + k]) == p:                
                 ans.append(i)
                 while i < n - k and s[i] == s[i + k]:                      
                     ans.append(i)
-                    i += 1
-                    
+                    i += 1                    
             i += 1
-        i = n - k
         if sorted(s[n - k:n]) == p:
             ans.append(n - k)
-        #print(ans[-1])
         return set(ans)
