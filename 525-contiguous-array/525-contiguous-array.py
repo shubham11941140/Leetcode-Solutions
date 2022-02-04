a7748e6269cb55b826a7a1218aa70541b0418cfb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-04 11:42:39 +0530

    Time: 1821 ms (5.04%), Space: 25.2 MB (5.28%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.py
+++ 525-contiguous-array/525-contiguous-array.py
@@ -3,17 +3,13 @@ class Solution:
         c0 = 0
         c1 = 0
         n = len(nums)
-        s = []
         d = []
         for i in range(n):
             if nums[i] == 0:
                 c0 += 1
             else:
                 c1 += 1
-            s.append((c0, c1))
             d.append(c0 - c1)
-        print(s)
-        print(d)
         f = dict()
         f[0] = [0]
         for i in range(n):
@@ -21,13 +17,7 @@ class Solution:
                 f[d[i]].append(i + 1)
             else:
                 f[d[i]] = [i + 1]
-        print(f)
-        ans = 0
-        for i in f:
-            if len(f[i]) > 1:
-                val = f[i][-1] - f[i][0]
-                ans = max(ans, val)
-        print(ans)
-        return ans
+        t = [f[i][-1] - f[i][0] for i in f if len(f[i]) > 1]
+        return 0 if not t else max(t)
             
         
