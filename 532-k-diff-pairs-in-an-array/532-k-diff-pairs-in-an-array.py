7ac735008af1c20ad02f3eb9f1b6ebe30edc1b33
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-09 12:12:58 +0530

    Time: 4673 ms (5.00%), Space: 15.9 MB (35.43%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -5,20 +5,10 @@ class Solution:
             return len([i for i in h if nums.count(i) > 1])  
         nums = sorted(list(set(nums)))
         c = 0
-        print(nums)
         for i in range(len(nums)):
             if nums[i] + k in nums:
                 c += 1
             if nums[i] - k in nums:
                 c += 1
         return c // 2
-            
-        nums.sort(reverse = True)
-        n = len(nums)
-        c = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                if abs(nums[i] - nums[j]) == k:
-                    c += 1
-        return c
         
