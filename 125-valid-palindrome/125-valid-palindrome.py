2085fc16aade4ca9662ac237f80873877856eed1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-10 23:40:14 +0530

    Time: 88 ms (20.84%), Space: 20.9 MB (5.28%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t = list(s)
+        k = [t[i].lower() for i in range(len(t)) if t[i].isalnum()]
+        return k == k[::-1]
+            
+        
