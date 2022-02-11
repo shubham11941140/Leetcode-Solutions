1f86d7d45d9be3cceaad17e8caef9f0e91646c9f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-11 12:58:56 +0530

    Time: 4829 ms (14.11%), Space: 14 MB (93.32%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,14 +4,9 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k = len(s1)
         n = len(s2)
-        s1 = sorted(list(s1))
         sm = Counter(s1)
         for i in reversed(range(n)):
             if Counter(s2[i:i + k]) == sm:
                 return True
-            '''
-            if sorted(s2[i:i + k]) == s1:
-                return True
-            '''
         return False
         
