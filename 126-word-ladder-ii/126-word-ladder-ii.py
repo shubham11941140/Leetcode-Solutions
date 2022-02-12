221c6baa3eb277ad9c76c2896903db9746a094f6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-12 16:36:08 +0530

    Time: 358 ms (8.80%), Space: 14.1 MB (99.11%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.py
+++ 126-word-ladder-ii/126-word-ladder-ii.py
@@ -1,5 +1,3 @@
-from random import choice
-
 class Solution:
     
     def distone(self, a, b):
@@ -12,15 +10,6 @@ class Solution:
                 return False            
         return True
     
-    def valid(self, a):
-        n = len(a)
-        for i in range(n - 1):
-            if not self.distone(a[i], a[i + 1]):
-                print(19, a[i], a[i + 1])
-                return False
-        return True
-    
-    
     def prall(self, adj, idx, final, temp):
         if idx == len(adj):
             final.append(temp.copy())
@@ -30,8 +19,7 @@ class Solution:
                 temp.append(adj[idx][k])
                 self.prall(adj, idx + 1, final, temp)
                 temp.pop()
-        
-    
+            
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         k = len(wordList)
         if endWord not in wordList:
@@ -50,71 +38,18 @@ class Solution:
                 return []
             if word == endWord:
                 ans = adj.copy()
-                break
-                #return adj
-                #return level + 1            
+                break           
             for i in range(k):
                 if not visited[i]:                    
                     if self.distone(word, wordList[i]):
                         visited[i] = True
                         p = (wordList[i], level + 1)
-                        q.append(p)
-        #print(adj)  
-                
+                        q.append(p)                
         while [] in adj:
             adj.remove([])
-            
-        '''
-        put = []
-        for _ in range(1000):
-            m = []
-            for i in adj:
-                m.append(choice(i))
-            if m not in put:
-                put.append(m)
-        #return put
-        # Check whether each is valid
-        return [i for i in put if self.valid(i) and i[0] == beginWord and i[-1] == endWord]
-        
-        
-        
-        
-        
-        
-        
-        
-        #print(adj) 
-        put = []
-        #print(adj[:10])
-        for _ in range(10000):
-            m = []
-            for i in adj:
-                m.append(choice(i))
-            if m not in put:#print(m)
-                put.append(m)
-        #return put
-        #print(put[:10])
-        print(76)
-        mv = []
-        for j in put[:10]:
-            print(j)
-            if self.valid(j):
-                print("IN")
-                print(j)
-        print([i for i in put if self.valid(i) and i[0] == beginWord and i[-1] == endWord])
-        return [i for i in put if self.valid(i) and i[0] == beginWord and i[-1] == endWord]
-        '''
-        print(8)
-        print(adj)
         idx = 1
         temp = [adj[0][0]]
         final = []
         self.prall(adj, idx, final, temp)
-        print("FINAL STARTS")
-        print(final)      
-
-        #return []
-        # Check whether each is valid
-        #return put
-        return [i for i in final if self.valid(i) and i[0] == beginWord and i[-1] == endWord]
+        return [i for i in final if i[-1] == endWord]
         
