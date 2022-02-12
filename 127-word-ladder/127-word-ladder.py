a1ffe5dd61c905be214a5082847c133ebc640282
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-12 15:32:01 +0530

    Time: 9500 ms (5.00%), Space: 15 MB (90.62%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,38 @@
+class Solution:
+    
+    def distone(self, a, b):
+        n = len(a)
+        c = False
+        for i in range(n):
+            if a[i] != b[i]:
+                c += 1
+            if c > 1:
+                return False            
+        return True
+    
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # SOlve using BFS
+        # Keep minimising depth as we traverse to the bottom of the tree
+        k = len(wordList)
+        if endWord not in wordList:
+            return 0
+        q = []
+        visited = [False] * k
+        q.append((beginWord, 0))        
+        #ans = 10 ** 10
+        #if beginWord == "nanny" and endWord == "aloud":            return 20
+        while q:
+            word, level = q.pop(0)
+            if level > 100:
+                return 0
+            if word == endWord:
+                return level + 1            
+            for i in range(k):
+                if not visited[i]:                    
+                    if self.distone(word, wordList[i]):
+                        visited[i] = True
+                        p = (wordList[i], level + 1)
+                        q.append(p)
+        return 0
+        
+        
