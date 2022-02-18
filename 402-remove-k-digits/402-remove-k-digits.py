78c5bec974513d3872b548341b25656b0b0d12fa
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-18 12:34:07 +0530

    Time: 50 ms (64.03%), Space: 14.2 MB (67.26%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -0,0 +1,57 @@
+class Solution:
+    
+    def call(self, a, s, k):
+        h = 0
+        for i in a:
+            while i in s:
+                s.remove(i)
+                h += 1
+                if h == k:
+                    break
+            if h == k:
+                break
+        return ''.join(s)
+    
+    def new(self, num, k):
+        s = list(num).copy()
+        while k:
+            if s[0] < s[1]:
+                s.remove(s[1])
+            else:
+                s.remove(s[0])
+            k -= 1
+        return ''.join(s)
+    
+    def removeKdigits(self, num: str, k: int) -> str:        
+        st = []
+        for n in num:
+            while st and k and st[-1] > n:
+                st.pop()
+                k -= 1            
+            if st or n is not '0':
+                st.append(n)  
+        if k:
+            st = st[0:-k]            
+        return ''.join(st) or '0'
+        
+        
+        
+        if len(num) == k:
+            return "0"        
+        v = self.new(num, k)
+
+        a = list(range(10))[::-1]
+        a = [str(i) for i in a]
+        s = list(num)
+        ret = "9" * (10 ** 6)
+        for i in range(10):
+            l = s.copy()
+            v1 = self.call(a[i:], l, k)
+            if len(v1) == len(s) - k:
+                if v1 < ret:
+                    ret = v1
+        if not ret or not v:
+            return "0"
+        print("V", v)
+        print("RET", ret)
+        return str(min(int(ret), int(v)))
