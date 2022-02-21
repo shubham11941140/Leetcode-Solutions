8da813bec4f9e71b07d6620ac5c620ce6db57304
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-21 14:47:56 +0530

    Time: 97 ms (82.03%), Space: 13.9 MB (84.18%) - LeetHub

--- 372-super-pow/372-super-pow.py
+++ 372-super-pow/372-super-pow.py
@@ -1,5 +1,4 @@
 class Solution:
     def superPow(self, a: int, b: List[int]) -> int:
-        b = int(''.join([str(i) for i in b]))
-        return pow(a, b, 1337)
+        return pow(a, int(''.join([str(i) for i in b])), 1337)
         
