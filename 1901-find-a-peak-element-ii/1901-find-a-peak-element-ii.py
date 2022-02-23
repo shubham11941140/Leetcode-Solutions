8850346c77058eb91bbf30f98f8ede10de11c3e9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-23 14:29:15 +0530

    Time: 1524 ms (59.59%), Space: 45.4 MB (92.40%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    def check(self, i, j, n, m):
+        return 0 <= i < n and 0 <= j < m
+    
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        n = len(mat)
+        m = len(mat[0])
+        for i in range(n):
+            for j in range(m):
+                t = 0
+                s = 0
+                for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                    if self.check(x, y, n, m):
+                        t += 1
+                        if mat[i][j] > mat[x][y]:
+                            s += 1
+                if t == s:
+                    return [i, j]
+                        
+                
+                
+        
