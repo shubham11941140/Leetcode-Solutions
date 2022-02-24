b2cfd674354eb0214b74710308d0d31fa87adf18
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-24 21:03:32 +0530

    Time: 5884 ms (63.36%), Space: 15.6 MB (29.01%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        n = len(mat)
+        m = len(mat[0])
+        s = sum([max(i) for i in mat])  
+        if n * m * s > 10 ** 7:
+            s = 2 * target
+        dp = [[False for i in range(s + 1)] for j in range(n + 1)]
+        dp[0][0] = True                
+        for i in range(1, n + 1):
+            b = list(set(mat[i - 1]))
+            for k in range(len(b)):  
+                for j in range(1, s + 1):                                                
+                    if not dp[i][j]:
+                        if j >= b[k]:
+                            dp[i][j] = dp[i - 1][j - b[k]]
+        ans = float("INF")
+        for i in range(1, s + 1):
+            if dp[n][i]:
+                ans = min(ans, abs(target - i))               
+        if ans == float("INF"):
+            return sum([min(i) for i in mat]) - target
+        return ans
