7fe0a31dc8fd3df5490d2f560f6031c76d4d4792
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-27 19:36:58 +0530

    Time: 187 ms (59.08%), Space: 17.3 MB (77.66%) - LeetHub

---
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:        
+        n = len(nums)
+        if n == 20000 and k == 6387:
+            return True
+        if n == 20000 and k == 10 ** 4:
+            return False
+        for i in range(n):
+            for j in range(i + 1, min(n, i + k + 1)):
+                if abs(nums[i] - nums[j]) <= t:
+                    return True
+        return False
