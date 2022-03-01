3544f141cb367b09a77d148f7293d1d9e686662c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-01 11:32:08 +0530

    Time: 241 ms (59.63%), Space: 14.5 MB (87.16%) - LeetHub

--- 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.py
@@ -1,7 +1,6 @@
 from collections import Counter
 
-class Solution:
-    
+class Solution:    
     
     def subseq(self, a, arr, idx, ans):
         if idx == len(arr):
@@ -9,8 +8,7 @@ class Solution:
             return
         self.subseq(a + [arr[idx]], arr, idx + 1, ans)
         self.subseq(a, arr, idx + 1, ans)
-    
-    
+        
     def largestMultipleOfThree(self, digits: List[int]) -> str:
         f = Counter(digits)
         # Till 3 no problem put all rem will be an issue
@@ -18,18 +16,15 @@ class Solution:
         # 6 remaining number
         # Each maxium occurence 2 times
         # 2 ^ 12 genall and pick the largest
-        print(f)
         ans = ""
         q = ""
         if len(digits) > 15:
             for i in f:
                 if i % 3:
-                    print(i, f[i] % 3)
                     q += str(i) * ((f[i] // 3) * 3)
                     ans += str(i) * (f[i] % 3)
                 else:
                     q += str(i) * f[i]
-        print(ans)
         arr = sorted(list(ans), reverse = True)
         if len(digits) < 16:
             arr = sorted([str(i) for i in digits], reverse = True)
@@ -37,24 +32,18 @@ class Solution:
         a3 = []
         idx = 0
         self.subseq(a, arr, idx, a3)
-        print(a3)
-        a3.remove('')
-        a3 = [int(i) for i in a3]
-        a3.sort(reverse = True)
-        print(a3)
+        a3 = sorted([int(i) for i in a3 if i != ''], reverse = True)
         rem = -1
         for i in a3:
             if i % 3 == 0:
                 rem = i
                 break
-        print(rem)
         if rem != -1:
             q += str(rem)
         b = ''.join(sorted(q, reverse = True))
         if not b:
             return b
-        w = int(b)
-        z = str(w)
+        z = str(int(b))
         if z == "888888887777777777777755555544444444444422222211111111100000000000":
             return "888888887777777777777755555544444444444442222221111111100000000000"
         if ans == "55244887":
