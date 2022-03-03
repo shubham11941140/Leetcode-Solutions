b2dd4929d1da643fe6f7598b3eb1bc95d13388c6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-03 11:26:26 +0530

    Time: 87 ms (5.54%), Space: 14.7 MB (11.18%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -1,8 +1,11 @@
 class Solution:
+    
+    def sn(self, n):
+        return (n * (n + 1)) // 2
+    
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         n = len(nums)
         b = [nums[i + 1] - nums[i] for i in range(n - 1)]
-        print(b)
         li = 0
         ri = -1
         c = []
@@ -14,21 +17,9 @@ class Solution:
                 ri = i
             if ri >= li and ri > -1:
                 c.append([li, ri])
-        print(c)
         ans = 0
         for left, right in c:
             t = right - left
-            if t >= 1:
-                # All len 3
-                for i in range(1, t + 1):
-                    ans += (t - i + 1)
-        print(ans)
+            ans += (t * (t + 1)) // 2
         return ans
-                
-            
-        
-        for i in range(n):
-            for j in range(i + 1, n):
-                f = 1
-                
         
