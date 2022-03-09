32b17354352182ae1af4395293b6691287422d61
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-09 15:16:05 +0530

    Time: 32 ms (88.14%), Space: 13.8 MB (73.92%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        a = [0 for i in range(n + 1)]
+        if not n:
+            return 0
+        a[0] = 0
+        a[1] = 1
+        for i in range(2, n + 1):
+            a[i] = a[i - 1] + a[i - 2]
+        return a[n]        
