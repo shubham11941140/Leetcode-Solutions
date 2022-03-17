3b2cc5554c28e5d2195e7fa7c077d7714dc519b7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-18 01:18:53 +0530

    Time: 47 ms (41.92%), Space: 14 MB (16.07%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -7,29 +7,15 @@ class Solution:
                 stack.append(i)
             elif stack and i == ')':
                 stack.pop()
-        return not stack
-            
-    def score(self, s):
-        
-        if s == "":
-            return 0
-        
+        return not stack    
+     
+    def scoreOfParentheses(self, s: str) -> int:
         if s == "()":
-            return 1
-        
+            return 1        
         if len(s) > 2 and self.check(s[1:len(s) - 1]):
-            return 2 * self.score(s[1:len(s) - 1])
-        
+            return 2 * self.scoreOfParentheses(s[1:len(s) - 1])        
         else:
-            # FInd the balance point
             for i in range(1, len(s) - 1):
                 if self.check(s[:i]) and self.check(s[i:]):
-                    print(i)
-                    return self.score(s[:i]) + self.score(s[i:])
- 
-
-    
-    def scoreOfParentheses(self, s: str) -> int:
-        #print(s[len(s) - 2:], s[:len(s) - 2])
-        return self.score(s)
+                    return self.scoreOfParentheses(s[:i]) + self.scoreOfParentheses(s[i:])
         
