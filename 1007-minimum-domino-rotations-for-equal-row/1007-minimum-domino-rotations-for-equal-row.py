4f34190ea1fd06b50146228e647d79792a26fd37
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-20 11:43:28 +0530

    Time: 2014 ms (13.51%), Space: 15.2 MB (22.57%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        ans = 10 ** 10
+        for k in range(1, 7):
+            c = len([i for i in range(len(tops)) if tops[i] != k and bottoms[i] == k])
+            if tops.count(k) + c == len(tops):
+                ans = min(ans, c)
+            c = len([i for i in range(len(bottoms)) if tops[i] == k and bottoms[i] != k])
+            if bottoms.count(k) + c == len(bottoms):
+                ans = min(ans, c)                
+        return -1 if ans == 10 ** 10 else ans
+    
+                
+        
