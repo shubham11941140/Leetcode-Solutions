7770d6374767ac60259d5542dbcd3abc1d18e402
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-22 18:41:36 +0530

    Time: 312 ms (76.96%), Space: 15 MB (82.95%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -3,21 +3,15 @@ class Solution:
         s = ""
         while n:
             if k > n - 1 + 26:
-                print(6)
                 s += "z"
                 k -= 26
             elif k > n and k < n + 26:
-                print(10)
                 val = k - (n - 1)
-                print("VAL", val + 1)
                 s += chr(val + ord('a') - 1)
-                k -= (val)
-                #add ord of val
+                k -= val
             elif k == n:
-                print(15)
                 s += "a" * n
                 break                                        
             n -= 1
-        print(s)
         return s[::-1]
         
