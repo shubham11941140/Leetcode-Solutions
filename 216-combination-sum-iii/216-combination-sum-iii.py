667a0ad8497abcc7ba30dde5b9a4202d835a51c1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-30 16:07:23 +0530

    Time: 3884 ms (5.45%), Space: 68.1 MB (30.95%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,29 @@
+class Solution:
+    
+    def __init__(self):
+        self.a = list(range(1, 10))
+        self.ans = []
+    
+    def rec(self, k, n, b):
+        if k == 0:
+            self.ans.append(b.copy())
+        for i in self.a:
+            if i not in b:
+                if i <= n:
+                    self.rec(k - 1, n - i, b + [i])
+                
+    
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        b = []
+        ans = []
+        self.rec(k, n, b)
+        print(len(self.ans))
+        c = []
+        for i in self.ans:
+            if sum(i) == n and sorted(i) not in c:
+                c.append(sorted(i))
+        print(c)
+        return c
+                
+        
+        
