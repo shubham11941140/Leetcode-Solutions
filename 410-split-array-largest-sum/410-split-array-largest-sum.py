769ede9b166526747df4c4fc34a533009324bb0f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-03-31 12:20:15 +0530

    Time: 406 ms (23.44%), Space: 13.8 MB (96.00%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,12 +1,9 @@
 from math import ceil
 
 class Solution:
-    
-    
+        
     def condition(self, mid, a):
-        # Check if array can be split into m subarrays where each subarray sum does not exceed m
         n = len(a)
-        # for i in range(n)
         i = 0
         c = 0
         while i < n:
@@ -17,92 +14,34 @@ class Solution:
             c += 1
             if c > n:
                 break
-            # print("20 C is", c)
-            # break
-        # print(c, m)
         return c        
         
     def splitArray(self, nums: List[int], m: int) -> int:
-                
-        
-        
-        
-        
-        
-        
+                                                                
         n = len(nums)
-        # Number will be between max and sum
-        # BS on that find smallest number that can be broken into m partitions and we are done
-        SUM = sum(nums)
-        MAX = max(nums)
-        left = MAX
-        right = SUM
+        left = max(nums)
+        right = sum(nums)
+        
+        # Base Case
+        if self.condition(left, nums) < m:
+            return left   
+        
+        # Iterative Case for small inputs
         for i in range(left, left + 1000 + 1):
-            if self.condition(left, nums) < m:
-                return left
-            elif self.condition(i, nums) == m:
+            if self.condition(i, nums) == m:
                 return i
-                                
-        f = 0                                        
-        while left <= right:
-            # Check is mid can be found
-            '''
-            if right - left <= 1000:
-                for i in range(left, right + 1):
-                    if self.condition(i, nums) == m:
-                        return i
-            '''
-                                               
-            mid = ceil((left + right) / 2)
-            # Check if mid satidies the condition
-            print("MID", left, right, mid, f)
-            f += 1
-            if f == 200:
-                break
-            
-            c = self.condition(mid, nums)
-            print(c)
-            #if c == m:
-            # Exact
-            
+        
+        # Large inputs Binary Search
+        while left <= right:                                               
+            mid = ceil((left + right) / 2)            
+            c = self.condition(mid, nums)          
             if c > m:
-                # More than needed
-                #mid increases
-                left = mid
-                
+                left = mid                
             elif c <= m:
-                # mid decreases
-                print(75, "NM", mid)
-                if c == m and self.condition(mid - 1, nums) != m:
-                    
+                if c == m and self.condition(mid - 1, nums) != m:                    
                     return mid
-                    '''
-                    print(57)
-                    while self.condition(mid, nums) == m:                        
-                        mid -= 1
-                        print("NEW_MID", mid)
-                    return mid + 1
-                    '''
                 right = mid
             
-            
-            '''
-            if self.condition(mid, m, nums):
-                # We can reduce the size
-                
-                if not self.condition(mid - 1, m, nums):
-                    # Not for smaller
-                    # Return found answer
-                    return mid
-                else:
-                    # We can reduce
-                
-                right = mid
-            else:
-                # Mid fails
-                # Increase size
-                left = mid - 1
-            # break
-            '''
+
         
         
