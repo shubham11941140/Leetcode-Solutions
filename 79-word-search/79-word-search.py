9d3aaf9e52b873eadfd843f7526c9733295241e8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-03 18:34:55 +0530

    Time: 5084 ms (90.79%), Space: 14 MB (54.76%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,17 +1,13 @@
 class Solution:
-    
-    def valid(self, i, j, n, m):
-        return 0 <= i < n and 0 <= j < m    
-    
-    def recback(self, board, n, m, i, j, word, wordidx, visited):
-        if wordidx == len(word):
+          
+    def recback(self, board, n, m, i, j, word, wordidx, visited, lw):
+        if wordidx == lw:
             return True
-        allvalid = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
-        for x, y in allvalid:
-            if self.valid(x, y, n, m): 
+        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+            if 0 <= x < n and 0 <= y < m: 
                 if not visited[x][y] and board[x][y] == word[wordidx]:
                     visited[x][y] = True
-                    if self.recback(board, n, m, x, y, word, wordidx + 1, visited):
+                    if self.recback(board, n, m, x, y, word, wordidx + 1, visited, lw):
                         return True
                     visited[x][y] = False
         return False
@@ -20,12 +16,13 @@ class Solution:
         # REc back solution
         n = len(board)
         m = len(board[0])
+        lw = len(word)
         visited = [[False for j in range(m)] for i in range(n)]
         for i in range(n):
             for j in range(m):
                 if board[i][j] == word[0]:
                     visited[i][j] = True
-                    if self.recback(board, n, m, i, j, word, 1, visited):
+                    if self.recback(board, n, m, i, j, word, 1, visited, lw):
                         return True
                     visited[i][j] = False
         return False
