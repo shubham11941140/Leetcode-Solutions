8fc5f8a4f1430df9c1661a4bb6032198c4b878b0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-08 22:05:38 +0530

    Time: 245 ms (23.71%), Space: 24.1 MB (78.87%) - LeetHub

--- 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.py
+++ 478-generate-random-point-in-a-circle/478-generate-random-point-in-a-circle.py
@@ -5,13 +5,20 @@ class Solution:
     def __init__(self, radius: float, x_center: float, y_center: float):
         self.r = radius
         self.x = x_center
-        self.y = y_center    
+        self.y = y_center  
+    
+    def generate(self):
+        x = uniform(self.x - self.r, self.x + self.r)
+        y = uniform(self.y - self.r, self.y + self.r)
+        return (x, y)
+    
+    def equation(self, x, y):
+        return (x - self.x) ** 2 + (y - self.y) ** 2 <= self.r ** 2
         
     def randPoint(self) -> List[float]:
         while True:
-            x = uniform(self.x - self.r, self.x + self.r)
-            y = uniform(self.y - self.r, self.y + self.r)
-            if (x - self.x) ** 2 + (y - self.y) ** 2 <= self.r ** 2:
+            x, y = self.generate()
+            if self.equation(x, y):
                 return [x, y]        
 
 # Your Solution object will be instantiated and called as such:
