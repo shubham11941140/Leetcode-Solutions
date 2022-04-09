49e8eaa19f5d7e0be8020c8a478b25dd109f59d9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-09 09:00:00 +0530

    Time: 187 ms (18.99%), Space: 18.8 MB (24.51%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,6 +2,4 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
         d = Counter(nums)
-        a = [(d[i], i) for i in d]
-        v = sorted(a, reverse = True)
-        return [i[1] for i in v[:k]]
+        return [y for x, y in sorted([(d[i], i) for i in d], reverse = True)[:k]]
