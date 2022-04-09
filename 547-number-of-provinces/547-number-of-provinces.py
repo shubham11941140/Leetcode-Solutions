43dc0abd0392cf598ec230a81b4c4edecb434030
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-09 09:20:33 +0530

    Time: 273 ms (51.76%), Space: 14.4 MB (36.63%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -7,8 +7,6 @@ class Solution:
                 self.cc(v, adj, i)
     
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # no of dfs calls
-        # Make the adj list
         n = len(isConnected)
         m = len(isConnected[0])
         adj = [[] for _ in range(n + 1)]
@@ -17,12 +15,6 @@ class Solution:
                 if isConnected[i][j] == 1:
                     adj[i].append(j)
                     adj[j].append(m)
-        print(adj)
         v = [False for _ in range(n + 1)]
-        p = 0
-        for i in range(n):
-            if not v[i]:
-                self.cc(v, adj, i)
-                p += 1
-        return p
+        return len([self.cc(v, adj, i) for i in range(n) if not v[i]])
         
