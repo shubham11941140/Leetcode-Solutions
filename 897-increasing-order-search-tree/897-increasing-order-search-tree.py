1536e87ce96404528bbe62b46007c4ebae83c4bc
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-17 16:58:44 +0530

    Time: 35 ms (78.67%), Space: 13.8 MB (90.59%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -6,22 +6,20 @@
 #         self.right = right
 class Solution:
     
-    def __init__(self):
-        self.a = []
-    
-    def inorder(self, root):
+    def inorder(self, root, a):
         if root is None:
             return
-        self.inorder(root.left)
-        self.a.append(root.val)
-        self.inorder(root.right)
+        self.inorder(root.left, a)
+        a.append(root.val)
+        self.inorder(root.right, a)
         
     def increasingBST(self, root: TreeNode) -> TreeNode:
-        self.inorder(root)
-        new_root = TreeNode(self.a[0])
+        a = []
+        self.inorder(root, a)
+        new_root = TreeNode(a[0])
         new_root_copy = new_root
-        for i in range(1, len(self.a)):
-            new_root.right = TreeNode(self.a[i])
+        for i in range(1, len(a)):
+            new_root.right = TreeNode(a[i])
             new_root = new_root.right
         return new_root_copy
         
