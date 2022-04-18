f063ea935953e03f61ce7ede542cc341928a5490
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-18 12:40:57 +0530

    Time: 48 ms (61.68%), Space: 13.7 MB (100.00%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -2,15 +2,8 @@ from collections import Counter
 
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        s = [i for i in str(n)]
-        cs = Counter(s)
-        c = []
         for i in range(30):
-            k = pow(2, i)
-            t = [x for x in str(k)]
-            if cs == Counter(t):
+            if Counter([y for y in str(n)]) == Counter([x for x in str(2 ** i)]):
                 return True
         return False
-        
-        print(2 ** 30 < 10 ** 9)
         
