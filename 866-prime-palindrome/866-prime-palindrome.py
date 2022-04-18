ac7504629c4d7901edccddde30cd610615b73eb3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-18 14:21:11 +0530

    Time: 5032 ms (5.60%), Space: 14.7 MB (5.60%) - LeetHub

---
+++ 866-prime-palindrome/866-prime-palindrome.py
@@ -0,0 +1,53 @@
+from math import ceil, sqrt
+
+class Solution:
+    
+    def __init__(self):
+        self.a = []
+        self.b = []
+        
+    def isprime(self, n):
+        if n == 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, ceil(sqrt(n)) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
+        
+    def createPalindrome(self, inp, b, isOdd):
+        n = inp
+        palin = inp
+        if (isOdd):
+            n = n // b
+        while (n > 0):
+            palin = palin * b + (n % b)
+            n = n // b
+        return palin
+
+    def generatePalindromes(self, n):
+        for j in range(2):
+            i = 1
+            while (self.createPalindrome(i, 10, j % 2) < n):
+                self.a.append(self.createPalindrome(i, 10, j % 2))
+                i = i + 1     
+    
+    def gen(self):
+        n = 10 ** 8 + 10 ** 6
+        self.generatePalindromes(n)
+        for i in self.a:
+            if self.isprime(i):
+                self.b.append(i)
+        self.b.sort()
+    
+    def primePalindrome(self, n: int) -> int:
+        self.gen()
+        print(len(self.b))
+        for i in self.b:
+            if i >= n:
+                return i
+        
+        
