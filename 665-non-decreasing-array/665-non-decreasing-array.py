8e9f1771b55fd8486022b9cf0fa7e4b9d927c5d1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-20 10:42:50 +0530

    Time: 204 ms (78.23%), Space: 15.1 MB (90.62%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.py
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -1,32 +1,24 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        f = []
         n = len(nums)
-        for i in range(n - 1):
-            if nums[i] > nums[i + 1]:
-                f.append(i)
-        print(f)
+        f = [i for i in range(n - 1) if nums[i] > nums[i + 1]]
         if not f:
             return True
         elif len(f) > 1:
             return False
         elif len(f) == 1:
-            # Check whether we can acheive
             nums1 = nums.copy()
+            nums2 = nums.copy()
             nums1[f[0]] = nums1[f[0] + 1]
-            flag1 = True
+            nums2[f[0] + 1] = nums2[f[0]]
+            flag1 = True                        
+            flag2 = True
             for i in range(n - 1):
                 if nums1[i] > nums1[i + 1]:
                     flag1 = False
-                    
-                    
-            nums2 = nums.copy()
-            nums2[f[0] + 1] = nums2[f[0]]
-            flag2 = True
+                    break                                        
             for i in range(n - 1):
                 if nums2[i] > nums2[i + 1]:
                     flag2 = False
-            return flag2 or flag1
-            
-        # return True if len(f) == 1 else False
-        
+                    break
+            return flag1 or flag2
