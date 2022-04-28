9539b2884254171bdfd43eb8c307f1fbc82d042b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-28 12:56:21 +0530

    Time: 108 ms (8.17%), Space: 14 MB (10.48%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -0,0 +1,32 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        minute = 0
+        n = len(grid)
+        m = len(grid[0])
+        while minute < 1000:
+            q = []
+            for i in range(n):
+                for j in range(m):
+                    if grid[i][j] == 1:
+                        for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                            if 0 <= x < n and 0 <= y < m:
+                                if grid[x][y] == 2:
+                                    q.append((i, j))
+                                    break
+            print(q)
+            if not q:
+                for i in range(n):
+                    for j in range(m):
+                        if grid[i][j] == 1:
+                            return -1
+                
+                
+                return minute            
+            for x, y in q:
+                grid[x][y] = 2
+            minute += 1
+
+
+                                
+            
+        
