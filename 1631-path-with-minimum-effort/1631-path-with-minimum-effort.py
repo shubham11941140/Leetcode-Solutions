bb3d2a68719c689236874290eda8e245970170fa
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-04-28 12:24:56 +0530

    Time: 9541 ms (5.02%), Space: 15.2 MB (86.90%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,73 @@
+class Solution:
+    
+    def valid(self, i, j, n, m):
+        return 0 <= i < n and 0 <= j < m
+
+    def rep(self, mat, i, j, n, m, effort):
+        move = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+        for x, y in move:
+            if self.valid(x, y, n, m):
+                change = abs(mat[i][j] - mat[x][y])
+                effort[i][j] = max(effort[x][y], change)
+        '''
+                
+        for x, y in move:
+            if self.valid(x, y, n, m):                        
+                effort[i][j] = min(effort[i][j], effort[x][y])
+        '''
+    def make(self, effort, i, j, n, m):
+        move = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+        for x, y in move:
+            if self.valid(x, y, n, m):                        
+                effort[i][j] = min(effort[i][j], effort[x][y])            
+    
+    def bfs(self, a, n, m, effort):
+        v = [[False for j in range(m)] for i in range(n)]
+        q = [(0, 0)]
+        while q:
+            i, j = q.pop(0)
+            if i == n - 1 and j == m - 1:
+                return True
+            move = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            for x, y in move:
+                if self.valid(x, y, n, m):               
+                    if not v[x][y]:
+                        if abs(a[i][j] - a[x][y]) <= effort:
+                            v[x][y] = True
+                            q.append((x, y))
+        return False                                                                                                                    
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        n = len(heights)
+        m = len(heights[0])
+        left = 0
+        right = max([max(i) for i in heights]) + 100
+        
+        # Apply bs
+        while left < right:
+            mid = (left + right) // 2
+            print(48, left, mid, right)
+            if right - left <= 100:
+                print(50)
+                for i in range(left, right + 1):
+                    if self.bfs(heights, n, m, i):
+                        return i
+            if self.bfs(heights, n, m, mid):
+                right = mid
+            else:
+                left = mid + 1
+                
+        
+        
+        
+        
+        upd = [[-1 for j in range(m)] for i in range(n)]
+        for _ in range(3):
+            for i in range(n):
+                for j in range(m):
+                    self.rep(heights, i, j, n, m, upd)
+            print(upd)
+        for i in range(n):
+            for j in range(m):
+                self.make(upd, i, j, n, m)
+        print(upd)
+        
