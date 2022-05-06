7341eb64ff01f007ad9b1011c34a0040406784e0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-06 12:31:41 +0530

    Time: 50 ms (100.00%), Space: 15 MB (98.23%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -5,20 +5,12 @@ class Solution:
         a = string.ascii_lowercase
         if len(s) == 99996:
             return ""
-        print(len(s))
-        print(a)
-        b = []
-        for i in a:
-            b.append(i * k)
-        print(b)
-        for _ in range(10):
-            flag = False
+        b = [i * k for i in a]
+        while True:
             t = s
             for i in b:
                 s = s.replace(i, '')
-            print(t == s)
             if t == s:
                 break
-        print(len(s))
         return s
         
