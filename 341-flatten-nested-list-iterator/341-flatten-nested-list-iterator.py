849a0afd9ee580c45eb6b5fbf81f9f4e87b879a0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-08 22:40:58 +0530

    Time: 84 ms (55.43%), Space: 17.9 MB (41.16%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.py
@@ -22,57 +22,26 @@
 
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.nested = nestedList
         self.ans = []
         for i in nestedList:
             self.traverse(i)
-        print(self.ans)
         self.idx = -1
-        self.l = len(self.ans)
-            
-        
-        
-        
-        #print(self.nested)
-        #print(len(self.nested))
-        #print(self.nested[0].getList())
-        #k = self.nested[0].getList()
-        #print(len(k))
-        #print(self.nested[1].getList())        
+        self.l = len(self.ans)      
         
     def traverse(self, nested_list):
         if nested_list.isInteger():
             self.ans.append(nested_list.getInteger())
         else:
             for i in nested_list.getList():
-                self.traverse(i)
-            '''
-            if i.getList():
-                for j in i.getList():
-                    self.traverse(j)            
-            else:
-                # Not a list
-                if i.isInteger():
-                    val = i.getInteger()
-                    self.ans.append(val)
-            '''
-                    
-        
-    
-    
+                self.traverse(i)                
     
     def next(self) -> int:
         self.idx += 1
-        return self.ans[self.idx]     
-        
+        return self.ans[self.idx]             
     
     def hasNext(self) -> bool:
         return self.idx + 1 < self.l
             
-        
-            
-         
-
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
 # while i.hasNext(): v.append(i.next())
