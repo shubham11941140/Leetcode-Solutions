089a2496ca50f3159b7821b64fd232777a7d13c3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-10 11:57:12 +0530

    Time: 3424 ms (5.39%), Space: 68 MB (29.87%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -5,7 +5,7 @@ class Solution:
         self.ans = []
     
     def rec(self, k, n, b):
-        if not k:
+        if not k and not n:
             self.ans.append(b.copy())
         for i in self.a:
             if i not in b:
@@ -20,8 +20,9 @@ class Solution:
         self.rec(k, n, b)
         c = []
         for i in self.ans:
-            if sum(i) == n and sorted(i) not in c:
-                c.append(sorted(i))
+            l = sorted(i)
+            if l not in c:
+                c.append(l)
         return c
                 
         
