f452bb489370256875502ec9d60a1c3f758d9e98
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-11 14:14:19 +0530

    Time: 7299 ms (5.08%), Space: 13.8 MB (96.44%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -6,41 +6,24 @@ class Solution:
         self.c = 0
     
     def pall(self, a, b, k):
-        if k == 0:
+        if not k:
             self.c += 1
-            #g = ''.join(b)
-            #ans.append(g)
             return
         else:
             for i in a:
-                if b:
-                    if i >= b[-1]:
-                        #print(14, i, b[-1])
-                        self.pall(a, b + [i], k - 1)
-                    else:
+                if b != -1:
+                    if i < b:
                         break
+                    else:
+                        self.pall(a, i, k - 1)
                 else:
-                    self.pall(a, b + [i], k - 1)
+                    self.pall(a, i, k - 1)
     
     
     def countVowelStrings(self, n: int) -> int:
-        # Generate all 4 length strings
         a = ['a','e','i','o','u'][::-1]
-        k = 50
-        ans = []
-        b = []
-        self.pall(a, b, n)
-        #print(ans)
-        print(len(ans))
-        print(self.c)
+        self.pall(a, -1, n)
         return self.c
-        final = []
-        for i in ans:
-            m = ''.join(sorted(i))
-            if m not in final:
-                final.append(m)
-        #print(final)
-        print(len(final))
         
         
         
