f91d2db49b56b4d5987a10faff5ce544469b365d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-16 18:27:36 +0530

    Time: 1023 ms (28.17%), Space: 15 MB (51.48%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,17 +1,12 @@
 class Solution:
 
     def valid(self, i, j, n, m):
-        return 0 <= i < n and 0 <= j < m
+        return 
 
 
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = len(grid[0])
-
-        
-        
-        
-        
+        m = len(grid[0])                                
         dp = [[-1 for j in range(m)] for i in range(n)]
         v = [[False for j in range(m)] for i in range(n)]  
         for i in range(n):
@@ -30,36 +25,13 @@ class Solution:
         v[i][j] = True
         while q:
             i, j, val = q.pop(0)
-            # Check all 8 directions
-            t = [(i - 1, j - 1), (i - 1, j), (i - 1, j + 1), (i, j - 1), (i, j + 1), (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)]
-            for x, y in t:
-                if self.valid(x, y, n, m) and grid[x][y] == 0 and not v[x][y]:
+            if i == n - 1 and j == m - 1:
+                if dp[n - 1][m - 1] not in [-1, 10 ** 100]:
+                    return dp[n - 1][m - 1] + 1
+                return -1
+            for x, y in [(i - 1, j - 1), (i - 1, j), (i - 1, j + 1), (i, j - 1), (i, j + 1), (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)]:
+                if 0 <= x < n and 0 <= y < m and grid[x][y] == 0 and not v[x][y]:
                     dp[x][y] = min(dp[x][y], val + 1)
                     v[x][y] = True
-                    q.append((x, y, val + 1))
-        
-        if dp[n - 1][m - 1] in [-1, 10 ** 100]:
-            return -1
-        else:
-            return dp[n - 1][m - 1] + 1
-                    
-        
-
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == 0:
-                    val = 10 ** 100
-                    if self.valid(i - 1, j, n, m) and grid[i - 1][j] == 0:
-                        val = min(val, dp[i - 1][j])
-                    if self.valid(i, j - 1, n, m) and grid[i][j - 1] == 0:
-                        val = min(val, dp[i][j - 1])
-                    if self.valid(i - 1, j - 1, n, m) and grid[i - 1][j - 1] == 0:
-                        val = min(val, dp[i - 1][j - 1])
-                    dp[i][j] = min(dp[i][j], val + 1)
-
-        print(dp)
-        print(dp[n - 1][m - 1])
-        if dp[n - 1][m - 1] == -1:
-            return -1
-        else:
-            return dp[n - 1][m - 1]
+                    q.append((x, y, val + 1))        
+        return -1
