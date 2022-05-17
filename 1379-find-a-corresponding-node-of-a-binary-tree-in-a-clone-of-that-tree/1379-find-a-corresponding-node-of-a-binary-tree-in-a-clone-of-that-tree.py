ba09ec960e846942a96a531d86539c00c9e82a43
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-17 11:50:37 +0530

    Time: 1187 ms (6.74%), Space: 24.2 MB (7.32%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -12,20 +12,15 @@ class Solution:
     
     def find(self, root, item):
         if root.val == item:
-            #print(item) 
             self.ans = root
-            return root
+            return
         if root.left:
             self.find(root.left, item)
         if root.right:
             self.find(root.right, item)
     
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        t = target.val
-        #print(t)
         if cloned:
-            ans = self.find(cloned, t)
-            print("ANS", ans, self.ans)
-            return self.ans
-        return None
+            self.find(cloned, target.val)
+        return self.ans
         
