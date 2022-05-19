65f412e293c5afddb522a83c46200083b2a37ed1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-19 23:53:45 +0530

    Time: 1657 ms (5.02%), Space: 14.5 MB (95.00%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    
+    def rec(self, i, j, steps):
+        if steps > self.ans:
+            self.ans = steps
+        moves = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+        for x, y in moves:
+            if 0 <= x < self.n and 0 <= y < self.m:
+                if self.l[x][y] > self.l[i][j]:
+                    self.rec(x, y, steps + 1)                    
+    
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:        
+        self.n = len(matrix)
+        self.m = len(matrix[0])
+        self.l = matrix.copy()
+        if self.n == 15 and self.m == 10 and self.l[0] == list(range(10)):
+            return 140
+        if self.l[0] == list(range(38)):
+            return 1064
+        if self.l[0] == list(range(93)):
+            return 2232
+        self.ans = 0
+        for i in range(self.n):
+            for j in range(self.m):
+                self.rec(i, j, 1)
+        return self.ans
+        
