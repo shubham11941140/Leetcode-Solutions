60b3a485bf968c6a203645ca90b2168d6c591491
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-22 11:26:21 +0530

    Time: 1435 ms (5.00%), Space: 18.1 MB (25.45%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,18 +1,6 @@
-class Solution:
-    
-    
-    def pall(self, s):
-        return s == s[::-1]
-    
+class Solution:    
     def countSubstrings(self, s: str) -> int:
         n = len(s)
-        c = 0
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                #print(s[i:j])
-                if self.pall(s[i:j]):
-                    #print(14, s[i:j])
-                    c += 1
-        return c
+        return len([1 for i in range(n) for j in range(i + 1, n + 1) if s[i:j] == s[i:j][::-1]])
 
         
