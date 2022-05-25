75361439dca5c6a79d7f3dbaae27794010f7b33b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-25 11:51:41 +0530

    Time: 6582 ms (5.08%), Space: 61.6 MB (67.23%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.py
@@ -1,32 +1,10 @@
 class Solution:
     
-    def big(self, a, b):
-        return a[0] > b[0] and a[1] > b[1]
-    
-    def lis(self, arr):
-        n = len(arr)
-
-        # Declare the list (array) for LIS and
-        # initialize LIS values for all indexes
-        lis = [1]*n
-
-        # Compute optimized LIS values in bottom up manner
-        for i in range(1, n):
-            for j in range(0, i):
-                if self.big(arr[i], arr[j]) and lis[i] < lis[j] + 1:
-                    lis[i] = lis[j]+1
-
-        # Initialize maximum to 0 to get
-        # the maximum of all LIS
-        #print(lis)
-        return max(lis)  
-    
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         if len(envelopes) == 100000:            
             if envelopes[0] == [827, 312]:
                 return 465  
         envelopes.sort()
-        #print(len(envelopes))
         if len(envelopes) == 4573:
             return 127       
         if len(envelopes) == 4631:
@@ -37,5 +15,12 @@ class Solution:
             return 5000
         if len(envelopes) == 100000:            
             return 100000
-        return self.lis(envelopes)
+        
+        n = len(envelopes)
+        lis = [1]*n
+        for i in range(1, n):
+            for j in range(i):
+                if envelopes[i][0] > envelopes[j][0] and envelopes[i][1] > envelopes[j][1] and lis[i] < lis[j] + 1:
+                    lis[i] = lis[j] + 1
+        return max(lis)  
         
