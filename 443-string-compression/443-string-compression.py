4c7885da577680291bd1d501dc29a5c6b809b0f5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-25 12:24:48 +0530

    Time: 94 ms (29.48%), Space: 13.9 MB (73.69%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,13 +1,9 @@
-from copy import deepcopy
-
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        #generate
         a = []
         n = len(chars)
-        print(type(chars))
-        print(type(chars[0]))
         i = 0
+        
         while i < n:
             j = chars[i]
             c = 0
@@ -17,26 +13,19 @@ class Solution:
                 if i == n:
                     break
             a.append((j, c))
-        print(a)
+            
         b = []
         for i, j in a:
             b.append(i)
             if j > 1:
                 b += list(str(j))
-        print(b)
+                
         for i in range(len(b)):
             chars[i] = b[i]
+            
         while len(chars) > len(b):
             chars.pop()
+            
         return len(chars)
-        chars.clear()
-        print(chars)
-        chars = b.copy()
-        return chars
-        chars = deepcopy(b)
-        print(chars, len(chars))
-        #print(type(chars[0]))
-        #print(len(chars))
-        return [str(i) for i in chars]
                 
         
