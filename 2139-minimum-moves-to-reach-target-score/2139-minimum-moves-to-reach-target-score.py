b2e23a7d5f37094adb9a735d9edf5305bb45a37c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-05-27 17:50:09 +0530

    Time: 62 ms (5.94%), Space: 13.7 MB (98.44%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -1,22 +1,6 @@
-class Solution:
+class Solution:                                          
     
-    def rec(self, target, steps, double):
-        if steps > self.ans:
-            return
-        if not double:
-            self.rec(1, steps + target - 1, double)
-        if target == 1:
-            print(steps)
-            self.ans = min(self.ans, steps)
-            return
-        elif target % 2:
-            self.rec(target - 1, steps + 1, double)
-        else:
-            if double:
-                self.rec(target // 2, steps + 1, double - 1)
-            self.rec(target - 1, steps + 1, double)
-            
-    def moves(self, t, d):
+    def minMoves(self, t: int, d: int) -> int:
         c = 0
         while d and t > 1:                
             c += 1
@@ -25,17 +9,4 @@ class Solution:
             else:
                 t //= 2
                 d -= 1
-            print(t, d, c)
-        if t == 1:
-            return c
-        else:
-            # t > 1 and d == 0
-            return c + t - 1
-            
-            
-        
-    
-    def minMoves(self, target: int, maxDoubles: int) -> int:
-        # Rec solution
-        return self.moves(target, maxDoubles)
-        
+        return c if t == 1 else c + t - 1
