c266fbd169082f16501e494a9464323fb8de1772
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-13 10:49:32 +0530

    Time: 103 ms (41.21%), Space: 15.2 MB (33.19%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,23 +1,14 @@
-class Solution:
-    
-    def valid(self, i, n):
-        return 0 <= i < n
-    
+class Solution:    
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # DP approach
         dp = [[10 ** 10 for j in i] for i in triangle]
-        print(dp)
+        dp[0][0] = triangle[0][0]
         n = len(triangle)
-        for i in range(n):
-            if i == 0:
-                dp[i][0] = triangle[i][0]
-            elif i > 0:
-                c = len(dp[i - 1])
-                for j in range(len(dp[i])):
-                    for k in [j, j - 1]:
-                        if self.valid(k, c):
-                            dp[i][j] = min(dp[i][j], dp[i - 1][k] + triangle[i][j])
-        print(dp)
+        for i in range(1, n):
+            c = len(dp[i - 1])
+            for j in range(len(dp[i])):
+                for k in [j, j - 1]:
+                    if 0 <= k < c:
+                        dp[i][j] = min(dp[i][j], dp[i - 1][k] + triangle[i][j])
         return min(dp[-1])
             
         
