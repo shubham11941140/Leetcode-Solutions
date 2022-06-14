1a73737cf407a93bb9a7b5ea7f55ea3758df026a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-14 09:55:22 +0530

    Time: 563 ms (15.89%), Space: 16.2 MB (54.90%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -2,15 +2,10 @@ class Solution:
     def minDistance(self, X: str, Y: str) -> int:
         m = len(X)
         n = len(Y)
-        L = [[None] * (n + 1) for i in range(m + 1)]
-        for i in range(m + 1):
-            for j in range(n + 1):
-                if i == 0 or j == 0 :
-                    L[i][j] = 0
-                elif X[i-1] == Y[j-1]:
-                    L[i][j] = L[i - 1][j - 1] + 1
-                else:
-                    L[i][j] = max(L[i - 1][j], L[i][j - 1])
+        L = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                L[i][j] = L[i - 1][j - 1] + 1 if X[i - 1] == Y[j - 1] else max(L[i - 1][j], L[i][j - 1])
         return (m + n) - (2 * L[m][n])
         
         
