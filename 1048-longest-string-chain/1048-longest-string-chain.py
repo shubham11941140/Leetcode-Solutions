fac6d62cd2f36e7fc2b54322e7ef4ddfd9e2eeb1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-15 11:45:46 +0530

    Time: 7210 ms (5.01%), Space: 14.6 MB (36.68%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -6,44 +6,28 @@ class Solution:
                 w = list(word2)
                 w.pop(i)
                 if word1 == ''.join(w):
-                    print(word1, word2)
                     return True
         return False
 
     def dfs(self, node, dp, vis):
-
-        # Mark as visited
         vis[node] = True
-
-        # Traverse for all its children
         for i in range(0, len(self.adj[node])): 
-
-            # If not visited
             if not vis[self.adj[node][i]]:
                 self.dfs(self.adj[node][i], dp, vis)
-
-            # Store the max of the paths
             dp[node] = max(dp[node], 1 + dp[self.adj[node][i]])    
     
     
     def long(self):
-
-        # Dp array
         dp = [0] * (self.v + 1)
-
-        # Visited array to know if the node
-        # has been visited previously or not
         vis = [False] * (self.v + 1)
-
-        # Call DFS for every unvisited vertex
-        for i in range(1, self.v + 1): 
+        for i in range(self.v + 1): 
             if not vis[i]:
                 self.dfs(i, dp, vis)
 
         ans = 0
 
         # Traverse and find the maximum of all dp[i]
-        for i in range(1, self.v + 1): 
+        for i in range(self.v + 1): 
             ans = max(ans, dp[i])
 
         return ans    
@@ -54,8 +38,8 @@ class Solution:
         # Find max distance
         # Return
         # Make a graph
-        if words in [["a","aa","aab","aabb","bbaac"], ["a","ab","ac","bd","abc","abd","abdd"]]:
-            return 4
+        #if words in [["a","aa","aab","aabb","bbaac"], ["a","ab","ac","bd","abc","abd","abdd"]]:
+            #return 4
         self.v = len(words)
         self.adj = [[] for i in range(self.v + 1)]
         for i in range(self.v):
