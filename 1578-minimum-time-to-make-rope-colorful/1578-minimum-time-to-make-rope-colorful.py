31dfe9266e143ebd732d2d102c64af3861811468
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-17 10:40:11 +0530

    Time: 1440 ms (57.61%), Space: 25.5 MB (5.97%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        # Get intervals and then remove
+        n = len(colors)
+        i = 0
+        r = []
+        while i < n:
+            oi = i
+            while i < n - 1 and colors[i] == colors[i + 1]:
+                i += 1
+            ni = i
+            print(oi, ni)
+            if ni > oi:
+                r.append((oi, ni))
+            i += 1
+        print(r)
+        ans = 0
+        for oi, ni in r:
+            f = neededTime[oi:ni + 1]
+            ans += (sum(f) - max(f))
+            print(f)
+        return ans
+        
