4baefce9669009bb3d42c0f68cbf88d7875bd33f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-17 10:28:40 +0530

    Time: 58 ms (66.09%), Space: 14.2 MB (56.67%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -4,26 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution:    
     
-    def dfs(self, node: TreeNode) -> int:
-        
+    def __init__(self):
+        self.res = 0
+    
+    def dfs(self, node: TreeNode) -> int:        
         if not node: return 0
-        val = self.dfs(node.left) + self.dfs(node.right)
-    
-        if val == 0: return 3
-        if val < 3: return 0
-        
-        self.res += 1
-        
+        val = self.dfs(node.left) + self.dfs(node.right)    
+        if not val: return 3
+        if val < 3: return 0        
+        self.res += 1        
         return 1    
     
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
-        #res = 0
-        #def minCameraCover(self, root: TreeNode) -> int:
-        self.res = 0
-
-
-        
         return self.res + 1 if self.dfs(root) > 2 else self.res        
         
