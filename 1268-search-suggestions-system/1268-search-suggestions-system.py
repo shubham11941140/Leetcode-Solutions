c36b19de13396d0e1bfecc62ae3aa4b63543f592
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-19 07:30:34 +0530

    Time: 1276 ms (12.52%), Space: 17 MB (98.45%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,24 +1,16 @@
-class Solution:
-    
-    def check(self, a, b, idx):
-        return a[:idx + 1] == b[:idx + 1]
-        
-    
+class Solution:           
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        # Lex Sort
         products.sort()
         ans = []
         for idx, i in enumerate(searchWord):
-            print(searchWord[:idx + 1])
             part = []
             l = 0
             for j in products:
-                if self.check(searchWord, j, idx):
+                if searchWord[:idx + 1] == j[:idx + 1]:
                     part.append(j)
                     l += 1
                     if l >= 3:
                         break
-            print(part)
             ans.append(part)
         return ans
                     
