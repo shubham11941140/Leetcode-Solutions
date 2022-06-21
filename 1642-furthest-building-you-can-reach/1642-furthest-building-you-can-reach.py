111bbcedb399b7f9ee1a147fa2897f25ef82072b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-21 10:14:13 +0530

    Time: 1066 ms (23.78%), Space: 28 MB (97.57%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -1,61 +1,23 @@
 class Solution:
     
-    
     def generate(self, a):
-        n = len(a)
-        b = []
-        for i in range(n - 1):
-            if a[i + 1] > a[i]:
-                v = a[i + 1] - a[i]
-                b.append(v)
-        return b
+        return [(a[i + 1] - a[i]) for i in range(len(a) - 1) if a[i + 1] > a[i]]
     
     def valid(self, h, b, l, idx): 
-        #print("IDX", idx, h[:idx])
-        p = self.generate(h[:idx])
-        #print(p)
-        p.sort()
-        #print(p)
-        t = len(p) - l
-        print(t)
-        s = 0
-        for i in range(t):
-            s += p[i]
-        print(s, b)
-        return b >= s
-        
-    
+        p = sorted(self.generate(h[:idx]))
+        return b >= sum([p[i] for i in range(len(p) - l)])            
     
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        # Greedy
-        # Make array
         n = len(heights)
-        a = []
-        for i in range(n - 1):
-            if heights[i + 1] > heights[i]:
-                diff = heights[i + 1] - heights[i]
-                f = [diff, i, i + 1]
-                a.append(f)
-        #print(a)
-        # BS to move on the indices
         l = 0
         r = n - 1
         if self.valid(heights, bricks, ladders, n):
-            print("BOOM", r, n)
-            return n - 1
-        c = 0
+            return r
         while l <= r:
-            c += 1
-            if c > 100:
-                break
             m = (l + r) // 2
-            print(l, m, r)
             if self.valid(heights, bricks, ladders, m):
-                print("M IS ", m)
-                print("FOR M+1", self.valid(heights, bricks, ladders, m + 1))
                 if not self.valid(heights, bricks, ladders, m + 1):
                     return m - 1
-                print("Valid Index", m)
                 l = m + 1
             else:
                 r = m - 1
