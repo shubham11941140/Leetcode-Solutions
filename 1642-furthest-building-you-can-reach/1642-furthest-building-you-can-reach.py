21457d53f9cba1385a0364431169ee65fe331ad0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-21 10:17:23 +0530

    Time: 1242 ms (12.81%), Space: 27.9 MB (98.54%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -3,20 +3,23 @@ class Solution:
     def generate(self, a):
         return [(a[i + 1] - a[i]) for i in range(len(a) - 1) if a[i + 1] > a[i]]
     
-    def valid(self, h, b, l, idx): 
-        p = sorted(self.generate(h[:idx]))
-        return b >= sum([p[i] for i in range(len(p) - l)])            
+    def valid(self, idx): 
+        p = sorted(self.generate(self.h[:idx]))
+        return self.b >= sum([p[i] for i in range(len(p) - self.l)])            
     
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        n = len(heights)
+        self.h = heights
+        self.b = bricks
+        self.l = ladders
+        self.n = len(self.h)
         l = 0
-        r = n - 1
-        if self.valid(heights, bricks, ladders, n):
+        r = self.n - 1
+        if self.valid(self.n):
             return r
         while l <= r:
             m = (l + r) // 2
-            if self.valid(heights, bricks, ladders, m):
-                if not self.valid(heights, bricks, ladders, m + 1):
+            if self.valid(m):
+                if not self.valid(m + 1):
                     return m - 1
                 l = m + 1
             else:
