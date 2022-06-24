9f68a45fd667d9cd861a6e8b5c34f6d8e35eb6a3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-24 10:55:37 +0530

    Time: 179 ms (21.14%), Space: 16.3 MB (5.35%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,29 +1,19 @@
 class Solution:
-    
-    def dfs(self, graph, a, u, k):
-        for i in graph[u]:
-            a[k].append(u)
-            a[k].append(i)
-            self.dfs(graph, a, i, k)
-            k += 1
-    
-    def pdfs(self, graph, node, curr, final):
-        if node == final:
+   
+    def dfs(self, node):
+        if node == self.n - 1:
             return
-        a = curr[-1].copy()
-        for i in graph[node]:
-            curr.append(a + [i])
-            self.pdfs(graph, i, curr, final)
-        
+        a = self.curr[-1].copy()
+        for i in self.graph[node]:
+            self.curr.append(a + [i])
+            self.dfs(i)        
                                 
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
-        curr = [[0]]
-        node = 0
-        final = n - 1
-        self.pdfs(graph, node, curr, final)
-        print(curr)
-        return [i for i in curr if i[0] == node and i[-1] == final]
+        self.graph = graph
+        self.n = len(graph)
+        self.curr = [[0]]
+        self.dfs(0)
+        return [i for i in self.curr if i[-1] == self.n - 1]
         
         
                 
