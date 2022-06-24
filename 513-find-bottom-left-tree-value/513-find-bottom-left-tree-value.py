cad698c90e7ebcf62987725c9012234fcf7897fe
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-24 10:32:25 +0530

    Time: 69 ms (43.33%), Space: 16.2 MB (62.62%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
@@ -6,22 +6,18 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        # BFS
-        adj = [[] for i in range(10 ** 4 + 100)]
+        l = []
+        a = []
         q = [(root, 0)]
         while q:
             root, level = q.pop(0)
-            adj[level].append(root)
+            l.append(level)
+            a.append(root.val)
             if root.left:
                 q.append((root.left, level + 1))
             if root.right:
                 q.append((root.right, level + 1))
-                
-        
-        while [] in adj:
-            adj.remove([])
-        #print(adj)
-        #print(adj[-1])
-        for i in adj[-1]:
-            return i.val
+        m = max(l)
+        idx = l.index(m)
+        return a[idx]
         
