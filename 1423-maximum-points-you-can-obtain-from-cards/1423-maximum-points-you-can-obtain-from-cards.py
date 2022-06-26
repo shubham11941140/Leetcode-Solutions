10b85860214e128292c526031121e57be4583054
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-26 07:34:28 +0530

    Time: 617 ms (43.35%), Space: 27.9 MB (5.40%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         a = cardPoints.copy()
+        b = a[::-1]
         n = len(a)
+        ans = 0
         pre = [0] * n
+        rpre = [0] * n        
         pre[0] = a[0]
+        rpre[0] = b[0]        
         for i in range(1, n):
             pre[i] = pre[i - 1] + a[i]
-        b = a[::-1]
-        rpre = [0] * n
-        rpre[0] = b[0]
-        for i in range(1, n):
-            rpre[i] = rpre[i - 1] + b[i]
-        print(pre, rpre)
-        
-        ans = 0
+            rpre[i] = rpre[i - 1] + b[i]                
         for i in range(k + 1):
             if 0 <= i - 1 < n and 0 <= k - i - 1 < n:
                 val = pre[i - 1] + rpre[k - i - 1]
@@ -21,11 +18,6 @@ class Solution:
                 val = pre[i - 1]
             elif not (0 <= i - 1 < n) and 0 <= k - i - 1 < n:
                 val = rpre[k - i - 1]
-            #print(val)
-                
-            
-            
-            print(i, k - i, val)
             ans = max(ans, val)
         return ans
             
