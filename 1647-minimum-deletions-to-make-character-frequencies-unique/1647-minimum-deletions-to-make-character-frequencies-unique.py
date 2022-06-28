4cbfcc04010bbae03e7e983c967d02ee2cb60cf8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-06-28 09:41:38 +0530

    Time: 165 ms (78.89%), Space: 15 MB (7.85%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -2,22 +2,16 @@ from collections import Counter
 class Solution:
     def minDeletions(self, s: str) -> int:
         c = Counter(s)
-        print(c)
         l = c.values()
-        print(l)
         s = sorted(l)
-        print(s)
         if len(s) == len(set(s)):
             return 0
         i = 0
         ans = 0
         while i < len(s):
-            if not s[i]:
-                i += 1
-            elif s.count(s[i]) == 1:
+            if not s[i] or s.count(s[i]) == 1:
                 i += 1
             else:
                 s[i] -= 1
                 ans += 1
-        print(s, ans)
         return ans
