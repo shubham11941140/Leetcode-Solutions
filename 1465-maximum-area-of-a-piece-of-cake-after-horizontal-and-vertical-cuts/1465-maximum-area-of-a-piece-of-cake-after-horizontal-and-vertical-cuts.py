546ca68ad0d6c25d3dee44c79f0a735c9ee36224
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-02 12:54:28 +0530

    Time: 319 ms (97.88%), Space: 29.3 MB (18.90%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -2,7 +2,5 @@ class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
         l = [0] + sorted(horizontalCuts) + [h]
         b = [0] + sorted(verticalCuts) + [w]
-        ml = max([(l[i + 1] - l[i]) for i in range(len(l) - 1)])
-        mb = max([(b[i + 1] - b[i]) for i in range(len(b) - 1)])
-        return (ml * mb) % (10 ** 9 + 7)
+        return (max([(l[i + 1] - l[i]) for i in range(len(l) - 1)]) * max([(b[i + 1] - b[i]) for i in range(len(b) - 1)])) % (10 ** 9 + 7)
         
