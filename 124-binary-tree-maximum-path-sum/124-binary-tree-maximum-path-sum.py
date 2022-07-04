44d93dda87004e2f75c2bb0c5d368f70bd08f865
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-04 11:10:50 +0530

    Time: 363 ms (5.02%), Space: 22 MB (5.14%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -11,19 +11,7 @@ class Solution:
             return -50
         l = self.path(root.left)
         r = self.path(root.right)
-        tos = root.val
-        tos += sum([k for k in [l, r] if k > 0])
-        '''
-        if l < 0 and r < 0:
-            tos = max(tos, root.val)
-        elif l < 0:
-            tos = max(tos, root.val + r)
-        elif r < 0:
-            tos = max(tos, root.val + l)
-        else:
-            tos = max(tos, root.val + l + r)
-        '''
-        self.rooted[root.val] = tos
+        self.rooted[root.val] = root.val + sum([k for k in [l, r] if k > 0])
         return root.val if l < 0 and r < 0 else root.val + max(l, r)
     
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
