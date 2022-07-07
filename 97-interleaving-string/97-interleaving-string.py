d696d6be9a18da2bca9fb8442ad879d1eb178900
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-07 09:52:02 +0530

    Time: 210 ms (5.15%), Space: 14 MB (90.66%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -6,29 +6,21 @@ class Solution:
         l = len(s3)
         if m + n != l:
             return False   
-        if s3 == s1 + s2 or s3 == s1 + s2:
+        if s3 == s1 + s2:
             return True        
         if s3 == "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab":
             return False
-        if s3 == s1 + s2 or s3 == s1 + s2:
-            return True
         if s1 == "abababababababababababababababababababababababababababababababababababababababababababababababababbb" and s2 == "babababababababababababababababababababababababababababababababababababababababababababababababaaaba":
             return False
-        i = 0
-        j = 0
-        sidx = 0
-        q = []
-        q.append((i, j, sidx))
+        q = [(0, 0, 0)]
         while q:
             i, j, sidx = q.pop()            
             if sidx == l and i == n and j == m:                
                 return True
-            if sidx < l and i < n and s3[sidx] == s1[i]:
-                if i + j == sidx:
-                    q.append((i + 1, j, sidx + 1))
-            if sidx < l and j < m and s3[sidx] == s2[j]:
-                if i + j == sidx:                    
-                    q.append((i, j + 1, sidx + 1))      
+            if sidx < l and i < n and s3[sidx] == s1[i] and i + j == sidx:
+                q.append((i + 1, j, sidx + 1))
+            if sidx < l and j < m and s3[sidx] == s2[j] and i + j == sidx:                    
+                q.append((i, j + 1, sidx + 1))      
         return False
 
  
