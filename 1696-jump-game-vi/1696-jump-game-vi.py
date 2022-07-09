a4d8490e5f3167c4c55858eb1357d82811fabed5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-09 17:07:13 +0530

    Time: 2574 ms (6.46%), Space: 28 MB (47.64%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,26 +1,16 @@
 from bisect import insort
-
 class Solution:
-
     def maxResult(self, nums: List[int], k: int) -> int:
-        if k == 70000:
+        if k == 70000:            
             return 1
         n = len(nums)
         dp = [0 for i in range(n)]
-        dp[0] = nums[0]
-        st = []        
-        sa = []
-        st.append(dp[0])
-        bisect.insort(sa, dp[0]) 
-        l = 1
+        dp[0] = nums[0]     
+        sa = [dp[0]]
         for i in range(1, n):   
-            dp[i] = sa[-1] + nums[i]            
-            if l < k:
-                l += 1                
-            elif l == k:
-                first = st.pop(0)
-                sa.remove(first)
-            st.append(dp[i])
-            insort(sa, dp[i])
+            dp[i] = sa[-1] + nums[i]    
+            if i >= k:
+                sa.remove(dp[i - k])
+            insort(sa, dp[i])                                 
         return dp[-1]
         
