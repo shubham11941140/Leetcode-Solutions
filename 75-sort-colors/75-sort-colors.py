1fe7f58c723aaf65d497f1c60ce43f1606e4d01f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-11 15:48:58 +0530

    Time: 90 ms (5.14%), Space: 13.8 MB (97.25%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,15 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        c0 = nums.count(0)
-        c1 = nums.count(1)
-        c2 = nums.count(2)
-        for i in range(c0):
-            nums[i] = 0
-        for i in range(c0, c0 + c1):
-            nums[i] = 1
-        for i in range(c0 + c1, n):
-            nums[i] = 2
+        nums.sort()
             
             
