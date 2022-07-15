4a3d163418d439571ffe43e1517471b2e3c2df60
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-15 23:14:55 +0530

    Time: 282 ms (18.91%), Space: 17.6 MB (29.69%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -0,0 +1,28 @@
+class Solution:
+    
+    def dfs(self, i, j):
+        self.v[i][j] = True
+        self.val += 1
+        for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
+            if 0 <= x < self.n and 0 <= y < self.m:
+                if not self.v[x][y] and self.g[x][y]:
+                    self.dfs(x, y)
+                    
+        
+    
+    
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.g = grid
+        self.n = len(self.g)
+        self.m = len(self.g[0])
+        self.v = [[False for j in range(self.m)] for i in range(self.n)]
+        self.a = 0
+        for i in range(self.n):
+            for j in range(self.m):
+                if not self.v[i][j] and self.g[i][j]:
+                    self.val = 0
+                    self.dfs(i, j)
+                    self.a = max(self.a, self.val)
+        return self.a
+        
+        
