d496059ccad84fe41d92d7a8b49df9c669a80a23
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-19 10:49:41 +0530

    Time: 41 ms (69.74%), Space: 13.8 MB (61.64%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        a = [[] for i in range(35)]
+        a[0].append(1)
+        for i in range(1, 35):
+            la = len(a[i - 1])
+            a[i] += [sum([a[i - 1][k] for k in [j - 1, j] if 0 <= k < la]) for j in range(i)]       
+        a.pop(0)
+        return a[rowIndex]                            
