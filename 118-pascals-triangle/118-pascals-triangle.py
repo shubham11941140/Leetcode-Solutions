412cf8db01129408cb42cbfd66d7968ce12db688
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-19 10:48:17 +0530

    Time: 61 ms (16.16%), Space: 14 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -3,13 +3,8 @@ class Solution:
         a = [[] for i in range(32)]
         a[0].append(1)
         for i in range(1, 32):
-            b = a[i - 1].copy()
-            for j in range(i):
-                s = 0
-                for k in [j - 1, j]:
-                    if 0 <= k < len(b):
-                        s += b[k]
-                a[i].append(s)        
+            la = len(a[i - 1])
+            a[i] += [sum([a[i - 1][k] for k in [j - 1, j] if 0 <= k < la]) for j in range(i)]       
         a.pop(0)
         return a[:numRows]
                     
