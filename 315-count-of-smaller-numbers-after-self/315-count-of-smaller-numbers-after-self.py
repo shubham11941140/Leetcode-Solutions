a231f90bd9e93f5ee5dda9ae87f2e594f41b148f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-07-24 00:05:11 +0530

    Time: 2997 ms (81.89%), Space: 32.5 MB (81.15%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        arr = sorted(nums) 
+        a = []  
+        for num in nums:
+            i = bisect_left(arr, num)
+            a.append(i)
+            del arr[i]         
+        return a  
