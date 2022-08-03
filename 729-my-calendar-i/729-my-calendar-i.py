7d67416c00512a525ea7ce4d8ba6ce5852286350
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-03 12:08:40 +0530

    Time: 972 ms (20.57%), Space: 14.7 MB (58.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -3,14 +3,9 @@ class MyCalendar:
     def __init__(self):
         self.c = []
         
-
     def book(self, start: int, end: int) -> bool:
         for s, e in self.c:
-            if start >= s and end < e:
-                return False
-            if s <= start < e:
-                return False
-            if end > s >= start:
+            if (start >= s and end < e) or s <= start < e or end > s >= start:
                 return False
         self.c.append((start, end))
         return True
