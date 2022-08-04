77dfc2c1dd8de40192237bbb61ef0bb2d0fc7397
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-04 15:55:21 +0530

    Time: 60 ms (16.83%), Space: 13.9 MB (63.37%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:        
+        l = lcm(p, q)
+        if not (l // q) % 2:
+            return 2
+        return (l // p) % 2        
