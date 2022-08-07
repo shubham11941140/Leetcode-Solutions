236974114bbbf9ccc4ce52c9e7e63bd0611652bd
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-07 14:43:11 +0530

    Time: 621 ms (50.64%), Space: 19.5 MB (48.43%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -5,17 +5,10 @@ class Solution:
         for j in range(5):
             dp[0][j] = 1            
         for i in range(1, n):
-            # a
-            dp[i][0] += (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4])
-            # e
-            dp[i][1] += (dp[i - 1][0] + dp[i - 1][2])
-            # i
-            dp[i][2] += (dp[i - 1][1] + dp[i - 1][3])
-            # o
-            dp[i][3] += dp[i - 1][2]
-            # u
-            dp[i][4] += (dp[i - 1][2] + dp[i - 1][3])
-            for j in range(5):
-                dp[i][j] %= M
+            dp[i][0] = (dp[i][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % M
+            dp[i][1] = (dp[i][1] + dp[i - 1][0] + dp[i - 1][2]) % M
+            dp[i][2] = (dp[i][2] + dp[i - 1][1] + dp[i - 1][3]) % M
+            dp[i][3] = (dp[i][3] + dp[i - 1][2]) % M
+            dp[i][4] = (dp[i][4] + dp[i - 1][2] + dp[i - 1][3]) % M
         return sum(dp[n - 1]) % M
         
