9de3bee620691df4b690d1cdf55957064241736f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-10 10:48:25 +0530

    Time: 31 ms (97.09%), Space: 14 MB (33.62%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.py
+++ 677-map-sum-pairs/677-map-sum-pairs.py
@@ -3,20 +3,13 @@ class MapSum:
     def __init__(self):
         self.d = {}
         
-
     def insert(self, key: str, val: int) -> None:
         self.d[key] = val
                     
     def sum(self, prefix: str) -> int:
         n = len(prefix)
-        ans = 0
-        for i in self.d:
-            if i[:n] == prefix:
-                ans += self.d[i]
-        return ans
+        return sum([self.d[i] for i in self.d if i[:n] == prefix])
         
-
-
 # Your MapSum object will be instantiated and called as such:
 # obj = MapSum()
 # obj.insert(key,val)
