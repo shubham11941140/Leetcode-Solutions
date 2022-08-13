8917db04d6feeebd1852d843552abb7010c1046a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-13 11:45:19 +0530

    Time: 7564 ms (5.00%), Space: 14.4 MB (16.18%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,48 @@
+class Solution:
+    
+    def binarySearch(self, arr, x):
+        l = 0
+        r = len(arr)
+        while l <= r:
+            m = l + ((r - l) // 2)
+            print(m)
+            res = (x == arr[m])
+            if (res == 0):
+                return m - 1
+            if (res > 0):
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+           
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        words.sort()
+        self.w = set(words)
+        lwords = [len(i) for i in words]
+        tlen = sum(lwords)
+        lb = min(lwords)
+        ub = max(lwords)
+        ans = []
+        for i in range(len(s)):
+            b = s[i:i+tlen]
+            idx = 0
+            check = []
+            while idx <= tlen:  
+                Flag = False
+                for j in range(lb, ub + 1):
+                    c = b[idx:idx + j]
+                    if c in self.w:
+                    #if self.binarySearch(words, c) != -1:
+                        check.append(c)
+                        idx += j
+                        Flag = True
+                        break
+                if not Flag:
+                    break
+            if sorted(check) == words:
+                ans.append(i)
+        return ans
+                
+               
+        
+        
