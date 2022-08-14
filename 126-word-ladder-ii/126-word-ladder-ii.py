73af7b10edcdfbefa58d730c9cf0901cb68c3cdc
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-14 13:16:00 +0530

    Time: 507 ms (13.64%), Space: 14.3 MB (83.01%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.py
+++ 126-word-ladder-ii/126-word-ladder-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     
     def distone(self, a, b):
-        return len([1 for i in range(self.wl) if a[i] != b[i]]) == 1
+        return [1 for i in range(self.wl) if a[i] != b[i]] == [1]
     
-    def prall(self, idx, temp):
+    def produce_all(self, idx, temp):
         t = temp[-1]
         if t == self.b:
-            self.f.append(temp.copy()[::-1])
+            self.f.append(temp.copy())
             return
         if idx == -1:
             return
         for i in self.a[idx]:
             if self.distone(t, i):
-                #temp.append(i)
-                self.prall(idx - 1, temp + [i])
-                #temp.pop()
+                self.produce_all(idx - 1, temp + [i])
             
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         k = len(wordList)
         self.wl = len(beginWord)
         if endWord not in wordList:
             return []
-        q = []
+        q = [(beginWord, 0)]
         visited = [False] * k
-        q.append((beginWord, 0))
         if k > 100:
             adj = [[] for i in range(100)]
         else:
@@ -34,21 +31,18 @@ class Solution:
             if level > 100:
                 return []
             if word == endWord:                
-                self.l = level
                 break           
             for i in range(k):
                 if not visited[i]:                    
                     if self.distone(word, wordList[i]):
                         visited[i] = True
-                        p = (wordList[i], level + 1)
-                        q.append(p)                
+                        q.append((wordList[i], level + 1))                
         while [] in adj:
             adj.remove([])
         self.a = adj.copy()
-        #print(self.a)
         idx = len(self.a) - 2
         temp = [endWord]
         self.f = []
         self.b = beginWord
-        self.prall(idx, temp)
-        return self.f
+        self.produce_all(idx, temp)
+        return [i[::-1] for i in self.f]
