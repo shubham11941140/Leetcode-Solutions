b3fa1b119929eae6169e66dba7818fb37c39a855
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-15 12:54:03 +0530

    Time: 6422 ms (5.04%), Space: 105.5 MB (7.22%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -3,31 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    
-    def insert(self, i):
-        t = ListNode(i)
-        t.next = self.r
-        self.r = t
-    
+class Solution:       
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         a = []
         while head is not None:
             a.append(head.val)
             head = head.next
-        print(a)
         n = len(a)
         z = [i for i in range(n) if not a[i]]
-        print(z)
-        b = []
-        for i in range(len(z) - 1):
-            l, r = z[i], z[i + 1]
-            s = sum([a[k] for k in range(l, r)])
-            b.append(s)
-        print(b)
+        lz = len(z)
+        b = [sum([a[k] for k in range(z[i], z[i + 1])]) for i in range(lz - 1)]
         self.r = None
         for i in b[::-1]:
-            self.insert(i)
+            t = ListNode(i)
+            t.next = self.r
+            self.r = t
         return self.r
                 
             
