d9d0948c8764a228794957cde4e75dff7f887340
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-17 15:45:07 +0530

    Time: 1211 ms (27.19%), Space: 17.6 MB (74.65%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.py
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.py
@@ -4,39 +4,25 @@ class ATM:
         self.a = [0] * 5
         self.b = [20, 50, 100, 200, 500]
         
-
     def deposit(self, banknotesCount: List[int]) -> None:
         for i in range(5):
             self.a[i] += banknotesCount[i]
         
-
     def withdraw(self, amount: int) -> List[int]:
-        # Greedy
-        f = self.a.copy()
         ans = [0] * 5
         for i in reversed(range(5)):
-                r = amount % self.b[i]
-                q = amount // self.b[i]
-                if self.a[i] >= q:
-                    amount -= (q * self.b[i])
-                    ans[i] = q
-                    self.a[i] -= q
-                else:
-                    amount -= (self.a[i] * self.b[i])
-                    ans[i] = self.a[i]
-                    self.a[i] = 0
-                if not amount:
-                    break
+            m = min((amount // self.b[i]), self.a[i])
+            amount -= (m * self.b[i])
+            ans[i] = m
+            self.a[i] -= m
+            if not amount:
+                break
         if amount:
             for i in range(5):
                 self.a[i] += ans[i]
             return [-1] 
-        print(f, self.a, ans)
         return ans
             
-        
-
-
 # Your ATM object will be instantiated and called as such:
 # obj = ATM()
 # obj.deposit(banknotesCount)
