db75d731af0531946456c957433ee25ce5467a0d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-19 14:54:17 +0530

    Time: 1179 ms (11.04%), Space: 15.4 MB (25.22%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -1,27 +1,22 @@
+import bisect
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        # Try 1
-        # Make a dictionary with key-value(tuple)
-        # value will be length of smallest subsequence ending there
-        # value is a list of lengths of sequences ending with i 
         d = {}
-        n = len(nums)
-        #d[nums[0]] = [1]
-        for i in range(n):
-            val = nums[i]
+        for val in nums:
             if val - 1 in d and d[val - 1]:
-                m = min(d[val - 1])
+                m = d[val - 1][0]
                 if val in d:
-                    d[val].append(m + 1)
+                    bisect.insort(d[val], m + 1)
+                    #d[val].append(m + 1)
                 else:
                     d[val] = [m + 1]
-                d[val - 1].remove(m)
+                d[val - 1].pop(0)
             else:
                 if val in d:
-                    d[val].append(1)        
+                    bisect.insort(d[val], 1)
+                    #d[val].append(1)        
                 else:
                     d[val] = [1]
-            #print(d)
         for i in d:
             if d[i]:
                 mdi = min(d[i])
