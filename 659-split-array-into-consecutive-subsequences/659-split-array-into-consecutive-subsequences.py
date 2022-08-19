b3e0b7a350fa150634b6a55b6d57fcf9d15932a6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-19 14:58:02 +0530

    Time: 1112 ms (15.06%), Space: 15.3 MB (25.22%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -1,25 +1,22 @@
-import bisect
+from bisect import insort
+
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         d = {}
         for val in nums:
             if val - 1 in d and d[val - 1]:
-                m = d[val - 1][0]
+                m = d[val - 1].pop(0)
                 if val in d:
-                    bisect.insort(d[val], m + 1)
-                    #d[val].append(m + 1)
+                    insort(d[val], m + 1)
                 else:
-                    d[val] = [m + 1]
-                d[val - 1].pop(0)
+                    d[val] = [m + 1]                
             else:
                 if val in d:
-                    bisect.insort(d[val], 1)
-                    #d[val].append(1)        
+                    insort(d[val], 1)       
                 else:
                     d[val] = [1]
         for i in d:
             if d[i]:
-                mdi = min(d[i])
-                if mdi < 3:
+                if d[i][0] < 3:
                     return False
         return True             
