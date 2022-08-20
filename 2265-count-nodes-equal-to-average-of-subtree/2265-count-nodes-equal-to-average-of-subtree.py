45cea15871fd443b24ca82a2d8468b22e8375809
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-20 15:43:46 +0530

    Time: 1528 ms (5.03%), Space: 15.1 MB (79.11%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def avg(self, root, s, co):
+        if not root:
+            return
+        s += root.val
+        co += 1
+        self.avg(root.left, s, co)
+        self.avg(root.right, s, co)  
+    
+    def croot(self, root):
+        if not root:
+            return
+        s = 0
+        co = 0
+        self.avg(root, s, co)
+        if co and s // co == root.val:
+            self.c += 1
+        self.croot(root.left)
+        self.croot(root.right)
+        
+        
+    def rsum(self, root):
+        if not root:
+            return
+        self.ans1 += root.val
+        self.ans2 += 1
+        #print(34, self.ans1, self.ans2)
+        if root.left:
+            self.rsum(root.left)
+        if root.right:
+            self.rsum(root.right)
+        
+    def call(self, root):
+        if not root:
+            return
+        self.ans1 = 0
+        self.ans2 = 0
+        self.rsum(root)
+        print(47, root.val, self.ans1, self.ans2)   
+        if self.ans2:
+            if root.val == (self.ans1 // self.ans2):
+                self.c += 1
+        if root.left:            
+            self.call(root.left)
+        if root.right:            
+            self.call(root.right)
+        
+                    
+    def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
+        self.c = 0
+        self.call(root)
+        return self.c
+        
