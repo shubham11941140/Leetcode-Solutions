ab90533ce0e493af24486f7151be4ba1ff208953
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-21 12:01:37 +0530

    Time: 129 ms (86.57%), Space: 15.5 MB (37.31%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def movesToStamp(self, stamp: str, target: str) -> List[int]:
+        n, m = len(stamp), len(target)
+        s = set(['#' * i + stamp[i:n - j] + '#' * j for i in range(n) for j in range(n - i)])
+        end = '#'*m
+        r = []
+        while target != end:
+            c = False
+            for i in reversed(range(m - n + 2)):
+                if target[i:i + n] in s:
+                    target = target[:i] + '#' * n + target[i + n:]
+                    r.append(i)
+                    c = True
+            if not c:
+                return []
+        return r[::-1]    
+    
