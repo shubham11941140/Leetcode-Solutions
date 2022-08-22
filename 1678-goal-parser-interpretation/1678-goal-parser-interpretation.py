da0de7d5cf9db20289e463b0edeea57a79035dbd
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-22 19:31:32 +0530

    Time: 57 ms (24.06%), Space: 14 MB (10.33%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        s = command
+        s = s.replace("(al)", "al")
+        s = s.replace("()", "o")
+        return s
+            
+        
