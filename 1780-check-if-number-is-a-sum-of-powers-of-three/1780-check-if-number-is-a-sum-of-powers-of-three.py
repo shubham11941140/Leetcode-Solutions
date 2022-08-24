333814b5afca3c53d5ed55fd8dae3b8b6248249f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-24 11:57:59 +0530

    Time: 7069 ms (5.12%), Space: 19.6 MB (7.53%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def subset(self, a, temp, idx):
+        if idx == len(a):
+            self.ans.append(temp.copy())
+            return
+        self.subset(a, temp + [a[idx]], idx + 1)
+        self.subset(a, temp, idx + 1)
+    
+    def checkPowersOfThree(self, n: int) -> bool:
+        x = 1
+        a = []
+        while x <= 10 ** 7:
+            a.append(x)
+            x *= 3
+        temp = []
+        idx = 0
+        self.ans = []
+        self.subset(a, temp, idx)
+        #print(self.ans)
+        #print(len(self.ans))
+        b = [sum(i) for i in self.ans]
+        #print(b)
+        return n in b
