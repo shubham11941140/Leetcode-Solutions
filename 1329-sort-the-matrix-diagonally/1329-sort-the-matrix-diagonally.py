b5f7beb29dd9b22f36282e03472030b260b25be7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-28 16:09:25 +0530

    Time: 286 ms (5.02%), Space: 14.3 MB (51.34%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -4,7 +4,7 @@ class Solution:
         m = len(mat[0])
         s = set([(i - j) for i in range(n) for j in range(m)])                    
         for k in s:
-            b = sorted([mat[i][j] for i in range(n) for j in range(m) if i - j == k])
+            b = sorted([mat[i][i - k] for i in range(n) if 0 <= (i - k) < m])
             c = 0
             for i in range(n):
                 for j in range(m):
