59a186d6f773f3dfe690681d462ff006a458eb58
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-31 14:41:22 +0530

    Time: 391 ms (68.37%), Space: 15.5 MB (78.42%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,31 +1,9 @@
 class Solution:
     
-
-    
-    
-    @cache
-    def bfs(self):        
-        while self.q:
-            i, j = self.q.pop(0)
-            self.v[i][j] = True
-            if 0 in [i, j]:
-                self.p = True
-            if i == self.n - 1 or j == self.m - 1:
-                self.a = True
-            if self.p and self.a:
-                break
-            for x, y in [(i, j - 1), (i, j + 1), (i - 1, j), (i + 1, j)]:
-                if 0 <= x < self.n and 0 <= y < self.m:
-                    if self.h[x][y] <= self.h[i][j]:
-                        if not self.v[x][y]:
-                            self.q.append((x, y))
-            
     def dfs(self, v, i, j, val):
         if i < 0 or j < 0 or i >= self.n or j >= self.m:
             return
-        if self.h[i][j] < val:
-            return
-        if v[i][j]:
+        if self.h[i][j] < val or v[i][j]:
             return
         v[i][j] = True
         for x, y in [(i + 1, j), (i - 1, j), (i, j - 1), (i, j + 1)]:
@@ -33,7 +11,6 @@ class Solution:
         
     
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # DFS at each cell check whether we reach limits
         self.n = len(heights)
         self.m = len(heights[0])
         self.h = heights
@@ -45,16 +22,4 @@ class Solution:
                     self.dfs(a, i, j, 0)
                 if i == self.n - 1 or j == self.m - 1:
                     self.dfs(p, i, j, 0)
-        print("A", a)
-        print("P", p)
-        ans = []
-        for i in range(self.n):
-            for j in range(self.m):
-                if a[i][j] and p[i][j]:
-                    print(i, j)
-                    ans.append([i, j])
-        return ans
-
-                
-                
-        
+        return [[i, j] for i in range(self.n) for j in range(self.m) if a[i][j] and p[i][j]]               
