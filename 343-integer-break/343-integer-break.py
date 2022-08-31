79ed58245c1cb6a05c99fcc4895ecbaa4ccfae8e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-08-31 15:22:42 +0530

    Time: 84 ms (5.71%), Space: 13.8 MB (65.25%) - LeetHub

--- 343-integer-break/343-integer-break.py
+++ 343-integer-break/343-integer-break.py
@@ -1,16 +1,9 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        # Break into positive integers
-        dp = [0 for i in range(60)]
+        dp = [0 for i in range(n + 1)]
         dp[1] = 1
-        for i in range(2, 60):
-            ans = 0
-            for j in range(i):
-                p = max(j, dp[j])
-                q = max(i - j, dp[i - j])
-                ans = max(ans, p * q)
-            dp[i] = ans
-        print(dp)
+        for i in range(2, n + 1):
+            dp[i] = max([max(j, dp[j]) * max(i - j, dp[i - j]) for j in range(i)])
         return dp[n]
         
         
