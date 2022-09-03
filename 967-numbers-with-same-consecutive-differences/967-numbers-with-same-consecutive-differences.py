461dab498b6ff508687a29ec9ed8fc6c2841dd72
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-03 11:04:56 +0530

    Time: 59 ms (67.01%), Space: 14.2 MB (41.16%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,25 +1,19 @@
 class Solution:
-    
-    
-    def rec(self, n, k, a, ans):
-        if len(a) == n:
-            s = ''.join([str(x) for x in a])
-            ans.append(int(s))
+        
+    def rec(self, a):
+        if len(a) == self.n:
+            self.ans.append(int(''.join([str(x) for x in a])))
             return
-        print(a)
         prev = a[-1]
-        possible = [prev + k, prev - k]
-        for i in possible:
+        for i in [prev + self.k, prev - self.k]:
             if 0 <= i <= 9:
-                # Append i to a and recurse
-                self.rec(n, k, a + [i], ans)
-    
-    
+                self.rec(a + [i])
+        
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        # Recursion solution
-        ans = []
+        self.n = n
+        self.k = k
+        self.ans = []
         for i in range(1, 10):
-            self.rec(n, k, [i], ans)
-        print(ans)
-        return list(set(ans))
+            self.rec([i])
+        return list(set(self.ans))
                     
