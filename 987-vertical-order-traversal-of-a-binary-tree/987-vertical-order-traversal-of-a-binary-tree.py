4d5889f9c74e1c986700ec7de09eb79be7caea34
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-04 12:18:51 +0530

    Time: 58 ms (39.45%), Space: 14.3 MB (28.65%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -6,26 +6,16 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # BFS
         q = [(root, 0, 0)]
         a = [[] for i in range(2500)]
         while q:
             root, row, col = q.pop(0)
-            print(root.val, row, col)
             a[col + 1100].append((row, root.val))
             if root.left:
                 q.append((root.left, row + 1, col - 1))
             if root.right:
                 q.append((root.right, row + 1, col + 1))
-        b = [i for i in a if i]
-        print(b)
-        sb = [sorted(i) for i in b]
-        print(sb)
-        d = []
-        for i in sb:
-            ji = [j[1] for j in i]
-            d.append(ji)
-        print(d)
-        return d
+        b = [sorted(i) for i in a if i]
+        return [[j[1] for j in i] for i in b]
             
         
