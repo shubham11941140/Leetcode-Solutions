472d8510f633218de886ca917bb18657c07e5fd1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-07 16:29:10 +0530

    Time: 87 ms (44.65%), Space: 16.5 MB (30.02%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -4,39 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    
-    
-    def po(self, root, a):
-        if not root:
-            return
-        a.append(str(root.val))
-        a.append('(')
-        self.po(root.left, a)
-        a.append(')')
-        a.append('(')
-        self.po(root.right, a)
-        a.append(')')
-        
-    def rec(self, root):
+class Solution:        
+    def tree2str(self, root: Optional[TreeNode]) -> str:
         if not root:
             return ''
-        # Root exists   
-        #print(root)
         if not root.left and not root.right:
-            #print(27)
             return str(root.val)
         if root.left and not root.right:  
-            #print(30)
-            return str(root.val) + '(' + self.rec(root.left) + ')'
+            return str(root.val) + '(' + self.tree2str(root.left) + ')'
         if not root.left and root.right:
-            #print(33)
-            return str(root.val) + '()' + '(' + self.rec(root.right) + ')'
+            return str(root.val) + '()' + '(' + self.tree2str(root.right) + ')'
         if root.left and root.right:
-            #print(36)
-            return str(root.val) + '(' + self.rec(root.left) + ')' + '(' + self.rec(root.right) + ')'
-        
-    def tree2str(self, root: Optional[TreeNode]) -> str:
-        s = self.rec(root)
-        print("S", s)
-        return s
+            return str(root.val) + '(' + self.tree2str(root.left) + ')' + '(' + self.tree2str(root.right) + ')'
+      
