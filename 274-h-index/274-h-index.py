29d9191c82ebc5d0502b4368523f422153d47a60
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-10 13:17:07 +0530

    Time: 760 ms (5.01%), Space: 14.2 MB (30.08%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        b = []
+        
+        for c in range(1001):
+            ans = 0
+            for cit in citations:
+                if cit >= c:
+                    ans += 1
+            print(c, ans)
+            if ans >= c:
+                b.append(c)
+        return b[-1]
+                
+        
