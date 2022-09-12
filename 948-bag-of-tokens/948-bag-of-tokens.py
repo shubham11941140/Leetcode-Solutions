df944883d4013aa36664679745135ba7063875c0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-12 11:09:01 +0530

    Time: 216 ms (5.19%), Space: 14.2 MB (7.08%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -0,0 +1,27 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        n = len(tokens)
+        l = 0
+        r = n - 1
+        s = 0
+        ans = 0
+        while l <= r:                        
+            print(l, r, s, ans)
+            if power >= tokens[l]:
+                power -= tokens[l]
+                s += 1
+                l += 1
+            elif power < tokens[l] and s >= 1:
+                power += tokens[r]
+                r -= 1
+                s -= 1
+            else:
+                break
+            ans = max(ans, s)
+        print(s)
+        return ans
+                
+                
+            
+        
