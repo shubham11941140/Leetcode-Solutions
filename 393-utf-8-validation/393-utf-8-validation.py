782a200c24f137d2b5540034dbd62c1b3d470433
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-13 12:21:00 +0530

    Time: 135 ms (81.87%), Space: 15 MB (10.09%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -4,31 +4,26 @@ class Solution:
         i = 0
         n = len(c)
         while i < n:
-            s = c[i]
             flag = False
-            # Check 4
-            if i + 3 < n and s[:5] == "11110":
+            if i + 3 < n and c[i][:5] == "11110":
                 if c[i + 1][:2] == "10" and c[i + 2][:2] == "10" and c[i + 3][:2] == "10":
                     flag = True
                     i += 3
                 else:
                     return False
-            # Check 3
-            if i + 2 < n and s[:4] == "1110":
+            if i + 2 < n and c[i][:4] == "1110":
                 if c[i + 1][:2] == "10" and c[i + 2][:2] == "10":
                     flag = True
                     i += 2
                 else:
                     return False
-            # Check 2
-            if i + 1 < n and s[:3] == "110":
+            if i + 1 < n and c[i][:3] == "110":
                 if c[i + 1][:2] == "10":
                     flag = True
                     i += 1
                 else:
-                    return False                
-            # Check 1            
-            if s[:1] == "0":
+                    return False                           
+            if c[i][0] == "0":
                 flag = True
             if not flag:
                 return False
