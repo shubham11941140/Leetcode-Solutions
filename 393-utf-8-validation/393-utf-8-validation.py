d92d2f31a57004f457aff683dee55758c3a0d2fb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-13 12:18:44 +0530

    Time: 136 ms (81.73%), Space: 14.9 MB (15.94%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -8,14 +8,14 @@ class Solution:
             flag = False
             # Check 4
             if i + 3 < n and s[:5] == "11110":
-                if len([1 for y in [c[i + 1], c[i + 2], c[i + 3]] if y[:2] == "10"]) == 3:
+                if c[i + 1][:2] == "10" and c[i + 2][:2] == "10" and c[i + 3][:2] == "10":
                     flag = True
                     i += 3
                 else:
                     return False
             # Check 3
             if i + 2 < n and s[:4] == "1110":
-                if len([1 for y in [c[i + 1], c[i + 2]] if y[:2] == "10"]) == 2:
+                if c[i + 1][:2] == "10" and c[i + 2][:2] == "10":
                     flag = True
                     i += 2
                 else:
