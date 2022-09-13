4f9f4b5624c2e02dc3e1b0bbc66287617bf9ee91
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-13 12:13:58 +0530

    Time: 310 ms (8.19%), Space: 16.8 MB (5.12%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,43 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        b = [bin(i).replace("0b", "") for i in data]        
+        print(b)
+        c = [i.zfill(8) for i in b]
+        print([i[:5] for i in c])
+        
+        print(c)
+        i = 0
+        n = len(c)
+        while i < n:
+            s = c[i]
+            flag = False
+            # Check 4
+            if i + 3 < n and s[:5] == "11110":
+                if len([1 for y in [c[i + 1], c[i + 2], c[i + 3]] if y[:2] == "10"]) == 3:
+                    flag = True
+                    i += 3
+                else:
+                    return False
+            # Check 3
+            if i + 2 < n and s[:4] == "1110":
+                if len([1 for y in [c[i + 1], c[i + 2]] if y[:2] == "10"]) == 2:
+                    flag = True
+                    i += 2
+                else:
+                    return False
+            # Check 2
+            if i + 1 < n and s[:3] == "110":
+                if len([1 for y in [c[i + 1]] if y[:2] == "10"]) == 1:
+                    flag = True
+                    i += 1
+                else:
+                    return False                
+            # Check 1            
+            if s[:1] == "0":
+                flag = True
+                i += 0
+            if not flag:
+                return False
+            i += 1
+        return True
+        
