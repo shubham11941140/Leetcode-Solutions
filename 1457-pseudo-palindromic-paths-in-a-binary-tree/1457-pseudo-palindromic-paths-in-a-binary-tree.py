0190f2280e0b35a7f710bb8a7ba5379cffe62c25
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-14 11:23:40 +0530

    Time: 1118 ms (78.19%), Space: 106.1 MB (5.30%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -14,34 +14,27 @@ class Solution:
             return
         if not root.left and not root.right:
             d[root.val] += 1
-            self.ans.append(d.copy())
+            z = 0
+            f = True
+            for i in d.values():
+                if i % 2 == 1:
+                    z += 1
+                if z > 1:
+                    f = False
+                    break
+            if f:
+                self.ans.append(1)
             return
         d[root.val] += 1
         self.rtl(root.left, d.copy())
         self.rtl(root.right, d.copy())
-        #d[root.val] -= 1
-        
-    def ps(self, a):
-        z = 0
-        for i in a.values():
-            if i % 2 == 1:
-                z += 1
-            if z > 1:
-                return False
-        return True
                     
     # R-to-L paths
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-
-        a = []
         d = defaultdict(int)
         self.ans = []
         self.rtl(root, d)
-        r = 0
-        for i in self.ans:
-            if self.ps(i):
-                r += 1
-        return r
+        return len(self.ans)
             
         
         
