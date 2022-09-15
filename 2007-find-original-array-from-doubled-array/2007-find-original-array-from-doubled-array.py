d30fa67436c3f5cec000aa77a99cb152c56c7e0d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-15 13:05:21 +0530

    Time: 2150 ms (59.59%), Space: 29.9 MB (91.07%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,33 +1,17 @@
 class Solution:
 
     def binary_search(self, arr, low, high, x):
-
-        # Check base case
         if high >= low:
-
             mid = (high + low) // 2
-
-            # If element is present at the middle itself
             if arr[mid] == x:
                 return mid
-
-            # If element is smaller than mid, then it can only
-            # be present in left subarray
             elif arr[mid] > x:
                 return self.binary_search(arr, low, mid - 1, x)
-
-            # Else the element can only be present in right subarray
             else:
-                return self.binary_search(arr, mid + 1, high, x)
-
-        else:
-            # Element is not present in the array
-            return -1    
-    
-    
-    
+                return self.binary_search(arr, mid + 1, high, x)        
+        return -1        
+        
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        # Remove all zeros before
         cz = changed.count(0)
         if cz % 2:
             return []
@@ -40,30 +24,23 @@ class Solution:
             if bs != -1:
                 ans.append(i)
                 del even[bs]
-                #even.remove(2 * i)
             else:
                 return []
         if len(ans) < len(odd):
             return []
-        else: # Equal
+        else:
             if len(even) % 2:
                 return []
             else:
-                # Check if split exists
                 while even:
-                    # Remove smallest
-                    #m = min(even)
                     m = even[0]
                     bs = self.binary_search(even, 0, len(even) - 1, 2 * m)
                     if bs != -1:
                         ans.append(m)
                         del even[bs]
                         del even[0]
-                        #even.remove(m)
-                        #even.remove(2 * m)
                     else:
                         return []
-        print(ans)
         return ans
             
                 
