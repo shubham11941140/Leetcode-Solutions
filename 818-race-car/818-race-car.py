13e98567af09ce8854eaec1d6b788dce9861de5e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-17 12:02:49 +0530

    Time: 70 ms (73.15%), Space: 17.9 MB (21.99%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -2,24 +2,17 @@ from collections import defaultdict, deque
 class Solution:                          
     @lru_cache
     def racecar(self, target: int) -> int:
-        q = []
+        q = deque()
         q.append((0, 1, 0))
         v = defaultdict(lambda: defaultdict(lambda: False))
-        #v = set()
         v[0][1] = True
-        #v.add((0, 1))
         while q:
-            p, s, steps = q.pop(0)
+            p, s, steps = q.popleft()
             if p == target:
                 return steps
-            npa = p + s
-            nsa = s * 2
-            if not v[npa][nsa]:
-                q.append((npa, nsa, steps + 1))  
-                v[npa][nsa] = True
-            if (s > 0 and p + s > target) or (s < 0 and p + s < target):
-                npr = p
-                nsr = (-1 if s >= 0 else 1)
-                if not v[npr][nsr]:
-                    q.append((npr, nsr, steps + 1))
-                    v[npr][nsr] = True
+            if not v[p + s][s * 2]:
+                q.append((p + s, s * 2, steps + 1))  
+                v[p + s][s * 2] = True
+            if (s > 0 and p + s > target) or (s < 0 and p + s < target) and not v[p][(-1 if s >= 0 else 1)]:
+                q.append((p, (-1 if s >= 0 else 1), steps + 1))
+                v[p][(-1 if s >= 0 else 1)] = True
