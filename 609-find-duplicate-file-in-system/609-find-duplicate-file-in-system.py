8238a989cb8d337319f6cf8a114c8f41b9276d63
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-19 13:34:18 +0530

    Time: 401 ms (5.08%), Space: 23.5 MB (95.76%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.py
@@ -1,20 +1,10 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        # Path (content)
         s = dict()
         for i in paths:
             d = i.split()
-            #print(d)
-            di = d[0]
             for j in d[1:]:
                 f, c = j.split('(')
-                print(c, di + '/' + f)
-                fp = di + '/' + f
-                if c in s:
-                    s[c].append(fp)
-                else:
-                    s[c] = [fp]
-        print(s)
+                s[c] = s[c] + [d[0] + '/' + f] if c in s else [d[0] + '/' + f]
         return [i for i in s.values() if len(i) > 1]
-        #return s.values()
         
