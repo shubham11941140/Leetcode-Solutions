5bb633f70e0ab5080266ed7961cda618ae01cf25
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-22 14:21:07 +0530

    Time: 68 ms (23.11%), Space: 14 MB (92.73%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        res = ""
+        for i in range(0, len(s), 2 * k):
+            res += s[i:i + k][::-1] + s[i + k:i + 2 * k]
+        return res        
