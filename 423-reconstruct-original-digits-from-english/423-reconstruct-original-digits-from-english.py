994e5d4075eec7afcb6e48b33302d62a3c646835
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-22 14:27:22 +0530

    Time: 62 ms (79.76%), Space: 14.3 MB (79.00%) - LeetHub

---
+++ 423-reconstruct-original-digits-from-english/423-reconstruct-original-digits-from-english.py
@@ -0,0 +1,16 @@
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        d = {'z': 0, 'w': 2, 'u': 4, 'x': 6, 'g': 8, 'o': 1, 't': 3, 'f': 5, 's': 7, 'i': 9}
+        cnt = [0 for i in range(10)]
+        for c in s:
+            if c in d:
+                cnt[d[c]] += 1
+        cnt[1] -= cnt[0] + cnt[2] + cnt[4]
+        cnt[3] -= cnt[2] + cnt[8]
+        cnt[5] -= cnt[4]
+        cnt[7] -= cnt[6]
+        cnt[9] -= cnt[5] + cnt[6] + cnt[8]
+        res = ""
+        for i in range(10):
+            res += str(i) * cnt[i]
+        return res       
