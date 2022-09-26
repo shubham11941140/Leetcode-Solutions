c1fc6c20f1b0b68c957245aead32f1d784429348
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-26 13:49:42 +0530

    Time: 127 ms (5.34%), Space: 14 MB (94.08%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -4,26 +4,20 @@ class Solution:
         d = {}
         for i in string.ascii_lowercase:
             d[i] = [i]
-
-        # Construct a complete graph
         for i in equations:
-            if i[1:3] == '==':
+            if i[1] == '=':
                 d[i[0]].append(i[3])
                 d[i[3]].append(i[0])
-        print(d)
         for i in equations:
-            if i[1:3] == '!=':
-                a, b = i[0], i[3]
-                q = [a]
+            if i[1] == '!':
+                q = [i[0]]
                 visited = set()
                 while q:
                     curr = q.pop(0)
                     visited.add(curr)
-                    if curr == b:
+                    if curr == i[3]:
                         return False
-                    for i in d[curr]:
-                        if i not in visited:
-                            q.append(i)
+                    q += [j for j in d[curr] if j not in visited]
         return True
 
   
