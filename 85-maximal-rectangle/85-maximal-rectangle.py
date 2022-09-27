55f3a9287231875f374c7a06b4f2b1936b6401bb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-27 11:55:10 +0530

    Time: 9238 ms (5.04%), Space: 15.3 MB (21.83%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    dp[i][j] = dp[i][j - 1] + 1 if j else 1
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    width = dp[i][j]
+                    for k in range(i, -1, -1):
+                        width = min(width, dp[k][j])
+                        res = max(res, width * (i - k + 1))
+        return res       
