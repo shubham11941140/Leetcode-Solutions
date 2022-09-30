35026fd3175c3b861243eed668c2e1ff9632a00b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-09-30 23:36:51 +0530

    Time: 260 ms (48.24%), Space: 18.9 MB (87.25%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,6 +1,6 @@
+from heapq import heappush, heappop
 class Solution:
     def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
-        import heapq
         h = []
         res = []
         i = 0
@@ -9,13 +9,13 @@ class Solution:
             if not h or i < n and buildings[i][0] <= -h[0][1]:
                 x = buildings[i][0]
                 while i < n and buildings[i][0] == x:
-                    heapq.heappush(h, (-buildings[i][2], -buildings[i][1]))
+                    heappush(h, (-buildings[i][2], -buildings[i][1]))
                     i += 1
             else:
                 x = -h[0][1]
                 while h and -h[0][1] <= x:
-                    heapq.heappop(h)
+                    heappop(h)
             y = len(h) and -h[0][0]
             if not res or res[-1][1] != y:
                 res.append([x, y])
-        return res   
+        return res  
