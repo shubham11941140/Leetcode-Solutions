9f3e58155db2ac6aa62cb10d18cb47b79b10941b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-07 23:30:41 +0530

    Time: 1959 ms (61.23%), Space: 14.3 MB (92.31%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class MyCalendarThree:
+
+    def __init__(self):
+        self.d = Counter()
+        
+    def book(self, start: int, end: int) -> int:
+        self.d[start] += 1
+        self.d[end] -= 1
+        active = ans = 0
+        for x in sorted(self.d):
+            active += self.d[x]
+            if active > ans:
+                ans = active
+        return ans        
+
+# Your MyCalendarThree object will be instantiated and called as such:
+# obj = MyCalendarThree()
+# param_1 = obj.book(start,end)
