ffb752d213d42359fc0c004d43a6b6a01f1aa31c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-18 12:32:01 +0530

    Time: 49 ms (91.94%), Space: 14.1 MB (16.06%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return '1'
+        s = self.countAndSay(n - 1)
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while j < len(s) and s[j] == s[i]:
+                j += 1
+            res.append(str(j - i))
+            res.append(s[i])
+            i = j
+        return ''.join(res)        
