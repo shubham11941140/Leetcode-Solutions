f3fbcce50501bf3a0d8de6ad96c2809e35d2637d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-24 12:14:17 +0530

    Time: 268 ms (37.67%), Space: 13.9 MB (90.64%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,15 +1,13 @@
 class Solution:
-    def helper(self, arr, index, s):
-        if index == len(arr):
+    def c(self, idx, s):
+        if idx == self.l:
             return len(s)
         res = 0
-        if len(set(s + arr[index])) == len(s + arr[index]):
-            res = max(res, self.helper(arr, index + 1, s + arr[index]))
-        res = max(res, self.helper(arr, index + 1, s))
+        if len(set(s + self.res[idx])) == len(s + self.res[idx]):
+            res = max(res, self.c(idx + 1, s + self.res[idx]))
+        res = max(res, self.c(idx + 1, s))
         return res    
     def maxLength(self, arr: List[str]) -> int:
-        res = []
-        for i in arr:
-            if len(set(i)) == len(i):
-                res.append(i)
-        return self.helper(res, 0, '')        
+        self.res = [i for i in arr if len(set(i)) == len(i)]
+        self.l = len(self.res)
+        return self.c(0, '')  
