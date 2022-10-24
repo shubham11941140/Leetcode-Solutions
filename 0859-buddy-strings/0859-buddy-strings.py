e4381fb62bfe227549171fcff8900c26f3c29011
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-24 12:19:36 +0530

    Time: 64 ms (47.37%), Space: 14.1 MB (96.60%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+        if A == B:
+            return len(set(A)) < len(A)
+        d = []
+        for i in range(len(A)):
+            if A[i] != B[i]:
+                d.append(i)
+        if len(d) != 2:
+            return False
+        return A[d[0]] == B[d[1]] and A[d[1]] == B[d[0]] 
