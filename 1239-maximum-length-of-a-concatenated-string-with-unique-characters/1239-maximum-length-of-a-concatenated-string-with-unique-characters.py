6da600c44988e64e37a83425b87c243096cc46ae
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-24 12:08:27 +0530

    Time: 130 ms (82.99%), Space: 13.9 MB (64.77%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def helper(self, arr, index, s):
+        if index == len(arr):
+            return len(s)
+        res = 0
+        if len(set(s + arr[index])) == len(s + arr[index]):
+            res = max(res, self.helper(arr, index + 1, s + arr[index]))
+        res = max(res, self.helper(arr, index + 1, s))
+        return res    
+    def maxLength(self, arr: List[str]) -> int:
+        res = []
+        for i in arr:
+            if len(set(i)) == len(i):
+                res.append(i)
+        return self.helper(res, 0, '')        
