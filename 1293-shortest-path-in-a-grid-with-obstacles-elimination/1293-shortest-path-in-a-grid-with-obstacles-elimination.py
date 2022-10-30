84e923126fc6a58867e3ad15b76b51d043754321
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-10-30 11:14:44 +0530

    Time: 1843 ms (22.71%), Space: 21.3 MB (41.13%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        if n == 1 and m == 1:
+            return 0
+        q = deque()
+        q.append((0, 0, 0, 0))
+        visited = set()
+        visited.add((0, 0, 0))
+        while q:
+            x, y, z, dist = q.popleft()
+            for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m:
+                    if grid[nx][ny] == 0:
+                        if (nx, ny, z) not in visited:
+                            if nx == n - 1 and ny == m - 1:
+                                return dist + 1
+                            visited.add((nx, ny, z))
+                            q.append((nx, ny, z, dist + 1))
+                    else:
+                        if z + 1 > k:
+                            continue
+                        if (nx, ny, z + 1) not in visited:
+                            if nx == n - 1 and ny == m - 1:
+                                return dist + 1
+                            visited.add((nx, ny, z + 1))
+                            q.append((nx, ny, z + 1, dist + 1))
+        return -1        
