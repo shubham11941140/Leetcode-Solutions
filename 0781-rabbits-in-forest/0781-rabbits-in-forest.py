3b5521797638cb6468c5d6b97481d7c1078a6df6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-01 14:47:39 +0530

    Time: 90 ms (42.72%), Space: 14 MB (69.93%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.py
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -1,6 +1,3 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        ans = 0
-        for i, j in Counter(answers).items():
-            ans += (j + i) // (i + 1) * (i + 1)
-        return ans       
+        return sum([((j + i) // (i + 1) * (i + 1)) for i, j in Counter(answers).items()])      
