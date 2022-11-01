c2dd933d8dfeb25b2411b7cf1a06dcf22f554a70
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-01 14:46:45 +0530

    Time: 93 ms (33.17%), Space: 13.9 MB (69.93%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.py
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -1,9 +1,6 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        d = defaultdict(int)
-        for i in answers:
-            d[i] += 1
         ans = 0
-        for i, j in d.items():
+        for i, j in Counter(answers).items():
             ans += (j + i) // (i + 1) * (i + 1)
-        return ans        
+        return ans       
