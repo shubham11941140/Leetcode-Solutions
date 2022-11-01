13049827efd31a574cf1ebf7ec39ba7654ba9aa1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-01 14:40:57 +0530

    Time: 482 ms (43.73%), Space: 14.2 MB (98.48%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -13,11 +13,11 @@ class Solution:
                         ans.append(-1)
                         break
                 else:
-                    if x - 1 >= 0 and grid[j][x - 1] == -1:
+                    if x >= 1 and grid[j][x - 1] == -1:
                         x -= 1
                     else:
                         ans.append(-1)
                         break
             else:
                 ans.append(x)
-        return ans        
+        return ans       
