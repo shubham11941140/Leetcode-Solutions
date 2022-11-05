3d9882b0fd24e803bc96ac123c78de72b8104e17
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-05 17:09:30 +0530

    Time: 5827 ms (21.63%), Space: 15.6 MB (63.66%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        res = []
+        Trie = lambda : defaultdict(Trie)
+        root = Trie()
+        def add(s):
+            cur = root
+            for c in s: cur = cur[c]
+            cur['$'] = s
+                
+        for word in words: add(word)
+        m, n = len(board), len(board[0])
+        
+        def dfs(i, j, root):
+            ch = board[i][j]
+            cur = root.get(ch)
+            if not cur: return 
+
+            if '$' in cur: 
+                res.append(cur['$'])
+                del cur['$']
+            
+            board[i][j] = '#'
+            if i<m-1: dfs(i+1, j, cur)
+            if i>0: dfs(i-1, j, cur)
+            if j<n-1: dfs(i, j+1, cur)
+            if j>0: dfs(i, j-1, cur)
+            board[i][j] = ch
+
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+        return res
