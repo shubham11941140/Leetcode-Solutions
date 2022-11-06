e7fbde130d72e6e921bd4044f479c1e39dd7f15b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-06 11:48:52 +0530

    Time: 1951 ms (99.52%), Space: 58.1 MB (87.08%) - LeetHub

--- 1776-car-fleet-ii/1776-car-fleet-ii.py
+++ 1776-car-fleet-ii/1776-car-fleet-ii.py
@@ -3,15 +3,16 @@ class Solution:
         n = len(cars)
         ans = [-1] * n
         st = []
-        for i in range(n - 1, -1, -1):
+        for i in reversed(range(n)):
             pos, speed = cars[i]
             while st and speed <= cars[st[-1]][1]:
                 st.pop()
             while st:
                 j = st[-1]
-                if (cars[j][0] - pos) / (speed - cars[j][1]) <= ans[j] or ans[j] == -1:
-                    ans[i] = (cars[j][0] - pos) / (speed - cars[j][1])
+                r = (cars[j][0] - pos) / (speed - cars[j][1])
+                if r <= ans[j] or ans[j] == -1:
+                    ans[i] = r
                     break
                 st.pop()
             st.append(i)
-        return ans        
+        return ans       
