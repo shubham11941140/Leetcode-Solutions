ffb0d1f6a616e5728fd369cff13b87c9ab8753ff
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-06 11:50:49 +0530

    Time: 2757 ms (12.87%), Space: 36.1 MB (55.64%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        if n == 0:
+            return 0
+        cars = sorted(zip(position, speed))
+        ans = 1
+        t = (target - cars[-1][0]) / cars[-1][1]
+        for i in range(n - 2, -1, -1):
+            nt = (target - cars[i][0]) / cars[i][1]
+            if nt > t:
+                ans += 1
+                t = nt
+        return ans        
