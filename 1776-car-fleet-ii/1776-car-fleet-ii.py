190cf63da6c2290014a697997e3fd475b31a1204
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-06 11:47:02 +0530

    Time: 1954 ms (99.52%), Space: 58.3 MB (26.79%) - LeetHub

---
+++ 1776-car-fleet-ii/1776-car-fleet-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getCollisionTimes(self, cars: List[List[int]]) -> List[float]:
+        n = len(cars)
+        ans = [-1] * n
+        st = []
+        for i in range(n - 1, -1, -1):
+            pos, speed = cars[i]
+            while st and speed <= cars[st[-1]][1]:
+                st.pop()
+            while st:
+                j = st[-1]
+                if (cars[j][0] - pos) / (speed - cars[j][1]) <= ans[j] or ans[j] == -1:
+                    ans[i] = (cars[j][0] - pos) / (speed - cars[j][1])
+                    break
+                st.pop()
+            st.append(i)
+        return ans        
