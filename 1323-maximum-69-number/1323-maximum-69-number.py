d72e13d339c3454cee1837bd55f7bdc4d6874021
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-07 13:21:37 +0530

    Time: 65 ms (15.34%), Space: 13.9 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -7,9 +7,7 @@ class Solution:
             if s[i] == '6':
                 t = s.copy()
                 t[i] = '9'
-                print(t)
                 f.append(int(''.join(t)))
-        print(f)
         return max(f) if f else num
                 
         
