f77f24656de833316145e40cc050e028e2345550
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-12 11:38:07 +0530

    Time: 1485 ms (32.49%), Space: 36 MB (57.44%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,25 @@
+from bisect import insort
+
+class MedianFinder:
+
+    def __init__(self):
+        self.a = []
+        self.l = 0
+        
+
+    def addNum(self, num: int) -> None:
+        insort(self.a, num)
+        self.l += 1
+        #print(self.a)
+
+    def findMedian(self) -> float:
+        if self.l % 2:
+            return self.a[self.l // 2]
+        return (self.a[self.l // 2] + self.a[(self.l // 2) - 1]) / 2
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
