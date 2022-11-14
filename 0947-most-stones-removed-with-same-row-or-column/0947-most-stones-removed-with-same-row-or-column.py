1342829b5ea44864553822eb0b3c3a22b53e53b2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-14 23:07:44 +0530

    Time: 143 ms (99.17%), Space: 14.5 MB (95.20%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        if not n:
+            return 0
+        m1 = defaultdict(list)
+        m2 = defaultdict(list)
+        for i in range(n):
+            m1[stones[i][0]].append(i)
+            m2[stones[i][1]].append(i)
+        ans = 0
+        st = []
+        vis = [0] * n
+        for i in range(n):
+            if vis[i]:
+                continue
+            st.append(i)
+            vis[i] = 1
+            while st:
+                x = st.pop()
+                for y in m1[stones[x][0]]:
+                    if not vis[y]:
+                        vis[y] = 1
+                        st.append(y)
+                for y in m2[stones[x][1]]:
+                    if not vis[y]:
+                        vis[y] = 1
+                        st.append(y)
+            ans += 1
+        return n - ans        
