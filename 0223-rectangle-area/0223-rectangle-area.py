1334b169bbf8261b2c76857dc092dd240ea5df97
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-17 12:31:04 +0530

    Time: 109 ms (50.80%), Space: 14 MB (71.27%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -0,0 +1,6 @@
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        if E >= C or G <= A or F >= D or H <= B:
+            return (C - A) * (D - B) + (G - E) * (H - F)
+        return (C - A) * (D - B) + (G - E) * (H - F) - (min(C, G) - max(A, E)) * (min(D, H) - max(B, F))
+        
