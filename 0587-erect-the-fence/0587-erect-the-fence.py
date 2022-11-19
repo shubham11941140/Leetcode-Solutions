7506fa9af94bf051098e5f331e2e57a5ec8c065e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-19 19:37:55 +0530

    Time: 702 ms (36.15%), Space: 14.7 MB (89.16%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.py
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -1,18 +1,19 @@
 class Solution:
+    def dist(self, p1, p2, p3):
+        return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])    
     def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
         n = len(trees)
         if n < 4:
             return trees
-        trees.sort(key = lambda x: (x[0], x[1]))
-        def ccw(p1, p2, p3):
-            return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
+        #trees.sort(key = lambda x: (x[0], x[1]))
+        trees.sort()
         hull = []
         for i in range(n):
-            while len(hull) >= 2 and ccw(hull[-2], hull[-1], trees[i]) < 0:
+            while len(hull) >= 2 and self.dist(hull[-2], hull[-1], trees[i]) < 0:
                 hull.pop()
             hull.append(trees[i])
         for i in reversed(range(n)):
-            while len(hull) >= 2 and ccw(hull[-2], hull[-1], trees[i]) < 0:
+            while len(hull) >= 2 and self.dist(hull[-2], hull[-1], trees[i]) < 0:
                 hull.pop()
             hull.append(trees[i])
         return list(set(map(tuple, hull)))
