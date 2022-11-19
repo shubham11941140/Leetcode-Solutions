2ba15ef62a821f62c23dfedf82b4e5913fadc46e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-19 19:35:53 +0530

    Time: 731 ms (30.12%), Space: 14.7 MB (57.83%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.py
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -1,19 +1,18 @@
 class Solution:
     def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
-        points = trees.copy()
-        n = len(points)
+        n = len(trees)
         if n < 4:
-            return points
-        points.sort(key=lambda x: (x[0], x[1]))
+            return trees
+        trees.sort(key = lambda x: (x[0], x[1]))
         def ccw(p1, p2, p3):
             return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
         hull = []
         for i in range(n):
-            while len(hull) >= 2 and ccw(hull[-2], hull[-1], points[i]) < 0:
+            while len(hull) >= 2 and ccw(hull[-2], hull[-1], trees[i]) < 0:
                 hull.pop()
-            hull.append(points[i])
+            hull.append(trees[i])
         for i in reversed(range(n)):
-            while len(hull) >= 2 and ccw(hull[-2], hull[-1], points[i]) < 0:
+            while len(hull) >= 2 and ccw(hull[-2], hull[-1], trees[i]) < 0:
                 hull.pop()
-            hull.append(points[i])
+            hull.append(trees[i])
         return list(set(map(tuple, hull)))
