fea37495ce0547ed1fe50e459b14021538507164
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-20 13:41:51 +0530

    Time: 79 ms (94.51%), Space: 15.3 MB (79.69%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        n = len(s)
+        st = []
+        num = 0
+        sign = 1
+        ans = 0
+        for i in range(n):
+            if s[i].isdigit():
+                num = 10 * num + int(s[i])
+            elif s[i] == '+':
+                ans += sign * num
+                num = 0
+                sign = 1
+            elif s[i] == '-':
+                ans += sign * num
+                num = 0
+                sign = -1
+            elif s[i] == '(':
+                st.append(ans)
+                st.append(sign)
+                ans = 0
+                sign = 1
+
+            elif s[i] == ')':
+                ans += sign * num
+                num = 0
+                ans *= st.pop()
+                ans += st.pop()
+        return ans + sign * num           
