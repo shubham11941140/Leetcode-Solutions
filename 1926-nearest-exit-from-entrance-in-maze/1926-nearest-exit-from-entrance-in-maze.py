2ba9a27ed77bb25608e6fcb56bce13df9ca79baa
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-21 17:47:25 +0530

    Time: 1836 ms (60.18%), Space: 14.4 MB (97.75%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        n, m = len(maze), len(maze[0])
+        q = collections.deque([(entrance[0], entrance[1], 0)])
+        maze[entrance[0]][entrance[1]] = '+'
+        while q:
+            x, y, d = q.popleft()
+            if (x == 0 or x == n - 1 or y == 0 or y == m - 1) and (x, y) != (entrance[0], entrance[1]):
+                return d
+            for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] == '.':
+                    maze[nx][ny] = '+'
+                    q.append((nx, ny, d + 1))
+        return -1        
