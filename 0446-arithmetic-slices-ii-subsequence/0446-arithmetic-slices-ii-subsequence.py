fc822dc29ef4e055fed2ec14fa7e19eded06830a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-27 15:59:24 +0530

    Time: 1458 ms (76.33%), Space: 52.5 MB (68.64%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        A = nums.copy()
+        n = len(A)
+        dp = [collections.defaultdict(int) for _ in range(n)]
+        ans = 0
+        for i in range(n):
+            for j in range(i):
+                d = A[i] - A[j]
+                dp[i][d] += 1
+                if d in dp[j]:
+                    dp[i][d] += dp[j][d]
+                    ans += dp[j][d]
+        return ans        
