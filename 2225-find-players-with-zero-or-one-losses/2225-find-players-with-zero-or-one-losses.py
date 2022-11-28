8c3255ced4622698261d1cea05a6423bb7eebdf5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-28 11:08:33 +0530

    Time: 4926 ms (18.86%), Space: 70.3 MB (10.72%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -4,7 +4,5 @@ class Solution:
         for i in matches:
             t += i
         t = set(t)
-        w = Counter([x for [x, y] in matches])
         l = Counter([y for [x, y] in matches])
         return [sorted([i for i in t if i not in l]), sorted([i for i in l if l[i] == 1])]
-    
