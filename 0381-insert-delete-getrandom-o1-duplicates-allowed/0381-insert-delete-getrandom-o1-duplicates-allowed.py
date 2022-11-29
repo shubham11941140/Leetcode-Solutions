1aa038b47ff51686b6d6e93778983d6119c411d7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-11-29 12:25:55 +0530

    Time: 1384 ms (17.38%), Space: 64.3 MB (99.41%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -0,0 +1,28 @@
+import random
+
+class RandomizedCollection:
+
+    def __init__(self):
+        self.a = []        
+        
+    def insert(self, val: int) -> bool:
+        flag = val not in self.a
+        self.a.append(val)
+        return flag
+        
+    def remove(self, val: int) -> bool:
+        if val not in self.a:
+            return False
+        self.a.remove(val)
+        return True      
+
+    def getRandom(self) -> int:
+        #print(self.a)
+        return random.choice(self.a)
+        return self.a[random.randrange(len(self.a))]
+        
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
