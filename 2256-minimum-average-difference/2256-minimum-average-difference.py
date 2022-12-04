3a64e2af90f4948db7ea1f4fb5a2bafec27706fe
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-12-04 22:37:01 +0530

    Time: 3261 ms (9.17%), Space: 25.5 MB (39.77%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,29 +1,16 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        # Find prefix sum
         n = len(nums)
         s = [0] * (n + 1)
         for i in range(n):
             s[i + 1] = s[i] + nums[i]
-        # Find average difference of each index
         ans = 10 ** 9
         idx = -1
         for i in range(n):
-            # Sum of first half
-            l = s[i + 1] - s[0]
-            # Sum of second half
-            r = s[n] - s[i + 1]
-            # average of first half
-            a = l // (i + 1)
-            # average of second half
-            if n - i - 1 > 0:
-                b = r // (n - i - 1)
-            else:
-                b = 0
-            # average difference
+            a = (s[i + 1] - s[0]) // (i + 1)
+            b = (s[n] - s[i + 1]) // (n - i - 1) if n - i - 1 else 0
             d = abs(a - b)
             if d < ans:
                 ans = d
                 idx = i
-        # Return the index
         return idx
