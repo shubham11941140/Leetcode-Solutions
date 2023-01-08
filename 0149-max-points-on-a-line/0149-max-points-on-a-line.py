09176a59ca8a89d18012922576816858cf719927
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-08 16:37:55 +0530

    Time: 65 ms (97.73%), Space: 14 MB (53.74%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,42 +1,23 @@
 class Solution:    
     
-    def equ(self, p1, p2):
-        n = p1[1] - p2[1]
-        d = p1[0] - p2[0]
-        if not d:
-            return 10 ** 15, 10 ** 15
-        a = n / d
-        c =  p1[1] - (a * p1[0])
-        return a, c
-
-    def lies(self, a, c, p):
-        return abs(p[1] - (a * p[0] + c)) < 10 ** (-5)
+    def slope(self, p1, p2):
+        return 'inf' if p1[0] == p2[0] else (p1[1] - p2[1]) / (p1[0] - p2[0])
 
     def maxPoints(self, points: List[List[int]]) -> int:
-        if len(points) < 3:
-            return len(points)
+        n = len(points)
+        if n < 3:
+            return n
         max_points = 0
-        for i in range(len(points)):
+        for i in range(n):
             slope_dict = {}
             same_points = 1
-            for j in range(i + 1, len(points)):
+            for j in range(i + 1, n):
                 if points[i][0] == points[j][0] and points[i][1] == points[j][1]:
                     same_points += 1
                     continue
                 slope = self.slope(points[i], points[j])
-                if slope in slope_dict:
-                    slope_dict[slope] += 1
-                else:
-                    slope_dict[slope] = 1
-            if len(slope_dict) == 0:
-                max_points = max(max_points, same_points)
-            else:
-                max_points = max(max_points, max(slope_dict.values()) + same_points)
+                slope_dict[slope] = slope_dict[slope] + 1 if slope in slope_dict else 1
+            max_points = max(max_points, same_points + max(slope_dict.values()) if slope_dict else same_points)
         return max_points
-
-    def slope(self, p1, p2):
-        if p1[0] == p2[0]:
-            return 'inf'
-        return (p1[1] - p2[1]) / (p1[0] - p2[0])
                         
         
