5c5d87e5c727e097890306e42f372f8c9f56a3eb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-08 16:41:15 +0530

    Time: 77 ms (92.29%), Space: 13.9 MB (75.79%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -2,8 +2,8 @@ class Solution:
     def slope(self, p1, p2):
         return 'inf' if p1[0] == p2[0] else (p1[1] - p2[1]) / (p1[0] - p2[0])
 
-    def maxPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
+    def maxPoints(self, p: List[List[int]]) -> int:
+        n = len(p)
         if n < 3:
             return n
         mp = 0
@@ -11,10 +11,10 @@ class Solution:
             sd = {}
             sp = 1
             for j in range(i + 1, n):
-                if points[i][0] == points[j][0] and points[i][1] == points[j][1]:
+                if p[i][0] == p[j][0] and p[i][1] == p[j][1]:
                     sp += 1
                     continue
-                s = self.slope(points[i], points[j])
+                s = self.slope(p[i], p[j])
                 sd[s] = sd[s] + 1 if s in sd else 1
             mp = max(mp, sp + max(sd.values()) if sd else sp)
         return mp
