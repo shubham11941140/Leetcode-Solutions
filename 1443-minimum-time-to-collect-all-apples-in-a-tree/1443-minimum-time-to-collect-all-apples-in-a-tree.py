ab7119b7fbfe9407a4c9c50072dd93c3adf0c052
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-11 14:54:29 +0530

    Time: 702 ms (85.90%), Space: 51.4 MB (58.97%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,16 +1,17 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        graph = defaultdict(list)
+        self.g = defaultdict(list)
+        self.h = hasApple.copy()
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        return self.dfs(graph, hasApple, 0, -1)
+            self.g[u].append(v)
+            self.g[v].append(u)
+        return self.dfs(0, -1)
 
-    def dfs(self, graph, hasApple, node, parent):
+    def dfs(self, node, parent):
         time = 0
-        for child in graph[node]:
+        for child in self.g[node]:
             if child != parent:
-                time += self.dfs(graph, hasApple, child, node)
-        if (time > 0 or hasApple[node]) and parent != -1:
+                time += self.dfs(child, node)
+        if (time or self.h[node]) and parent != -1:
             time += 2
         return time        
