48649e84ae6413be40f8569a7088498b17b8b694
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-14 19:19:40 +0530

    Time: 33 ms (98.85%), Space: 14 MB (56.90%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,41 +1,31 @@
 class Solution:
     
-    
-    def dfs(self, g, node, visited, cc):
+    def dfs(self, g, node, v, cc):
         for i in g[node]:
-            if i not in visited:
-                visited.add(i)
+            if i not in v:
+                v.add(i)
                 cc.append(i)
-                self.dfs(g, i, visited, cc)
+                self.dfs(g, i, v, cc)
     
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        A = s1
-        B = s2
-        S = baseStr
-        # Make a graph
-        graph = {chr(i): chr(i) for i in range(97, 123)}
         g = collections.defaultdict(list)
-        # Add edges based on equivalence
-        n = len(A)
+        n = len(s1)
         for i in range(n):
-            if A[i] != B[i]:
-                g[A[i]].append(B[i])
-                g[B[i]].append(A[i])
-        print(g)
-        # Find all connected components in the graph
-        visited = set()
-        acc = []
+            if s1[i] != s2[i]:
+                g[s1[i]].append(s2[i])
+                g[s2[i]].append(s1[i])
+        v = set()
+        a = []
         for i in g:
-            if i not in visited:
-                visited.add(i)
+            if i not in v:
+                v.add(i)
                 cc = [i]
-                self.dfs(g, i, visited, cc)
-                acc.append(cc)
-        print(acc)
-        sacc = [sorted(i) for i in acc]
-        for j in S:
-            for i in sacc:
+                self.dfs(g, i, v, cc)
+                a.append(cc)
+        sa = [sorted(i) for i in a]
+        for j in baseStr:
+            for i in sa:
                 if j in i:
-                    S = S.replace(j, i[0])
-        return S
+                    baseStr = baseStr.replace(j, i[0])
+        return baseStr
                 
