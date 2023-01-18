5023a66c64b978238f92a4af1aca982c3de9e385
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-18 14:17:13 +0530

    Time: 543 ms (83.11%), Space: 19 MB (43.34%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,15 +1,11 @@
 class Solution:
+    
+    def kadane(self, nums):
+        cur_sum = max_sum = nums[0]
+        for num in nums[1:]:
+            cur_sum = max(num, cur_sum + num)
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
+
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        # Kadane's algorithm
-        def kadane(nums):
-            cur_sum = max_sum = nums[0]
-            for num in nums[1:]:
-                cur_sum = max(num, cur_sum + num)
-                max_sum = max(max_sum, cur_sum)
-            return max_sum
-
-        # If all elements are negative, return the max element
-        if max(nums) < 0:
-            return max(nums)
-
-        return max(kadane(nums), sum(nums) + kadane([-num for num in nums]))
+        return max(nums) if max(nums) < 0 else max(self.kadane(nums), sum(nums) + self.kadane([-num for num in nums]))
