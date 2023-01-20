c4aa62fef14ba8c0131536703f765b761754b7c1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-20 13:56:28 +0530

    Time: 230 ms (77.03%), Space: 22.4 MB (40.47%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,12 +1,14 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        self.dfs(nums, 0, [], res)
-        return res
-    
-    def dfs(self, nums, index, path, res):
+        self.r = set()
+        self.nums = nums.copy()
+        self.n = len(nums)
+        self.dfs(0, [])
+        return self.r
+
+    def dfs(self, index, path):
         if len(path) > 1:
-            res.add(tuple(path))
-        for i in range(index, len(nums)):
-            if not path or nums[i] >= path[-1]:
-                self.dfs(nums, i + 1, path + [nums[i]], res)        
+            self.r.add(tuple(path))
+        for i in range(index, self.n):
+            if not path or self.nums[i] >= path[-1]:
+                self.dfs(i + 1, path + [self.nums[i]])        
