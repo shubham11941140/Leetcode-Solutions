ae20454035c6f9ab82332db8ee2b488c858d5910
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-01-28 13:55:28 +0530

    Time: 804 ms (16.39%), Space: 19.1 MB (25.42%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -0,0 +1,29 @@
+class SummaryRanges:
+
+    def __init__(self):
+        self.s = []
+        
+    def addNum(self, value: int) -> None:
+        self.s.append(value)     
+
+    def getIntervals(self) -> List[List[int]]:
+        self.s = sorted(list(set(self.s)))
+        start = self.s[0]
+        end = self.s[0]
+        res = []
+        for i in range(1, len(self.s)):
+            if self.s[i] == end + 1:
+                end = self.s[i]
+            else:
+                res.append([start, end])
+                start = self.s[i]
+                end = self.s[i]
+        res.append([start, end])
+        return res
+        
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(value)
+# param_2 = obj.getIntervals()
