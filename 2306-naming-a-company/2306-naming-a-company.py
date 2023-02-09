b0c564e4ce5c542b3c3a87880cfcc4e2a0668900
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-09 23:31:26 +0530

    Time: 845 ms (66.30%), Space: 28.7 MB (43.48%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -1,22 +1,22 @@
 class Solution:      
     def distinctNames(self, ideas : List[str]) -> int:        
-        hashMap = collections.defaultdict(set)
+        h = defaultdict(set)
         for w in ideas:
-            hashMap[w[0]].add(w[1:])
-        
+            h[w[0]].add(w[1:])
+
         res = 0
-        for char1 in hashMap:
-            for char2 in hashMap:
+        for char1 in h:
+            for char2 in h:
                 if char1 == char2:
                     continue
                 intersect = 0
 
-                for w in hashMap[char1]:
-                    if w in hashMap[char2]:
+                for w in h[char1]:
+                    if w in h[char2]:
                         intersect += 1
-                
-                distinct1 = len(hashMap[char1]) - intersect
-                distinct2 = len(hashMap[char2]) - intersect
 
-                res += distinct1*distinct2
+                d1 = len(h[char1]) - intersect
+                d2 = len(h[char2]) - intersect
+
+                res += d1 * d2
         return res
