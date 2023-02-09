75d95cd295d8a986e58af7544be26e9d4aebd262
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-09 23:34:49 +0530

    Time: 886 ms (65.22%), Space: 28.7 MB (43.48%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.py
+++ 2306-naming-a-company/2306-naming-a-company.py
@@ -3,20 +3,10 @@ class Solution:
         h = defaultdict(set)
         for w in ideas:
             h[w[0]].add(w[1:])
-
-        res = 0
-        for char1 in h:
-            for char2 in h:
-                if char1 == char2:
-                    continue
-                intersect = 0
-
-                for w in h[char1]:
-                    if w in h[char2]:
-                        intersect += 1
-
-                d1 = len(h[char1]) - intersect
-                d2 = len(h[char2]) - intersect
-
-                res += d1 * d2
-        return res
+        r = 0
+        for c1 in h:
+            for c2 in h:
+                if c1 != c2:
+                    c = sum([1 for w in h[c1] if w in h[c2]])
+                    r += ((len(h[c1]) - c) * (len(h[c2]) - c))
+        return r
