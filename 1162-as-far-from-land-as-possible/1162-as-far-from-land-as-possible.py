d9ce24f229e9a73112e4d95e6d457fbb696ee84f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-10 13:27:11 +0530

    Time: 532 ms (88.13%), Space: 14.5 MB (80.10%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        queue = deque()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    queue.append((i, j))
+        queue = deque([(i, j) for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == 1])
+        n = len(grid)
+        m = len(grid[0])
         if len(queue) == 0 or len(queue) == len(grid) * len(grid[0]):
             return -1
         ans = 0
@@ -15,7 +13,7 @@ class Solution:
                 for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                     x = i + dx
                     y = j + dy
-                    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 0:
+                    if 0 <= x < n and 0 <= y < m and not grid[x][y]:
                         grid[x][y] = 1
                         queue.append((x, y))
-        return ans - 1        
+        return ans - 1      
