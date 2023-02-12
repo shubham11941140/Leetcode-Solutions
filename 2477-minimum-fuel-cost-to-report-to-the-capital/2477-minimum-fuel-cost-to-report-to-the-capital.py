09ed6035c460dbbb68bb60a57d74baf60e8e3bf4
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-12 18:37:28 +0530

    Time: 2088 ms (51.40%), Space: 159.6 MB (73.83%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,41 +1,20 @@
 class Solution:
-    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
-    	# Build the graph, since it is undirected, we add both directions
-        graph = defaultdict(list)
-        for c1,c2 in roads:
-            graph[c1].append(c2)
-            graph[c2].append(c1)
-        
-        # The total fuel needed, it will be updated in the dfs.
-        res = 0
-
-        # The graph has no cycles, but it is undirected, 
-        # so we need to pass in the paraent node here to make sure it is not traveling backward.
-        def dfs(node,par):
-            nonlocal res
-            # Count how many people arrived at the current node.
-            # Start with 1 because each node initially has one people.
+    def dfs(self, node,par):
             totalPeople = 1
-
-            for nei in graph[node]:
-            	# Make sure to not travel backward.
+            for nei in self.g[node]:
                 if nei != par:
-
-                    people, car = dfs(nei,node)
-                    
-                    # Adding the people arraving the current node
+                    people, car = self.dfs(nei,node)
                     totalPeople += people
-                    
-                    # Add the number of cars going from the neighbor node to the current node to the result
-                    # Since each car traveled on edge, number of cars == number of fuel used
-                    res += car
+                    self.r += car
+            cars = ceil(totalPeople/self.seats)
+            return totalPeople, cars   
 
-            # Based on the number of people arrived at the current node, we can calcualte the cars actually needed.
-            # In other words, we try to rearrange the people in the minimum number of cars possible bease on the seats each car has.
-            cars = ceil(totalPeople/seats)
-            
-            # If this is at a leaf node, totalPeople=1, and cars=1 
-            return totalPeople,cars
-
-        dfs(0,None)
-        return res
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        self.g = defaultdict(list)
+        self.seats = seats
+        for u, v in roads:
+            self.g[u].append(v)
+            self.g[v].append(u)
+        self.r = 0
+        self.dfs(0, None)
+        return self.r
