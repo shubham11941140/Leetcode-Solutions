0a5ebacf46413a51897a8521941f64b73c8f1d77
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-12 18:40:10 +0530

    Time: 1922 ms (91.59%), Space: 159.3 MB (75.39%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,20 +1,19 @@
 class Solution:
-    def dfs(self, node,par):
-            totalPeople = 1
-            for nei in self.g[node]:
-                if nei != par:
-                    people, car = self.dfs(nei,node)
-                    totalPeople += people
-                    self.r += car
-            cars = ceil(totalPeople/self.seats)
-            return totalPeople, cars   
+    def dfs(self, v, par):
+        t = 1
+        for i in self.g[v]:
+            if i != par:
+                p, c = self.dfs(i, v)
+                t += p
+                self.r += c
+        return t, ceil(t / self.s)
 
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         self.g = defaultdict(list)
-        self.seats = seats
+        self.s = seats
+        self.r = 0
         for u, v in roads:
             self.g[u].append(v)
             self.g[v].append(u)
-        self.r = 0
         self.dfs(0, None)
         return self.r
