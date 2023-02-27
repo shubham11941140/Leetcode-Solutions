4d41ec528bc782fd321e95333249f19c2bbbd958
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-02-27 23:07:02 +0530

    Time: 144 ms (24.52%), Space: 14.8 MB (69.97%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -11,18 +11,17 @@ class Node:
 """
 
 class Solution:
+    
+    def dfs(self, grid):
+        n = len(grid)
+        if n == 1:
+            return Node(grid[0][0], True, None, None, None, None)
+        else:
+            a = self.dfs([row[:n//2] for row in grid[:n//2]])
+            b = self.dfs([row[n//2:] for row in grid[:n//2]])
+            c = self.dfs([row[:n//2] for row in grid[n//2:]])
+            d = self.dfs([row[n//2:] for row in grid[n//2:]])
+            return Node(a.val, True, None, None, None, None) if a.isLeaf and b.isLeaf and c.isLeaf and d.isLeaf and a.val == b.val == c.val == d.val else Node(0, False, a, b, c, d) 
+        
     def construct(self, grid: List[List[int]]) -> 'Node':
-        def dfs(grid):
-            n = len(grid)
-            if n == 1:
-                return Node(grid[0][0], True, None, None, None, None)
-            else:
-                a = dfs([row[:n//2] for row in grid[:n//2]])
-                b = dfs([row[n//2:] for row in grid[:n//2]])
-                c = dfs([row[:n//2] for row in grid[n//2:]])
-                d = dfs([row[n//2:] for row in grid[n//2:]])
-                if a.isLeaf and b.isLeaf and c.isLeaf and d.isLeaf and a.val == b.val == c.val == d.val:
-                    return Node(a.val, True, None, None, None, None)
-                else:
-                    return Node(0, False, a, b, c, d)
-        return dfs(grid)        
+        return self.dfs(grid)          
