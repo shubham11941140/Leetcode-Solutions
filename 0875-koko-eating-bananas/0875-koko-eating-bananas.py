d623b1a9289c12b2a4621af58914eed9c61130ce
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-08 13:23:19 +0530

    Time: 889 ms (5.01%), Space: 15.6 MB (16.17%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def satisfy(self, a, k):
+        return sum([ceil(a[i] / k) for i in range(len(a))])                 
+        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 0
+        right = 10 * (sum(piles) + 1)
+        while left <= right:
+            mid = (left + right) // 2            
+            calc = self.satisfy(piles, mid)
+            if calc <= h:
+                if mid > 1 and self.satisfy(piles, mid - 1) <= h:
+                    right = mid
+                else:
+                    return mid
+            elif calc > h:
+                left = mid + 1        
