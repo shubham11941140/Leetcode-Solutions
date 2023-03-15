f40162b842352ba642802e2189c4fb2bf747da63
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-15 13:56:15 +0530

    Time: 43 ms (23.49%), Space: 13.9 MB (61.14%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        queue = [root]
+        while queue[0] is not None:
+            node = queue.pop(0)
+            queue.append(node.left)
+            queue.append(node.right)
+        while queue and queue[0] is None:
+            queue.pop(0)
+        return len(queue) == 0        
