ead422c8dadbbeb834218c0693b2970b8aa36df2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-24 16:17:29 +0530

    Time: 1136 ms (97.71%), Space: 67.6 MB (56.48%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for u, v in connections:
+            graph[u].append(v)
+            graph[v].append(-u)
+        return self.dfs(graph, 0, set())
+    
+    def dfs(self, graph, node, visited):
+        visited.add(node)
+        count = 0
+        for nei in graph[node]:
+            if abs(nei) in visited:
+                continue
+            if nei > 0:
+                count += 1
+            count += self.dfs(graph, abs(nei), visited)
+        return count        
