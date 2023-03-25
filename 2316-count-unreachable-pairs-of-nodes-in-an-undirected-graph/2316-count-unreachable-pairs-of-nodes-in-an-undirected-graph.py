14ea1dd66f39091322f029b2233dcc004e87c1ab
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-25 22:53:24 +0530

    Time: 2201 ms (60.50%), Space: 106.3 MB (29.54%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,26 +1,22 @@
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        self.g = defaultdict(list)
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        # Find the connected components
-        visited = set()
-        components = []
-        for node in range(n):
-            if node not in visited:
-                component = []
-                self.dfs(node, graph, visited, component)
-                components.append(component)
-        #print(components)  
-        l = [len(component) for component in components]
-        c = 0
-        for i in range(len(l)):
-            c += l[i] * (n - l[i])
-        return c // 2       
-    def dfs(self, node, graph, visited, component):
-        visited.add(node)
+            self.g[u].append(v)
+            self.g[v].append(u)
+        self.v = [False] * n
+        comp = []
+        for i in range(n):
+            if not self.v[i]:
+                c = []
+                self.dfs(i, c)
+                comp.append(c)
+        l = [len(c) for c in comp]
+        return sum([i * (n - i) for i in l]) // 2
+
+    def dfs(self, node, component):
+        self.v[node] = True
         component.append(node)
-        for neighbor in graph[node]:
-            if neighbor not in visited:
-                self.dfs(neighbor, graph, visited, component)        
+        for n in self.g[node]:
+            if not self.v[n]:
+                self.dfs(n, component)       
