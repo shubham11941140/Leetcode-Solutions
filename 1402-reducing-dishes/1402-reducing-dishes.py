f5d5d7163149e9f350ffd6adfd8044618bcaff34
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-29 15:06:12 +0530

    Time: 151 ms (30.69%), Space: 14 MB (46.84%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,10 +1,4 @@
 class Solution:
-    def comp(self, a, i, n):
-        val = 0
-        for j in range(i, n):
-            val += a[j] * (j - i + 1)
-        return val
-
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()
         n = len(satisfaction)
