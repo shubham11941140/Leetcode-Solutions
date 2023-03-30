5bac2069e1552f03c417683c80b57b9d61c2b927
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-30 16:42:54 +0530

    Time: 48 ms (83.33%), Space: 15.7 MB (24.38%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.py
+++ 0087-scramble-string/0087-scramble-string.py
@@ -1,11 +1,13 @@
 class Solution:
     @cache
     def isScramble(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2) or sorted(s1) != sorted(s2):
-            return False
-        if len(s1) < 4 or s1 == s2:
+        if s1 == s2:
             return True
-        for i in range(1,len(s1)):
-            if self.isScramble(s1[:i],s2[:i]) and self.isScramble(s1[i:],s2[i:]) or self.isScramble(s1[:i],s2[-i:]) and self.isScramble(s1[i:],s2[:-i]):
+        if sorted(s1) != sorted(s2):
+            return False
+        for i in range(1, len(s1)):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
                 return True
-        return False 
+            if self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i]):
+                return True
+        return False
