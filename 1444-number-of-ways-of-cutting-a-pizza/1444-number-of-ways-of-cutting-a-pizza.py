f96ca9ecdbcc69aac81b2a10f3e02ae8c6b2dcd3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-31 16:03:24 +0530

    Time: 200 ms (83.33%), Space: 15.9 MB (49.86%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -1,10 +1,9 @@
 class Solution:
     def ways(self, pizza: List[str], k: int) -> int:
-        mod = 10**9 + 7
         r, c = len(pizza), len(pizza[0])
         g = [[0] * (c + 1) for _ in range(r + 1)]
-        for i in range(r - 1, -1, -1):
-            for j in range(c - 1, -1, -1):
+        for i in reversed(range(r)):
+            for j in reversed(range(c)):
                 g[i][j] = g[i + 1][j] + g[i][j + 1] - g[i + 1][j + 1] + (pizza[i][j] == 'A')
         @lru_cache(None)
         def dp(i, j, k):
@@ -12,5 +11,5 @@ class Solution:
                 return 0
             if not k:
                 return 1
-            return (sum([dp(x, j, k - 1) for x in range(i + 1, r) if g[i][j] > g[x][j]]) + sum([dp(i, y, k - 1) for y in range(j + 1, c) if g[i][j] > g[i][y]])) % mod
+            return (sum([dp(x, j, k - 1) for x in range(i + 1, r) if g[i][j] > g[x][j]]) + sum([dp(i, y, k - 1) for y in range(j + 1, c) if g[i][j] > g[i][y]])) % (10**9 + 7)
         return dp(0, 0, k - 1)            
