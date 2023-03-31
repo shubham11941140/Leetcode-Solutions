92df26c8ae3d104a5193a66fbe4d86c62572cd28
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-03-31 15:52:50 +0530

    Time: 208 ms (76.02%), Space: 16 MB (49.86%) - LeetHub

---
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.py
@@ -0,0 +1,25 @@
+class Solution:
+    def ways(self, pizza: List[str], k: int) -> int:
+        mod = 10**9 + 7
+        r, c = len(pizza), len(pizza[0])
+        g = [[0] * (c + 1) for _ in range(r + 1)]
+        for i in range(r - 1, -1, -1):
+            for j in range(c - 1, -1, -1):
+                g[i][j] = g[i + 1][j] + g[i][j + 1] - g[i + 1][j + 1] + (pizza[i][j] == 'A')
+        @lru_cache(None)
+        def dp(i, j, k):
+            if g[i][j] == 0:
+                return 0
+            if k == 0:
+                return 1
+            ans = 0
+            for x in range(i + 1, r):
+                if g[i][j] - g[x][j] > 0:
+                    ans += dp(x, j, k - 1)
+                    ans %= mod
+            for y in range(j + 1, c):
+                if g[i][j] - g[i][y] > 0:
+                    ans += dp(i, y, k - 1)
+                    ans %= mod
+            return ans
+        return dp(0, 0, k - 1)        
