89499c324a05c208d999a8be6a87b3ea2e795af0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-05 18:08:44 +0530

    Time: 729 ms (99.55%), Space: 29.5 MB (7.17%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,35 +1,4 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
         return max([ceil(n / (i + 1)) for i, n in enumerate(list(accumulate(nums)))])
-        
-        
-        mn = 0
-        nums = list(accumulate(nums))
-        for i, n in enumerate(nums):
-            mn = max(mn, ceil(n/(i+1)))
-        return mn
-    
-        n = len(nums)
-        lval = min(nums)
-        for i in range(n):
-            nums[i] = nums[i] - lval
-        for _ in range(1000):
-            c = nums.copy()
-            for i in range(1, n):#(n - 1, 0, -1):
-                if nums[i - 1] < nums[i]:
-                    s = nums[i - 1] + nums[i]
-                    if s % 2 == 0:
-                        nums[i - 1] = s // 2
-                        nums[i] = s // 2
-                    else:
-                        nums[i - 1] = s // 2 + 1
-                        nums[i] = s // 2
-            # Check if the first number is maximum
-            if nums[0] == max(nums) and nums.count(max(nums)) == 1:
-                #print(1515151515, _)
-                #print(nums)
-                break
-            if c == nums:
-                #print(_)
-                break
-        return max(nums) + lval
+
