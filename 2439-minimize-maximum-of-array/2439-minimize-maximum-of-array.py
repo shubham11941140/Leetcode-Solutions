f6f546f15b081cb80f4b635df1b8dd3274251209
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-05 18:08:13 +0530

    Time: 733 ms (99.10%), Space: 29.6 MB (7.17%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        
+        return max([ceil(n / (i + 1)) for i, n in enumerate(list(accumulate(nums)))])
         
         
         mn = 0
