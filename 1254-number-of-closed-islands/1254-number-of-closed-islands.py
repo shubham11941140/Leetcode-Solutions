dd62c532aa1f351b2c50f75cba257c5909187557
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-06 16:02:45 +0530

    Time: 128 ms (69.66%), Space: 14.5 MB (61.65%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,21 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+                return False
+            if grid[i][j] == 1:
+                return True
+            grid[i][j] = 1
+            up = dfs(i - 1, j)
+            down = dfs(i + 1, j)
+            left = dfs(i, j - 1)
+            right = dfs(i, j + 1)
+            return up and down and left and right
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    if dfs(i, j):
+                        count += 1
+        return count        
