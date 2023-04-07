d220b9d4c994de2062341a85de41a466b980e04a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-07 18:42:29 +0530

    Time: 624 ms (89.70%), Space: 74.7 MB (69.38%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        A = grid.copy()
+        def dfs(i, j):
+            if i < 0 or j < 0 or i == len(A) or j == len(A[0]):
+                return
+            if A[i][j] == 0:
+                return
+            A[i][j] = 0
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        for i in range(len(A)):
+            dfs(i, 0)
+            dfs(i, len(A[0]) - 1)
+
+        for j in range(len(A[0])):
+            dfs(0, j)
+            dfs(len(A) - 1, j)
+
+        return sum(sum(row) for row in A)        
