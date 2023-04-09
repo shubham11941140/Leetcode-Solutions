776a6fc57973b9f5f4a1a10503d59326135ba213
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-09 15:37:38 +0530

    Time: 3347 ms (11.91%), Space: 81.1 MB (83.93%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -1,12 +1,12 @@
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
         n = len(colors)
-        graph = collections.defaultdict(list)
+        graph = defaultdict(list)
         indegree = [0] * n
         for u, v in edges:
             graph[u].append(v)
             indegree[v] += 1
-        q = collections.deque([i for i in range(n) if indegree[i] == 0])
+        q = deque([i for i in range(n) if not indegree[i]])
         dp = [[0] * 26 for _ in range(n)]
         for i in range(n):
             dp[i][ord(colors[i]) - ord('a')] = 1
@@ -16,8 +16,6 @@ class Solution:
                 for i in range(26):
                     dp[v][i] = max(dp[v][i], dp[u][i] + (i == ord(colors[v]) - ord('a')))
                 indegree[v] -= 1
-                if indegree[v] == 0:
+                if not indegree[v]:
                     q.append(v)
-        if any(indegree):
-            return -1
-        return max(max(dp[i]) for i in range(n))       
+        return -1 if any(indegree) else max(max(dp[i]) for i in range(n))      
