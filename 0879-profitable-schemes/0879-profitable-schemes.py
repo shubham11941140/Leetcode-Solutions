4fad513dc9cbead52d550e73f494121b9a5a5dc1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-21 15:56:54 +0530

    Time: 2746 ms (47.62%), Space: 57 MB (33.33%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -6,8 +6,5 @@ class Solution:
         for i in range(1, len(group) + 1):
             for j in range(n + 1):
                 for k in range(minProfit + 1):
-                    if j < group[i - 1]:
-                        dp[i][j][k] = dp[i - 1][j][k]
-                    else:
-                        dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - group[i - 1]][max(0, k - profit[i - 1])]
+                    dp[i][j][k] = dp[i - 1][j][k] + (0 if j < group[i - 1] else dp[i - 1][j - group[i - 1]][max(0, k - profit[i - 1])])
         return dp[-1][-1][-1] % (10**9 + 7)
