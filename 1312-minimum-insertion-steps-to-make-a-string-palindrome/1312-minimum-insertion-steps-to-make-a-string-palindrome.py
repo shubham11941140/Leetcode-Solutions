7876a8bd4a84d5a3d87f487716729190454408dc
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-22 15:40:27 +0530

    Time: 1232 ms (41.99%), Space: 16 MB (67.55%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+        # reverse the string
+        t = s[::-1]
+        # find the length of the longest common subsequence
+        n = len(s)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        # return the minimum number of insertions
+        return n - dp[n][n]        
