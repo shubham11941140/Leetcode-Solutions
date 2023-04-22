15c4f2b7838dd90b1d76627275020127a9902297
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-22 15:41:10 +0530

    Time: 1245 ms (38.75%), Space: 16 MB (75.05%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -7,9 +7,5 @@ class Solution:
         dp = [[0] * (n + 1) for _ in range(n + 1)]
         for i in range(1, n + 1):
             for j in range(1, n + 1):
-                if s[i - 1] == t[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-        # return the minimum number of insertions
+                dp[i][j] = dp[i - 1][j - 1] + 1 if s[i - 1] == t[j - 1] else max(dp[i - 1][j], dp[i][j - 1])
         return n - dp[n][n]        
