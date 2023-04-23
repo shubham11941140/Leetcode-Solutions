5a1e45850e2a5df9c1b10e1982283968740c9761
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-23 15:14:00 +0530

    Time: 1418 ms (82.19%), Space: 17.9 MB (95.89%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberOfArrays(self, s: str, k: int) -> int:
+        # dp[i] means the number of ways to restore s[i:]
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
+        mod = 10**9 + 7
+        
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '0':
+                continue
+            num = 0
+            for j in range(i, len(s)):
+                num = num * 10 + int(s[j])
+                if num > k:
+                    break
+                dp[i] += dp[j + 1]
+                dp[i] %= mod
+        
+        return dp[0]        
