fcd07861c5e24d6ed288f6aacf552971b2ead995
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-27 14:43:16 +0530

    Time: 43 ms (8.42%), Space: 16.1 MB (34.09%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.py
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -1,15 +1,3 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        # initialize a variable count to 0
-        count = 0
-        # initialize a variable i to 1
-        i = 1
-        # while i*i is less than or equal to n
-        while i*i <= n:
-            # increment count by 1
-            count += 1
-            # increment i by 1
-            i += 1
-        # return the final value of count
-        return int(sqrt(n))
-        return count      
+        return int(sqrt(n))   
