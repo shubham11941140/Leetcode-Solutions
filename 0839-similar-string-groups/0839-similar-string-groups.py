abb4803ba5cd6256cd6a99281e6499293c2e9288
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-04-29 00:02:07 +0530

    Time: 4355 ms (5.24%), Space: 16.6 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,7 +1,6 @@
 class Solution:
     
     def dfs(self, strs, i, visited):
-        #add current string to memo
         visited.add(i)
         for j in range(len(strs)):
             if self.isSimilar(strs[i], strs[j]) and j not in visited:
@@ -9,17 +8,13 @@ class Solution:
                 
     # calculate the similarity of two strings            
     def isSimilar(self, str1, str2):
-        diff_count = 0
-        for i in range(len(str1)):
-            if str1[i] != str2[i]:
-                diff_count += 1
-        return diff_count <= 2                
+        return sum([1 for i in range(len(str1)) if str1[i] != str2[i]]) <= 2    
+    
     def numSimilarGroups(self, strs: List[str]) -> int:
         visited = set()
         group = 0
         for i in range(len(strs)):
             if i not in visited:
                 group += 1
-                self.dfs(strs, i ,visited)
-        
+                self.dfs(strs, i ,visited)        
         return group        
