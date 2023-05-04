17e1b698ad2ab970807822487119e5c839e75c47
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-04 18:59:55 +0530

    Time: 72 ms (45.87%), Space: 16.7 MB (5.50%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,21 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        l = len(senate)
+        d = collections.deque()
+        r = collections.deque()
+        for i, c in enumerate(senate):
+            if (c == 'R'):
+                r.append(i)
+            else:
+                d.append(i)
+        while (r and d):
+            p = r.popleft()
+            q = d.popleft()
+            if (p < q):
+                r.append(p + l)
+            else:
+                d.append(q + l)
+        if (r):
+            return 'Radiant'
+        else:
+            return 'Dire'        
