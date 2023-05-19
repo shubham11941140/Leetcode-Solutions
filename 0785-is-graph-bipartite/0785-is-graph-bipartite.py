df7406da5a747413e382a5a8b996662d011554d3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-19 13:15:07 +0530

    Time: 194 ms (26.50%), Space: 16.8 MB (21.33%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,23 @@
+class Solution:
+    
+    def dfs(self, graph, visited, node):
+        for i in graph[node]:                 
+            if visited[i] == -1:
+                visited[i] = 1 - visited[node]
+                if not self.dfs(graph, visited, i):
+                    return False
+            elif visited[i] != -1 and visited[node] == visited[i]:
+                return False
+        return True          
+        
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        visited = [-1] * n    
+        for i in range(n):
+            if visited[i] == -1:
+                visited[i] = 1
+                f = self.dfs(graph, visited, i)                
+                if not f:
+                    return False       
+        return True
+        
