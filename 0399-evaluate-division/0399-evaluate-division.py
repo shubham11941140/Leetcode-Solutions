42a016ce54feffd0132ae0d7375dceaa78c31783
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-20 09:35:51 +0530

    Time: 49 ms (18.79%), Space: 16.5 MB (8.30%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,51 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # Solve using connected componenets of a graph
+        d = {}
+        k = 0
+        for i in equations:
+            for j in i:
+                if j not in d:
+                    d[j] = k
+                    k += 1
+        adj = [[] for i in range(k)]
+        for i in range(len(values)):
+            a, b = equations[i]
+            adj[d[a]].append([d[b], values[i]])
+            adj[d[b]].append([d[a], 1/values[i]])
+        ans = []
+        for a, b in queries:
+            if a not in d or b not in d:
+                ans.append(-1)
+                continue
+            elif a == b:
+                ans.append(1)
+                continue
+            else:
+                q = []
+                path = []
+                src = d[a]
+                dst = d[b]
+                path.append(src)
+                q.append(path.copy())
+                flag = False
+                while q:
+                    path = q.pop(0)
+                    last = path[- 1]
+                    if last == dst:
+                        flag = True
+                        break
+                    for i, j in adj[last]:
+                        if i not in path:
+                            q.append(path.copy() + [i])
+                if not flag:
+                    ans.append(-1)
+                    continue
+                f = 1
+                for i in range(len(path) - 1):
+                    for v, w in adj[path[i]]:
+                        if v == path[i + 1]:
+                            f *= w
+                ans.append(f)
+        return ans
+        
