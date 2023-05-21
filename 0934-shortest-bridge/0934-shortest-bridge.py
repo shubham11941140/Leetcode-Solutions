1bc637e1008ec634f16559c06d71ef949e4a2525
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-21 13:23:51 +0530

    Time: 9967 ms (5.01%), Space: 168.5 MB (5.00%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -0,0 +1,47 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])        
+        
+        def dfs(v, c, i, j):
+            v[i][j] = True
+            grid[i][j] = c
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= x < n and 0 <= y < m and not v[x][y] and grid[x][y]:
+                    dfs(v, c, x, y)
+            
+        
+
+        c = 2
+        v = [[False for i in range(m)] for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if not v[i][j] and grid[i][j]:
+                    dfs(v, c, i, j)
+                    c += 1
+        #print(grid)
+        
+        is1 = []
+        is2 = []
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    is1.append((i, j))
+                if grid[i][j] == 3:
+                    is2.append((i, j))
+        #print(is1, is2)
+        
+        m = []
+        for x in is1:
+            for y in is2:
+                s = abs(x[0] - y[0]) + abs(x[1] - y[1])
+                #print(s)
+                m.append(s)
+        #print(m)
+        return min(m) - 1
+        return 0
+                    
+                
+            
+
+      
