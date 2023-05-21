cda343e6ce9fa2c2d6505098f21b135888a97e83
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-21 13:45:34 +0530

    Time: 1014 ms (7.02%), Space: 20.9 MB (17.18%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,44 +1,46 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = len(grid[0])        
-        
-        def dfs(v, c, i, j):
+        m = len(grid[0])   
+        c = 2
+        v = [[False for i in range(m)] for i in range(n)]        
+        def dfs(i, j):
             v[i][j] = True
             grid[i][j] = c
             for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
                 if 0 <= x < n and 0 <= y < m and not v[x][y] and grid[x][y]:
-                    dfs(v, c, x, y)
-            
-        
-
-        c = 2
-        v = [[False for i in range(m)] for i in range(n)]
+                    dfs(x, y)    
+        def nei(i, j, c):
+            t = 0
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= x < n and 0 <= y < m and grid[x][y] == c:
+                    t += 1
+            return t
+                    
         for i in range(n):
             for j in range(m):
                 if not v[i][j] and grid[i][j]:
-                    dfs(v, c, i, j)
-                    c += 1
-        #print(grid)
-        
+                    dfs(i, j)
+                    c += 1        
         is1 = []
         is2 = []
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == 2:
+                if grid[i][j] == 2 and nei(i, j, 2) <= 3:
                     is1.append((i, j))
-                if grid[i][j] == 3:
+                if grid[i][j] == 3 and nei(i, j, 3) <= 3:
                     is2.append((i, j))
-        #print(is1, is2)
+        print(len(is1), len(is2))
         
-        m = []
+        m = 10 ** 5
+        d = []
         for x in is1:
             for y in is2:
                 s = abs(x[0] - y[0]) + abs(x[1] - y[1])
-                #print(s)
-                m.append(s)
-        #print(m)
-        return min(m) - 1
+                d.append(s)
+                m = min(m, s)
+        return m - 1
+        
         return 0
                     
                 
