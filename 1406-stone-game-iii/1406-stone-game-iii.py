18f26810f097d710aca8cd11ce37986775ae3eef
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-27 20:37:55 +0530

    Time: 2764 ms (75.34%), Space: 21.3 MB (53.36%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,18 +1,14 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
         n = len(stoneValue)
-        dp = [0] * (n + 1)  # dp[i] represents the maximum score difference starting from index i
-
-        for i in range(n - 1, -1, -1):
+        dp = [0] * (n + 1)
+        for i in reversed(range(n)):
             take = 0
-            dp[i] = float('-inf')  # Initialize dp[i] to negative infinity
-
-            # Try taking 1, 2, or 3 stones and calculate the maximum score difference
+            dp[i] = float('-inf')
             for j in range(3):
                 if i + j < n:
                     take += stoneValue[i + j]
                     dp[i] = max(dp[i], take - dp[i + j + 1])
-
         if dp[0] > 0:
             return "Alice"
         elif dp[0] < 0:
