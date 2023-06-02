8f14ee212b1ba362ac9cc9e4f677fd5d75f918e8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-03 00:02:01 +0530

    Time: 6432 ms (7.64%), Space: 16.5 MB (55.17%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,27 +1,21 @@
 class Solution:
-    
-    def dist(self, cod1, cod2):
-        return math.sqrt(math.pow(cod1[0] - cod2[0], 2) + math.pow(cod1[1] - cod2[1], 2))    
-    
+   
     def dfs(self, curr, d):
         d.add(curr)
         for i in self.u: 
             if i not in d:
-                if self.dist(curr, i) <= curr[2]:  
+                if sqrt((curr[0] - i[0]) ** 2 + (curr[1] - i[1]) ** 2) <= curr[2]:
                     self.dfs(i, d)
     
     def maximumDetonation(self, bombs: List[List[int]]) -> int:   
         nb = [(i[0], i[1], i[2]) for i in bombs]
         self.u = nb.copy()
         c = Counter(nb)
-        #print(nb)
         m = []
         for i in nb:
             d = set()
             self.dfs(i, d)
-            s = len(d)
-            s += sum([c[i] - 1 for i in d])
-            m.append(s)
+            m.append(sum([c[i] for i in d]))
         return max(m)
 
        
