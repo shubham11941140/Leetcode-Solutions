a57c8abf8c7087dffe24db81f2ea650e6539eb7a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-10 20:47:56 +0530

    Time: 68 ms (9.16%), Space: 16.4 MB (29.77%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -3,23 +3,25 @@ class Solution:
     def calc(self, m, e):
         return ((m * (m - 1)) // 2) + e - m + 1 if e >= m - 1 else ((m * (m - 1)) // 2) - (((m - e - 1) * (m - e)) // 2)
                
-    def help(self, n, idx, m):               
-        return self.calc(m, idx) + self.calc(m, n - 1 - idx) + m
+    def help(self, m):               
+        return self.calc(m, self.i) + self.calc(m, self.n - 1 - self.i) + m
 
     def maxValue(self, n: int, index: int, maxSum: int) -> int:
         l = 1
         r = maxSum
+        self.n = n
+        self.i = index
         while l <= r:
             if r - l <= 100:
                 for i in reversed(range(l, r + 1)):
-                    if self.help(n, index, i) <= maxSum:
+                    if self.help(i) <= maxSum:
                         return i
             m = (l + r) // 2
-            s = self.help(n, index, m)
+            s = self.help(m)
             if s > maxSum:
                 r = m
             if s <= maxSum:
-                if self.help(n, index, m + 1) > maxSum:
+                if self.help(m + 1) > maxSum:
                     return m                
                 l = m
         return 0
