a8c19cfb2c007da5ca07bda5a96a21438d615351
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-13 10:55:41 +0530

    Time: 3745 ms (5.04%), Space: 21.8 MB (26.88%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -2,10 +2,7 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         n = len(grid)
         c = 0
-        for i in grid:
-            for k in range(n):
-                if i == [grid[j][k] for j in range(n)]:
-                    c += 1
-        return c
+        l = len([1 for i in grid for k in range(n) if i == [grid[j][k] for j in range(n)]])
+        return l
                     
         
