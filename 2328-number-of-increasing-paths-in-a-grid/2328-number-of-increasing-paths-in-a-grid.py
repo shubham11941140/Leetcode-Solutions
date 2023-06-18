c51587184c164c3c029de40381d8239bb595ea23
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-18 17:58:23 +0530

    Time: 2507 ms (41.75%), Space: 222.2 MB (5.34%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -2,17 +2,13 @@ class Solution:
     def countPaths(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        mod = 10 ** 9 + 7
-
         @lru_cache(None)
         def count(row, col):
             res = 1
             for dx, dy in [[row, col + 1], [row, col - 1], [row + 1, col], [row - 1, col]]:
                 if 0 <= dx < rows and 0 <= dy < cols and grid[dx][dy] > grid[row][col]:
-                    res += count(dx, dy) % mod
+                    res += count(dx, dy) % (10 ** 9 + 7)
             return res
-
-        count_values = []
-        return sum([count(i, j) for i in range(rows) for j in range(cols)]) % mod
+        return sum([count(i, j) for i in range(rows) for j in range(cols)]) % (10 ** 9 + 7)
 
       
