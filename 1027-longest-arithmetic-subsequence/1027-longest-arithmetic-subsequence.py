b155ec5f28c4de6bf1d98eb5c9b32590e0d47dda
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-23 10:25:01 +0530

    Time: 2896 ms (76.46%), Space: 33.9 MB (77.03%) - LeetHub

---
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [{} for i in range(n)]
+        res = 0
+        for i in range(n):
+            for j in range(i):
+                d = nums[i] - nums[j]
+                if d in dp[j]:
+                    dp[i][d] = dp[j][d] + 1
+                else:
+                    dp[i][d] = 2
+                res = max(res, dp[i][d])
+        return res
+       
