976e179e6a3b1bd7fbcde9271b446bd6f92ca205
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-25 12:24:54 +0530

    Time: 2081 ms (57.14%), Space: 41.5 MB (9.02%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.py
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.py
@@ -1,14 +1,14 @@
 class Solution:
     def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
         @lru_cache(None)
-        def dfs(curr: int, remaining_fuel: int) -> int:
-            if remaining_fuel < 0:
+        def dfs(curr: int, rem: int) -> int:
+            if rem < 0:
                 return 0
             else:
                 result = int(curr == finish)
             for loc in range(len(locations)):                    
                 if curr != loc:
                     cost = abs(locations[curr] - locations[loc])
-                    result += dfs(loc, remaining_fuel - cost)
+                    result += dfs(loc, rem - cost)
             return result        
         return dfs(start, fuel) % (10 ** 9 + 7)   
