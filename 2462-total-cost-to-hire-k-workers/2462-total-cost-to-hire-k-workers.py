4b980ab0aea1a8ec459c69deb179423575d42a59
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-26 12:49:44 +0530

    Time: 1020 ms (19.46%), Space: 30.4 MB (27.52%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -11,25 +11,25 @@ class Solution:
             l += 1
             r -= 1
             j -= 1
-        total_cost = 0
+        t = 0
         for i in range(k):
             if not heap1:
                 cost, idx = heappop(heap2)
                 if l <= r:                    
                     heappush(heap2, (costs[r], r))
                     r -= 1
-                total_cost += cost   
+                t += cost   
                 continue
             if not heap2 or heap1[0] <= heap2[0]:
                 cost, idx = heappop(heap1)
                 if l <= r:                    
                     heappush(heap1, (costs[l], l))
                     l += 1
-                total_cost += cost
+                t += cost
             else:
                 cost, idx = heapq.heappop(heap2)
                 if l <= r:                    
                     heappush(heap2, (costs[r], r))
                     r -= 1
-                total_cost += cost
-        return total_cost        
+                t += cost
+        return t      
