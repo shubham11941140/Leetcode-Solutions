adea6c1c7eb3bf42f326b62065a6fbf8ad2ceab6
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-06-26 12:49:24 +0530

    Time: 999 ms (24.70%), Space: 31.9 MB (11.01%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -1,45 +1,35 @@
 class Solution:
-    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:  
-        
-        
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:                 
         heap1 = []
         heap2 = []
         l, r = 0, len(costs) - 1
         j = candidates
         while l <= r and j:
-            heapq.heappush(heap1, (costs[l], l))
+            heappush(heap1, (costs[l], l))
             if l != r:
-                heapq.heappush(heap2, (costs[r], r))
+                heappush(heap2, (costs[r], r))
             l += 1
             r -= 1
             j -= 1
-        #print(l, r, j)
-        #print(heap1, heap2)
-
-        min_heap = []
         total_cost = 0
         for i in range(k):
             if not heap1:
-                cost, idx = heapq.heappop(heap2)
+                cost, idx = heappop(heap2)
                 if l <= r:                    
-                    heapq.heappush(heap2, (costs[r], r))
+                    heappush(heap2, (costs[r], r))
                     r -= 1
                 total_cost += cost   
                 continue
             if not heap2 or heap1[0] <= heap2[0]:
-                cost, idx = heapq.heappop(heap1)
+                cost, idx = heappop(heap1)
                 if l <= r:                    
-                    heapq.heappush(heap1, (costs[l], l))
+                    heappush(heap1, (costs[l], l))
                     l += 1
                 total_cost += cost
             else:
                 cost, idx = heapq.heappop(heap2)
                 if l <= r:                    
-                    heapq.heappush(heap2, (costs[r], r))
+                    heappush(heap2, (costs[r], r))
                     r -= 1
                 total_cost += cost
-            print(total_cost)
-
-
-
         return total_cost        
