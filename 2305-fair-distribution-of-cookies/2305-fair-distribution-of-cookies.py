a0c4263ceac256b675d2c5c8a64b4b69cb7deafb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-01 11:39:38 +0530

    Time: 52 ms (93.28%), Space: 16.4 MB (51.49%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        n = len(cookies)
+        self.ans = math.inf
+        
+        def dfs(idx):
+            if idx == n:
+                self.ans = min(self.ans,max(children))
+                return
+            
+            seen = set()
+            for i in range(k):
+                if children[i] in seen:
+                    continue
+                seen.add(children[i])
+                children[i] += cookies[idx]
+                if children[i] <self.ans:
+                    dfs(idx+1)
+                children[i] -= cookies[idx]
+                
+        children = [0 for i in range(k)]
+        dfs(0)
+        return self.ans        
