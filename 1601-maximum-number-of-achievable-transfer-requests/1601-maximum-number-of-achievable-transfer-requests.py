61a65869011dc0d8d41d9fe0359883c77189b5e9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-02 09:57:00 +0530

    Time: 3033 ms (22.33%), Space: 16.2 MB (95.15%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -1,33 +1,30 @@
 class Solution:
     
-    def sub(self, req, idx, curr):
-        if idx == self.r:
-            self.a.append(curr)
-            return
-        self.sub(req, idx + 1, curr + [req[idx]])
-        self.sub(req, idx + 1, curr)
     
-    def satisfy(self, n, a):
-        b = [0] * n
+    def satisfy(self, a):
+        b = [0] * self.n
         for i in a:
             b[i[0]] -= 1
             b[i[1]] += 1
-        if b.count(0) == n:
+        if b.count(0) == self.n:
             return len(a)
-        return 0
-                              
+        return 0    
+    
+    def sub(self, req, idx, curr):
+        if idx == self.r:
+            c = self.satisfy(curr)
+            self.m = max(self.m, c)
+            return
+        self.sub(req, idx + 1, curr + [req[idx]])
+        self.sub(req, idx + 1, curr)
+                                  
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        self.n = n
         self.r = len(requests)
         self.a = []
+        self.m = 0
         self.sub(requests, 0, [])
-        #print(self.a)
-        print(len(self.a))
-        m = 0
-        for i in self.a:
-            # Check satisfy
-            c = self.satisfy(n, i)            
-            m = max(m, c)
-        return m
+        return self.m
             
             
         
