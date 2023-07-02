55bb869b529c495178b1c50b09ca1f59c5959ea5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-02 09:54:09 +0530

    Time: 3388 ms (22.33%), Space: 25.9 MB (18.45%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,34 @@
+class Solution:
+    
+    def sub(self, req, idx, curr):
+        if idx == self.r:
+            self.a.append(curr)
+            return
+        self.sub(req, idx + 1, curr + [req[idx]])
+        self.sub(req, idx + 1, curr)
+    
+    def satisfy(self, n, a):
+        b = [0] * n
+        for i in a:
+            b[i[0]] -= 1
+            b[i[1]] += 1
+        if b.count(0) == n:
+            return len(a)
+        return 0
+                              
+    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
+        self.r = len(requests)
+        self.a = []
+        self.sub(requests, 0, [])
+        #print(self.a)
+        print(len(self.a))
+        m = 0
+        for i in self.a:
+            # Check satisfy
+            c = self.satisfy(n, i)            
+            m = max(m, c)
+        return m
+            
+            
+        
+   
