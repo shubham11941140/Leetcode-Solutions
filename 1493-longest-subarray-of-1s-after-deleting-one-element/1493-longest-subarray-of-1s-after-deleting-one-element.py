e4f435a1bd4d83e9fdcb2a2d368512289446d8f2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-05 09:25:30 +0530

    Time: 345 ms (95.40%), Space: 19.2 MB (11.96%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -3,11 +3,8 @@ class Solution:
         n = len(nums)
         z = [i for i in range(n) if not nums[i]]
         lz = len(z)
-        if not z:
+        if not z or lz == 1:
             return n - 1
-        elif lz == 1:
-            return n - 1
-        print(n, z)
         ls = 0
         for i in range(lz):
             if i == 0 and i < lz - 1:
@@ -18,7 +15,6 @@ class Solution:
                 ls = max(ls, ss)
             if i > 0 and i == lz - 1:
                 ss = (z[i] - z[i - 1] - 1) + (n - z[i] - 1)
-                ls = max(ls, ss)
-            
+                ls = max(ls, ss)            
         return ls
         
