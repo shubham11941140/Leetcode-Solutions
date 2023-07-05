4663cd35d39145e5f509323dece1a4e40227e9eb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-05 09:26:31 +0530

    Time: 328 ms (99.83%), Space: 18.7 MB (93.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -7,14 +7,12 @@ class Solution:
             return n - 1
         ls = 0
         for i in range(lz):
-            if i == 0 and i < lz - 1:
+            if i == 0:
                 ss = (z[i + 1] - z[i] - 1) + z[i]
-                ls = max(ls, ss)
             if 0 < i < lz - 1:
                 ss = (z[i] - z[i - 1] - 1) + (z[i + 1] - z[i] - 1) 
-                ls = max(ls, ss)
             if i > 0 and i == lz - 1:
                 ss = (z[i] - z[i - 1] - 1) + (n - z[i] - 1)
-                ls = max(ls, ss)            
+            ls = max(ls, ss)            
         return ls
         
