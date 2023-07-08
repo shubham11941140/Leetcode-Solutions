f8ac863475fdade4f132b70068cb49f4205df127
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-08 11:36:57 +0530

    Time: 751 ms (66.37%), Space: 30.1 MB (27.43%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -3,8 +3,6 @@ class Solution:
         length = len(weights)
         if k in [1, length]:
             return 0
-        answer = 0
-        ans1 = 0
         res1 = sorted([(weights[i] + weights[i + 1]) for i in range(length - 1)], reverse = True)
         result = res1[::-1]
         return sum([(res1[i] - result[i]) for i in range(k - 1)])
