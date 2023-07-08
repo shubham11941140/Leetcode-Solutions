efcc0d4a68c532f73a0b40b2940a8572903f079f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-08 11:32:37 +0530

    Time: 884 ms (25.66%), Space: 30.1 MB (27.43%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -0,0 +1,19 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        length=len(weights)
+        if k == 1 or k == length:
+            return 0
+        answer = 0
+        ans1 = 0
+        k -= 1
+        res1 = []
+        for i in range(length-1):
+            res1.append(weights[i]+weights[i+1])
+        result = res1.copy()
+        res1.sort(reverse=True)
+        result.sort()
+        for i in range(k):
+            ans1 += res1[i]
+        for i in range(k):
+            answer += result[i]
+        return ans1 - answer        
