3bc46ac12d6527783a3c0b7c0d9aff4dd410ed67
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-22 13:07:39 +0530

    Time: 336 ms (38.74%), Space: 25.9 MB (22.52%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -0,0 +1,27 @@
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        # Possible moves of a knight
+        moves = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+
+        def is_valid(x, y):
+            return 0 <= x < n and 0 <= y < n
+
+        memo = {}
+
+        def dp(k, x, y):
+            if not is_valid(x, y):
+                return 0
+            if k == 0:
+                return 1
+
+            if (k, x, y) in memo:
+                return memo[(k, x, y)]
+
+            total_prob = 0
+            for dx, dy in moves:
+                total_prob += dp(k - 1, x + dx, y + dy)
+
+            memo[(k, x, y)] = total_prob / 8
+            return memo[(k, x, y)]
+
+        return dp(k, row, column)        
