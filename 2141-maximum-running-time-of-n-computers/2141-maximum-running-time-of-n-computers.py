94d9aedff14e062b25bde14a553f03bbac3f2818
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-27 11:27:51 +0530

    Time: 2276 ms (40.96%), Space: 31.2 MB (7.23%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        def cannot_run(n,batteries,time):            
-            return sum([min(t, time) for t in batteries]) < n * time
-        
-        low,high =0,sum(batteries)// n
-        while low <high:
-            guess =high -(high -low)// 2 # prevent infinite loop to find the maximal T
-            if cannot_run(n,batteries,guess):
+        def cannot_run(time):            
+            return sum([min(t, time) for t in batteries]) < n * time        
+        low, high = 0, sum(batteries) // n
+        while low < high:
+            guess = high - (high - low) // 2
+            if cannot_run(guess):
                 high =guess -1
             else:
                 low =guess
