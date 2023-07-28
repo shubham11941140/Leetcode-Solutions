3af7e6ce9f308eb24c5a2467f35f7ac7ba29bc2b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-28 17:43:37 +0530

    Time: 47 ms (85.56%), Space: 16.4 MB (70.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,13 +1,11 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        # Create a 2D array to store the maximum score difference between
-        # player 1 and player 2 for the subarray starting at index i and
-        # ending at index j
-        dp = [[0] * len(nums) for _ in range(len(nums))]
-        for i in range(len(nums)):
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
             dp[i][i] = nums[i]
-        for diagonal in range(1, len(nums)):
-            for i in range(len(nums) - diagonal):
+        for diagonal in range(1, n):
+            for i in range(n - diagonal):
                 j = i + diagonal
                 dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
         return dp[0][-1] >= 0        
