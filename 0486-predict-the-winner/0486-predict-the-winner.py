412fd459aae553b882034955d32cb36b6a7dd99d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-28 17:44:44 +0530

    Time: 29 ms (99.88%), Space: 16.3 MB (88.23%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -6,6 +6,5 @@ class Solution:
             dp[i][i] = nums[i]
         for d in range(1, n):
             for i in range(n - d):
-                j = i + d
-                dp[i][j] = max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1])
+                dp[i][i + d] = max(nums[i] - dp[i + 1][i + d], nums[i + d] - dp[i][i + d - 1])
         return dp[0][-1] >= 0        
