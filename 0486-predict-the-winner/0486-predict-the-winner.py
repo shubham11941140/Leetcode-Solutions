3c3db81176f6ab9131a43fe1b643338564fd6753
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-28 17:43:04 +0530

    Time: 38 ms (97.94%), Space: 16.2 MB (97.57%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -4,14 +4,8 @@ class Solution:
         # player 1 and player 2 for the subarray starting at index i and
         # ending at index j
         dp = [[0] * len(nums) for _ in range(len(nums))]
-
-        # Fill the diagonal with the values of the array, as the subarray
-        # starting and ending at the same index will only contain one number
         for i in range(len(nums)):
             dp[i][i] = nums[i]
-
-        # Iterate through the array and fill the remaining cells in the 2D array
-        # We start at the second diagonal and work our way up
         for diagonal in range(1, len(nums)):
             for i in range(len(nums) - diagonal):
                 j = i + diagonal
