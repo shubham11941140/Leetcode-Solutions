ebf87d9e8c33e8d362be355c382c287444c731cc
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-30 19:15:33 +0530

    Time: 543 ms (68.39%), Space: 16.4 MB (92.90%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -2,14 +2,10 @@ class Solution:
     def strangePrinter(self, s: str) -> int:
         if not s:
             return 0
-
         n = len(s)
         dp = [[0] * n for _ in range(n)]
-
         for i in reversed(range(n)):
             dp[i][i] = 1
             for j in range(i + 1, n):
                 dp[i][j] = dp[i][j - 1] if s[i] == s[j] else min([dp[i][k] + dp[k + 1][j] for k in range(i, j)])
-
-
         return dp[0][n - 1]        
