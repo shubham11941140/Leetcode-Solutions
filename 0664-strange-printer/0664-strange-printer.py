3a8bbfa7f1ab7732369adf5ff8b0d8a1c6790fe1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-30 19:14:53 +0530

    Time: 543 ms (68.39%), Space: 16.4 MB (80.65%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -6,14 +6,13 @@ class Solution:
         n = len(s)
         dp = [[0] * n for _ in range(n)]
 
-        for i in range(n - 1, -1, -1):
+        for i in reversed(range(n)):
             dp[i][i] = 1
             for j in range(i + 1, n):
                 if s[i] == s[j]:
                     dp[i][j] = dp[i][j - 1]
                 else:
                     dp[i][j] = float('inf')
-                    for k in range(i, j):
-                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j])
+                    dp[i][j] = min([dp[i][k] + dp[k + 1][j] for k in range(i, j)])
 
         return dp[0][n - 1]        
