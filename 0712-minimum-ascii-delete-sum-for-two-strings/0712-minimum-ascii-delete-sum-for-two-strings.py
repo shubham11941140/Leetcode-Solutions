b011fb9bee4580c127bb1ffc258b926981144e90
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-07-31 11:18:02 +0530

    Time: 504 ms (84.09%), Space: 21 MB (62.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,27 +1,15 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         m, n = len(s1), len(s2)
-
-        # Create a 2D DP table with dimensions (m+1) x (n+1)
         dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-        # Fill the first row and first column of DP table
         for i in range(1, m + 1):
             dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
-
         for j in range(1, n + 1):
             dp[0][j] = dp[0][j - 1] + ord(s2[j - 1])
-
-        # Fill the DP table using recurrence relation
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if s1[i - 1] == s2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = min(
-                        dp[i - 1][j] + ord(s1[i - 1]),
-                        dp[i][j - 1] + ord(s2[j - 1])
-                    )
-
-        # The answer will be in the bottom-right cell of the DP table
+                    dp[i][j] = min(dp[i - 1][j] + ord(s1[i - 1]), dp[i][j - 1] + ord(s2[j - 1]))
         return dp[m][n]        
