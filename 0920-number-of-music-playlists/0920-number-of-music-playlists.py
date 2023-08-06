1afc03e9a4f3d1e710dca731c06989f8bf63f8e0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-08-06 10:57:04 +0530

    Time: 116 ms (14.28%), Space: 16.6 MB (60.90%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,13 +1,12 @@
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        L = goal
         N = n
         K = k
-        dp = [[0] * (L + 1) for _ in range(N + 1)]
+        dp = [[0] * (goal + 1) for _ in range(N + 1)]
         dp[0][0] = 1
         for i in range(1, N + 1):
-            for j in range(1, L + 1):
+            for j in range(1, goal + 1):
                 dp[i][j] += dp[i - 1][j - 1] * (N - i + 1)
                 dp[i][j] += dp[i][j - 1] * max(i - K, 0)
                 dp[i][j] %= 10**9 + 7
-        return dp[N][L]        
+        return dp[N][goal]        
