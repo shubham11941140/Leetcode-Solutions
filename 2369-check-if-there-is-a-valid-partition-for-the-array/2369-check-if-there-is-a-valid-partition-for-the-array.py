6d9904dd4e121728774316719ed10f524748efdc
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-08-13 20:21:42 +0530

    Time: 882 ms (92.31%), Space: 30.4 MB (78.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -11,10 +11,6 @@ class Solution:
             if nums[i] == nums[i - 1]:
                 dp[i + 1] = dp[i + 1] or dp[i - 1]
             
-            if nums[i] == nums[i - 1] == nums[i - 2]:
-                dp[i + 1] = dp[i + 1] or dp[i - 2]
-            
-            if nums[i] == nums[i - 1] + 1 and nums[i] == nums[i - 2] + 2:
-                dp[i + 1] = dp[i + 1] or dp[i - 2]
-        
+            if (nums[i] == nums[i - 1] == nums[i - 2]) or (nums[i] == nums[i - 1] + 1 and nums[i] == nums[i - 2] + 2):
+                dp[i + 1] = dp[i + 1] or dp[i - 2]        
         return dp[n]
