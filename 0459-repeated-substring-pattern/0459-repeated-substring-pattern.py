10d32ea71eceacb523d791f3838096cf3e85fc52
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-08-21 10:46:22 +0530

    Time: 50 ms (73.05%), Space: 16.2 MB (94.58%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -4,5 +4,7 @@ class Solution:
         for i in range(1, n):            
             if not (n % i) and s[:i] * (n // i) == s:
                 return True
+            if i > n // 2:
+                break
         return False
         
