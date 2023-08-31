a7e768fbd254365a19d9059a08e2ff21bd9fc299
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-08-31 10:11:25 +0530

    Time: 123 ms (87.14%), Space: 17.9 MB (32.14%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -2,27 +2,25 @@ class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
         inter = sorted([(max(0, i - ranges[i]), min(n, i + ranges[i])) for i in range(n + 1)])
         taps = 0  # Number of taps used
-        current_end = 0  # The current rightmost point covered by taps
+        ce = 0  # The current rightmost point covered by taps
         i = 0
 
         while i <= n:
             # Find the farthest tap we can reach from the current position
-            farthest = current_end
+            farthest = ce
 
-            while i <= n and inter[i][0] <= current_end:
+            while i <= n and inter[i][0] <= ce:
                 farthest = max(farthest, inter[i][1])
                 i += 1
 
             # If we couldn't find a tap that covers the current position, return -1
-            if farthest == current_end:
+            if farthest == ce:
                 return -1
 
             # Increment the taps count and update the current end
             taps += 1
-            current_end = farthest
-
-            # If we've covered the entire garden, return the taps count
-            if current_end >= n:
+            ce = farthest
+            if ce >= n:
                 return taps
 
         return -1  # If we can't cover the entire garden
