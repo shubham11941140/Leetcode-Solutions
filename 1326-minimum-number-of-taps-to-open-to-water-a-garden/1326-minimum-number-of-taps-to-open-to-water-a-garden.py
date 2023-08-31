1523acc88d7ba49106d1138595edb5d9319078be
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-08-31 10:12:18 +0530

    Time: 136 ms (70.48%), Space: 18 MB (30.48%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -4,11 +4,8 @@ class Solution:
         taps = 0  # Number of taps used
         ce = 0  # The current rightmost point covered by taps
         i = 0
-
         while i <= n:
-            # Find the farthest tap we can reach from the current position
             f = ce
-
             while i <= n and inter[i][0] <= ce:
                 f = max(f, inter[i][1])
                 i += 1
