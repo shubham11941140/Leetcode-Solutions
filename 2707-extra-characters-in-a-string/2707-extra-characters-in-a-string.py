8432ead4fc484283b87293161e084b01e3c942a5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-09-02 12:17:44 +0530

    Time: 215 ms (84.39%), Space: 16.4 MB (66.40%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -7,10 +7,9 @@ class Solution:
         word_set = set(dictionary)
         
         for i in range(1, max_len):
-            dp[i] = dp[i - 1] + 1  # Initialize with a worst-case scenario (one more character than previous)
+            dp[i] = dp[i - 1] + 1
             
             for length in range(1, i + 1):
-                # Check if the substring s[i-length:i] exists in the dictionary
                 if s[i - length:i] in word_set:
                     # If it does, update dp[i] with the minimum of current dp[i] and dp[i-length]
                     dp[i] = min(dp[i], dp[i - length])
