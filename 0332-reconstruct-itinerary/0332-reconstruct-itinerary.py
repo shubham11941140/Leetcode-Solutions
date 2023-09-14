d6649fdd4458775b8c899761a814ea748ec23cf9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-09-14 10:25:16 +0530

    Time: 70 ms (99.57%), Space: 16.4 MB (99.90%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = collections.defaultdict(list)
+        for start, end in tickets:
+            graph[start].append(end)
+        for start in graph:
+            graph[start].sort(reverse=True)
+        stack = ["JFK"]
+        ans = []
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop())
+            ans.append(stack.pop())
+        return ans[::-1]        
