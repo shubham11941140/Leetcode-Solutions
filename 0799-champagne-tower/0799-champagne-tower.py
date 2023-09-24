100575fed27f1f1e6fc4a5689e9666675c38957c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-09-24 21:59:21 +0530

    Time: 140 ms (20.25%), Space: 16.3 MB (72.75%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,14 +1,7 @@
 from collections import Counter
 
 class Solution:
-    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        # print(inorder)
-        
-        # Store all child in an array
-        
-        # Apply dp
-        
-        
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:       
         if not poured:
             return 0
         c = [[0 for i in range(101)] for j in range(101)]
@@ -20,11 +13,3 @@ class Solution:
                     c[r + 1][col] += left
                     c[r + 1][col + 1] += left
         return min(1, c[query_row][query_glass])
-        
-
-        
-        
-
-        
-        
-        
