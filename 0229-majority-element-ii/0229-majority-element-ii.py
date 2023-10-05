1bb15e18b6ae079f7c8efb65790e2aaa0573c9d5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-05 13:47:04 +0530

    Time: 115 ms (37.08%), Space: 17.7 MB (49.91%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        d = Counter(nums)
+        n = len(nums)
+        return [i for i in d if d[i] > n // 3]
+                
+        
