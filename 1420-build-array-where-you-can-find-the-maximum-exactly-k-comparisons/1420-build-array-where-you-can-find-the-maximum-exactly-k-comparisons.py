23b09a62d89afccc476dc26a7503eda6cd961eee
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-07 13:36:53 +0530

    Time: 3262 ms (6.19%), Space: 31.9 MB (38.94%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,19 +1,17 @@
 class Solution:
+    
     def dp(self,i,n,m,k,last,dct):
         if i==n:
             if k==0:
                 return 1
             return 0
-        if (i,k,last) in dct:
-            return dct[(i,k,last)]
-        val=0
+        if (i, k, last) in dct:
+            return dct[(i, k, last)]
+        val = 0
         for j in range(1,m+1):
-            if j<=last:
-                val+=self.dp(i+1,n,m,k,last,dct)
-            else:
-                val+=self.dp(i+1,n,m,k-1,j,dct)
-        dct[(i,k,last)]=val
-        return val%1000000007
+            val += (self.dp(i+1,n,m,k,last,dct) if j <= last else self.dp(i+1,n,m,k-1,j,dct))
+        dct[(i,k,last)] = val
+        return val
 
     def numOfArrays(self, n: int, m: int, k: int) -> int:
         return self.dp(0, n, m, k, -1, {}) % (10 ** 9 + 7)
