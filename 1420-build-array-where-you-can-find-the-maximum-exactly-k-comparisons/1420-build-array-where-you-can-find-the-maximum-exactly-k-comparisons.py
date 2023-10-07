3d732aac791020de2a9a28c604808a5dade15cdf
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-07 13:42:41 +0530

    Time: 3240 ms (6.19%), Space: 32 MB (38.94%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,18 +1,5 @@
 class Solution:
-    
-    def dp(self,i, n, m, k, last, dct):
-        if i == n:
-            return int(not k)
-        if (i, k, last) in dct:
-            return dct[(i, k, last)]
-        val = 0
-        for j in range(1, m + 1):
-            val += (self.dp(i + 1, n, m, k, last, dct) if j <= last else self.dp(i + 1, n, m, k - 1, j, dct))
-        dct[(i, k, last)] = val
-        return val
-
-    def numOfArrays(self, n: int, m: int, k: int) -> int:
-        
+    def numOfArrays(self, n: int, m: int, k: int) -> int:        
         def dp(i, k, last, dct):
             if i == n:
                 return int(not k)
@@ -23,9 +10,5 @@ class Solution:
                 val += (dp(i + 1, k, last, dct) if j <= last else dp(i + 1, k - 1, j, dct))
             dct[(i, k, last)] = val
             return val        
-        
-        
-        
-        
         return dp(0, k, -1, {}) % (10 ** 9 + 7)
         
