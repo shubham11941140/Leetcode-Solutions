3cd299e2f33ec6ac83158e93b58a431366db1384
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-14 12:13:48 +0530

    Time: 2063 ms (41.85%), Space: 437.3 MB (30.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,15 +1,14 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         n = len(cost)
-        INF = 10 ** 20
 
         @lru_cache(None)  # Using lru_cache for memoization
         def calc(ctime, index):
             if ctime >= n:
                 return 0
             if index == n:
-                return INF
+                return 10 ** 20
 
             return min(calc(ctime + time[index] + 1, index + 1) + cost[index], calc(ctime, index + 1))
 
-        return min(INF, calc(0, 0))      
+        return min(10 ** 20, calc(0, 0))      
