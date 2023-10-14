4fa64c978bb558d514c5b4dce3e23a16c119a89e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-14 12:09:45 +0530

    Time: 2086 ms (40.37%), Space: 437.5 MB (30.00%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -0,0 +1,21 @@
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        N = len(cost)
+        INF = 10 ** 20
+
+        @lru_cache(None)  # Using lru_cache for memoization
+        def calc(ctime, index):
+            if ctime >= N:
+                return 0
+            if index == N:
+                return INF
+
+            return min(calc(ctime + time[index] + 1, index + 1) + cost[index], calc(ctime, index + 1))
+
+        # Initialize the best cost to a large value
+        best = INF
+
+        # Call the calc function to find the minimum cost
+        best = min(best, calc(0, 0))
+
+        return best        
