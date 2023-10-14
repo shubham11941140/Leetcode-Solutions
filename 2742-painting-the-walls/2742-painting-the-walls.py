4aae2823de2b1d27a304d73f2715f39378e5fbec
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-14 12:14:24 +0530

    Time: 2112 ms (37.03%), Space: 437.5 MB (30.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(cost)
 
         @lru_cache(None)  # Using lru_cache for memoization
-        def calc(ctime, index):
+        def calc(ctime, i):
             if ctime >= n:
                 return 0
-            if index == n:
+            if i == n:
                 return 10 ** 20
 
-            return min(calc(ctime + time[index] + 1, index + 1) + cost[index], calc(ctime, index + 1))
+            return min(calc(ctime + time[i] + 1, i + 1) + cost[i], calc(ctime, i + 1))
 
         return min(10 ** 20, calc(0, 0))      
