9632c15812f2401f21451242c455932c361bc8c9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-17 10:55:32 +0530

    Time: 266 ms (71.56%), Space: 24.9 MB (20.06%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -0,0 +1,42 @@
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        in_degree = [0] * n
+        root = -1
+
+        # update inDegree count for leftChild and rightChild
+        for child in leftChild:
+            if child != -1:
+                in_degree[child] += 1
+                if in_degree[child] > 1:
+                    return False
+
+        for child in rightChild:
+            if child != -1:
+                in_degree[child] += 1
+                if in_degree[child] > 1:
+                    return False
+
+        # find root
+        for i in range(n):
+            if in_degree[i] == 0:
+                if root == -1:
+                    root = i
+                else:
+                    return False
+
+        # if no root
+        if root == -1:
+            return False
+
+        # check if all nodes are reachable from root
+        if self.dfs(root, leftChild, rightChild) == n:
+            return True
+        else:
+            return False
+
+    def dfs(self, root, leftChild, rightChild):
+        if root == -1:
+            return 0
+        else:
+            return 1 + self.dfs(leftChild[root], leftChild, rightChild) + self.dfs(rightChild[root], leftChild, rightChild)
+        
