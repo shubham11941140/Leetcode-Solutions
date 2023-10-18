34e3039986f2ca278ea1a3f5bf74c5a6bf293144
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-18 12:59:33 +0530

    Time: 1430 ms (69.12%), Space: 45.6 MB (65.88%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         graph = [[] for _ in range(n)]
         indegree = [0] * n
-        maxTime = time.copy()
+        m = time.copy()
 
         for edge in relations:
             graph[edge[0] - 1].append(edge[1] - 1)
@@ -13,11 +13,11 @@ class Solution:
             node = queue.popleft()
 
             for neighbor in graph[node]:
-                maxTime[neighbor] = max(maxTime[neighbor], maxTime[node] + time[neighbor])
+                m[neighbor] = max(m[neighbor], m[node] + time[neighbor])
                 indegree[neighbor] -= 1
 
                 if not indegree[neighbor]:
                     queue.append(neighbor)
-        return max(maxTime)
+        return max(m)
 
         
