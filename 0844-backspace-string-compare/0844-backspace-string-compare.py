042f9b27dc14024c15c774dc2d6177201dcf6734
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-19 13:31:35 +0530

    Time: 41 ms (43.68%), Space: 16.3 MB (54.52%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,18 @@
+class Solution:    
+    
+    def change(self, s):
+        stack = []
+        for i in s:
+            if i == '#':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return stack
+    
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        return self.change(s) == self.change(t)
+        
+                
+                
+        
