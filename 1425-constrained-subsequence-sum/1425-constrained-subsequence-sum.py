0ff205762b4af658c7e72ae0b49472d82328b84f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-22 17:30:57 +0530

    Time: 1255 ms (97.02%), Space: 30 MB (88.51%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -2,7 +2,6 @@ class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
         dp = [0] * len(nums)
         q = deque()
-
         for i in range(len(nums)):
             if q and i - q[0] > k:
                 q.popleft()
@@ -11,6 +10,5 @@ class Solution:
                 q.pop()
             if dp[i] > 0:
                 q.append(i)
-
         return max(dp)
         
