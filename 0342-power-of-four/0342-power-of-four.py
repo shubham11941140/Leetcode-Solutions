54d9d5ac673f74e8fe073f6cdcd2001bb9cab052
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-23 13:08:52 +0530

    Time: 43 ms (34.73%), Space: 16 MB (99.16%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,6 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        return 4 ** round(log(n, 4)) == n
-        
+        return False if n <= 0 else 4 ** round(log(n, 4)) == n
