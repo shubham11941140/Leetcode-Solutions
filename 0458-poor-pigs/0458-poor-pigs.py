b5f2188077937ea0be40bc3cd439a3f541a5382c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-10-29 12:51:16 +0530

    Time: 40 ms (37.60%), Space: 16.3 MB (13.60%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.py
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -2,7 +2,7 @@ class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         t = (minutesToTest // minutesToDie) + 1
         for i in range(100):
-            if pow(t, i) >= buckets:
+            if t ** i >= buckets:
                 return i
             
         
