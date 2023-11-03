2b6289825074e8892b65510374f017889b239d73
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-03 10:10:20 +0530

    Time: 52 ms (6.26%), Space: 16.1 MB (76.57%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        res = []
+        j = 0
+        for i in range(1, n + 1):
+            if j < len(target):
+                if i == target[j]:
+                    res.append("Push")
+                    j += 1
+                else:
+                    res.append("Push")
+                    res.append("Pop")
+            else:
+                break
+        return res
+        
