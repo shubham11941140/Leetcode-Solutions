904c78a9dcaa3608f469033a3c5e36e127fb3977
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-08 10:34:51 +0530

    Time: 49 ms (7.77%), Space: 16.2 MB (73.40%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -1,4 +1,4 @@
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
         min_step = max(abs(sx - fx), abs(sy - fy))
-        return t != 1 if min_step == 0 else min_step <= t        
+        return t != 1 if not min_step else min_step <= t        
