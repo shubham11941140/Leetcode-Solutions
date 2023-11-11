4a23a8a49d6390d56e5917464cc7aab1208d622d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-11 11:56:13 +0530

    Time: 856 ms (40.59%), Space: 19.4 MB (60.59%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
@@ -12,20 +12,20 @@ class Graph:
 
     def shortestPath(self, node1: int, node2: int) -> int:
         INF = float("inf")
-        distances = {node: INF for node in range(self.n)}
-        distances[node1] = 0
-        heap = [(0, node1)]
+        d = {i: INF for i in range(self.n)}
+        d[node1] = 0
+        h = [(0, node1)]
 
-        while heap:
-            curr_dist, curr_node = heappop(heap)
-            if curr_dist > distances[curr_node]:
+        while h:
+            cd, cn = heappop(h)
+            if cd > d[cn]:
                 continue
-            for neighbor, weight in self.g[curr_node].items():
-                dist = curr_dist + weight
-                if dist < distances[neighbor]:
-                    distances[neighbor] = dist
-                    heappush(heap, (dist, neighbor))
-        return distances[node2] if distances[node2] != INF else -1      
+            for neighbor, weight in self.g[cn].items():
+                dist = cd + weight
+                if dist < d[neighbor]:
+                    d[neighbor] = dist
+                    heappush(h, (dist, neighbor))
+        return d[node2] if d[node2] != INF else -1      
 
 # Your Graph object will be instantiated and called as such:
 # obj = Graph(n, edges)
