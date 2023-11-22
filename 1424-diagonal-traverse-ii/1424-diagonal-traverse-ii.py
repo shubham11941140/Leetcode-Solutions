e180b1fb65c146a13fbb920a2dd099812c35e5b7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-22 19:17:58 +0530

    Time: 792 ms (55.17%), Space: 41.2 MB (35.88%) - LeetHub

---
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        diagonals = defaultdict(list)
+        for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                diagonals[i + j].append(nums[i][j])
+        result = []
+        for key in sorted(diagonals.keys()):
+            result.extend(diagonals[key][::-1])
+        return result        
