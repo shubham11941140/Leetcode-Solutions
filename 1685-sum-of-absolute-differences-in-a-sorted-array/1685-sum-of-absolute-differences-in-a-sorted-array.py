c3b46ae26eda159f1af84f95ee069b765e02f7e0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-26 00:24:18 +0530

    Time: 730 ms (56.73%), Space: 31.6 MB (75.64%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -4,7 +4,4 @@ class Solution:
         for num in nums:
             prefix.append(prefix[-1] + num)
         total = prefix[-1]
-        res = []
-        for i, num in enumerate(nums):
-            res.append(total - 2 * prefix[i] + num * (2 * i - len(nums)))
-        return res        
+        return [total - 2 * prefix[i] + num * (2 * i - len(nums)) for i, num in enumerate(nums)]      
