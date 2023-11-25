8a51a40034b3d2cacf2663bcae30425bc0704fe3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-26 00:25:42 +0530

    Time: 708 ms (79.81%), Space: 31.7 MB (75.64%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
         p = [0]
-        for num in nums:
-            p.append(p[-1] + num)
-        total = p[-1]
-        return [total - 2 * p[i] + num * (2 * i - len(nums)) for i, num in enumerate(nums)]      
+        for i in nums:
+            p.append(p[-1] + i)
+        return [p[-1] - 2 * p[i] + num * (2 * i - len(nums)) for i, num in enumerate(nums)]      
