6c47407193d304c9eea61901864a61599f0f4595
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-11-26 22:47:46 +0530

    Time: 1086 ms (70.14%), Space: 42.9 MB (12.50%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -3,11 +3,10 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         for i in range(1, m):
             for j in range(n):
-                if matrix[i][j] == 1:
+                if matrix[i][j]:
                     matrix[i][j] += matrix[i - 1][j]
         res = 0
         for i in range(m):
             row = sorted(matrix[i], reverse=True)
-            for j in range(n):
-                res = max(res, row[j] * (j + 1))
+            res = max(res, max([row[j] * (j + 1) for j in range(n)]))
         return res        
