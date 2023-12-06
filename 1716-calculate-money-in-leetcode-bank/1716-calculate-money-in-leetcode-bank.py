55e258d07533ff8d2d387ae77fb168d9b3ff75b9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-12-06 12:26:03 +0530

    Time: 33 ms (91.19%), Space: 16.3 MB (40.24%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,4 +1,3 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        weeks = n // 7
-        return sum([sum(range(i + 1, i + 8)) for i in range(weeks)]) + sum(range(weeks + 1, weeks + 1 + (n % 7)))     
+        return sum([sum(range(i + 1, i + 8)) for i in range(n // 7)]) + sum(range((n // 7) + 1, (n // 7) + 1 + (n % 7)))     
