cef3ddc473041b88ed18255a40c0738d2912d7e3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-12-06 12:24:35 +0530

    Time: 40 ms (56.48%), Space: 16.2 MB (40.24%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -2,11 +2,6 @@ class Solution:
     def totalMoney(self, n: int) -> int:
         weeks = n // 7
         remainder = n % 7
-
-        total = 0
-        for i in range(weeks):
-            total += sum(range(i+1, i+8))
-
+        total = sum([sum(range(i + 1, i + 8)) for i in range(weeks)])
         total += sum(range(weeks+1, weeks+1+remainder))
-
         return total        
