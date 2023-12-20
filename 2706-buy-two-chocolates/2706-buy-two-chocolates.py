c501fc649b307cd50720f0c6bfd9c192fff8a128
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-12-20 15:03:16 +0530

    Time: 56 ms (93.54%), Space: 17.3 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,15 +1,5 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        # Sort the prices array in ascending order
         prices.sort()
-
-        # Calculate the cost of the two cheapest chocolates
         cost = prices[0] + prices[1]
-
-        # Check if you can afford to buy the two chocolates without going into debt
-        if cost <= money:
-            # If you can afford, return the leftover amount
-            return money - cost
-        else:
-            # If you cannot afford, return the initial amount of money
-            return money        
+        return money - cost if cost <= money else money    
