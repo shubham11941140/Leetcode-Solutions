2063f81a27685f79dc09d32eca9b18ee81636d38
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-12-22 23:44:44 +0530

    Time: 36 ms (86.87%), Space: 17.4 MB (5.69%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        one_int = s.count('1')  # Count of '1's in the string
+        zero_int = 0  # Count of '0's in the left substring
+        max_score = 0  # Maximum score
+
+        # Iterate over the string starting from the second character to the second-to-last character
+        for char in s[:-1]:
+            if char == '1':
+                one_int -= 1  # Decrement count of '1's
+            else:
+                zero_int += 1  # Increment count of '0's
+
+            # Calculate the current score
+            current_score = zero_int + one_int
+
+            # Update the maximum score
+            max_score = max(max_score, current_score)
+
+        # Return the maximum score
+        return max_score        
