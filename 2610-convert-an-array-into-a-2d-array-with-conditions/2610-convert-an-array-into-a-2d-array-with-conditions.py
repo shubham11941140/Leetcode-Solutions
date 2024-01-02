ad28007f9b4deb4d00060a5eb814343876838202
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-01-02 12:24:07 +0530

    Time: 63 ms (26.98%), Space: 17.4 MB (5.17%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,8 +1,7 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         f = Counter(nums)
-        m = max(f.values())
-        return [[i for i in f if c <= f[i]] for c in range(1, m + 1)]
+        return [[i for i in f if c <= f[i]] for c in range(1, max(f.values()) + 1)]
                     
                 
             
