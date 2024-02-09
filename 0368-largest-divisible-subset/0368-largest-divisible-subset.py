ed66aa65864fa7af0eb1c826b68f42327d743a57
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-02-09 18:02:14 +0530

    Time: 204 ms (89.14%), Space: 16.9 MB (76.96%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -3,9 +3,9 @@ class Solution:
         if not nums:
             return []
         nums.sort()
-        solution = [[num] for num in nums]
+        s = [[num] for num in nums]
         for i in range(len(nums)):
             for j in range(i):
-                if nums[i] % nums[j] == 0 and len(solution[i]) < len(solution[j]) + 1:
-                    solution[i] = solution[j] + [nums[i]]
-        return max(solution, key = len)        
+                if not (nums[i] % nums[j]) and len(s[i]) < len(s[j]) + 1:
+                    s[i] = s[j] + [nums[i]]
+        return max(s, key = len)        
