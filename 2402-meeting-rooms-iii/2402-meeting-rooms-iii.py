f652dfc80266bb3b2af1113e3a1331fb4913d67a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-02-18 11:53:36 +0530

    Time: 2372 ms (9.10%), Space: 62.3 MB (90.33%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -3,33 +3,33 @@ class Solution:
         meetings.sort(key=lambda x: x[0])
         d = {i:[] for i in range(n)}
         e = {i:0 for i in range(n)}
+        l = {i:0 for i in range(n)}
         for i in meetings:
             f = False
             for j in d:
                 # Unused case
                 if not d[j]:
-                    d[j].append(i)
+                    d[j] = i
+                    l[j] += 1
                     e[j] = i[1]
                     f = True
                     break
                 # If the meeting is not overlapping
-                if i[0] >= d[j][-1][1] and e[j] <= i[0]:
-                    #print(17, d[j], e[j], i)
-                    d[j].append(i)
+                if i[0] >= d[j][1] and e[j] <= i[0]:
+                    d[j] = i
                     e[j] = i[1]
+                    l[j] += 1
                     f = True
                     break
             if not f:
                 m = min(e.values())
-                #print(23, m)
                 for j in d:
                     if e[j] == m:
                         e[j] = m + i[1] - i[0]
-                        d[j].append(i)
+                        d[j] = i
+                        l[j] += 1
                         break
-        #print(d, e)   
-        f = {i: len(d[i]) for i in d}
-        m = max(f.values())
-        for i in f:
-            if f[i] == m:
+        m = max(l.values())
+        for i in l:
+            if l[i] == m:
                 return i        
