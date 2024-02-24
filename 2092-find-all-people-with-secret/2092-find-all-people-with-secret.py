9c21fe6e426217bbc5df517ab529a3587f766262
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-02-24 10:20:15 +0530

    Time: 1752 ms (31.79%), Space: 68.6 MB (22.51%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -2,18 +2,18 @@ class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
         q = [(0, 0), (0, firstPerson)]
 
-        graph = collections.defaultdict(list)
-        for person_i, person_ii, time in meetings:
-            graph[person_i].append((person_ii, time))
-            graph[person_ii].append((person_i, time))
+        g = collections.defaultdict(list)
+        for p, p1, t in meetings:
+            g[p].append((p1, t))
+            g[p1].append((p, t))
 
-        answer = set()
+        a = set()
         while q:
-            time, person_i = heapq.heappop(q)
-            if person_i in answer:
+            t, p = heappop(q)
+            if p in a:
                 continue
-            answer.add(person_i)
-            for person_ii, meeting_time in graph[person_i]:
-                if meeting_time >= time:
-                    heapq.heappush(q, (meeting_time, person_ii))
-        return list(answer)      
+            a.add(p)
+            for pii, m in g[p]:
+                if m >= t:
+                    heappush(q, (m, pii))
+        return list(a)      
