2d24f5e1acdf1e226048608a6cb793f7a1cbf727
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-03-01 15:07:16 +0530

    Time: 24 ms (99.25%), Space: 16.6 MB (45.47%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        # Count the number of '1's in the binary string
+        count_ones = s.count("1")
+
+        # If there are no '1's, there's no odd number possible, return empty string
+        if count_ones == 0:
+            return ""
+
+        # Otherwise, construct the maximum odd binary number by doing the following:
+        # - Use all '1's except one to maintain oddness ('1' * (count_ones - 1))
+        # - Fill the rest of the string with '0's ((len(s) - count_ones) * "0")
+        # - Add a '1' at the end to ensure the number is odd
+        return "1" * (count_ones - 1) + (len(s) - count_ones) * "0" + "1"        
