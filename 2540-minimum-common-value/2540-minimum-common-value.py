48d8b870ad7ad1b5c364f7b59fd78b1537b581d2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-03-09 23:55:22 +0530

    Time: 354 ms (47.91%), Space: 35.6 MB (53.64%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0  # Pointers for both arrays
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]  # Found a common integer
+            elif nums1[i] < nums2[j]:
+                i += 1  # Move pointer in nums1
+            else:
+                j += 1  # Move pointer in nums2
+
+        return -1        
