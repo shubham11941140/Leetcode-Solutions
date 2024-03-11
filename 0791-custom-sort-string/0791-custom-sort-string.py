029534de93a86542f0d4df2c68502755d8842548
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-03-11 14:07:58 +0530

    Time: 40 ms (22.55%), Space: 16.7 MB (26.18%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,22 +1,8 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        # Count the occurrences of each character in the string s
-        char_count = Counter(s)
-        
-        # Initialize an empty list to store the sorted characters
-        sorted_characters = []
-        
-        # Add characters to the sorted_characters list following the order specified
-        # Multiply the character by its count to add it that many times
+        cc = Counter(s)
+        sc = []
         for char in order:
-            sorted_characters.append(char * char_count[char])
-            # Set the count for this character to 0 since it's been handled
-            char_count[char] = 0
-        
-        # Add remaining characters that were not in 'order' to the list
-        # These are added at the end in their original order of occurrence
-        for char, count in char_count.items():
-            sorted_characters.append(char * count)
-        
-        # Join all the characters in the sorted_characters list to form the sorted string
-        return ''.join(sorted_characters)        
+            sc.append(char * cc[char])
+            cc[char] = 0
+        return ''.join(sc + [char * count for char, count in cc.items()])        
