60428d2dd63dc7c72978e5c01f43b19ec0defc84
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-03-14 16:45:46 +0530

    Time: 228 ms (34.07%), Space: 20.4 MB (28.42%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        prefix_sum = 0
+        count = defaultdict(int)  # Store the count of prefix sums
+        total_subarrays = 0
+        for num in nums:
+            prefix_sum += num
+            if prefix_sum == goal:
+                total_subarrays += 1
+            total_subarrays += count[prefix_sum - goal]
+            count[prefix_sum] += 1
+        return total_subarrays        
