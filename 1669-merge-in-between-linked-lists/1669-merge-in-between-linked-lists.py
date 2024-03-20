2f856062465156cad566f9ab09266a04b5b98c7d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-03-20 12:21:22 +0530

    Time: 182 ms (89.59%), Space: 21.1 MB (31.60%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -5,24 +5,14 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        # Find the node just before position 'a'
         preA = list1
         for _ in range(a - 1):
             preA = preA.next
-
-        # Find the node at position 'b'
         postB = preA
         for _ in range(b - a + 2):
             postB = postB.next
-
-        # Connect preA with list2
         preA.next = list2
-
-        # Traverse to the end of list2
         while list2.next is not None:
             list2 = list2.next
-
-        # Connect the end of list2 with postB
         list2.next = postB
-
         return list1        
