1a5483794582f173c4dc5efefcc72200c3b02417
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-04-08 20:14:58 +0530

    Time: 37 ms (77.14%), Space: 16.5 MB (59.90%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        preferences = [0, 0]  # Index 0 for circular sandwiches, index 1 for square sandwiches
+        for preference in students:
+            preferences[preference] += 1
+        for sandwich in sandwiches:
+            if preferences[sandwich] == 0:
+                return sum(preferences)
+            preferences[sandwich] -= 1
+        return 0
