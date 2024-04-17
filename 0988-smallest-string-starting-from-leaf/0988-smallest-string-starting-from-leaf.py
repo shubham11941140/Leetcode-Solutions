1eba9853f72d96326d87584412f7d59a58875e45
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-04-17 23:02:21 +0530

    Time: 36 ms (90.42%), Space: 18.2 MB (7.08%) - LeetHub

---
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    
+    def roottoleafsum(self, root, a, val):
+        val += chr(ord("a") + root.val)
+        if root.left is None and root.right is None:
+            a.append(val)
+        else:            
+            if root.left:
+                self.roottoleafsum(root.left, a, val)
+            if root.right:
+                self.roottoleafsum(root.right, a, val)
+                
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        a = []
+        val = ""
+        self.roottoleafsum(root, a, val)
+        return min([i[::-1] for i in a])
+            
+        
