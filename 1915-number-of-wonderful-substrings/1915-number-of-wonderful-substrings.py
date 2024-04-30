bea1f4d1a71bc25aaa794519ca2a86cd080a4e1a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-04-30 12:17:35 +0530

    Time: 1456 ms (44.23%), Space: 17.5 MB (79.81%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        count = [1] + [0] * 1024
+        cur = res = 0
+        for c in word:
+            cur ^= 1 << (ord(c) - ord('a'))
+            res += count[cur]
+            res += sum(count[cur ^ (1 << i)] for i in range(10))
+            count[cur] += 1
+        return res        
