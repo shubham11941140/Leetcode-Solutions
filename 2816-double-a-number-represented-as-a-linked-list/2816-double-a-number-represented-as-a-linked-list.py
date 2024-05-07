e73d1d3bd2a453723e7eb73bf216dfea9ca08223
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-07 21:43:53 +0530

    Time: 214 ms (92.96%), Space: 20.6 MB (17.06%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.py
@@ -9,7 +9,7 @@ class Solution:
             a = head.val * 2
             if head.next:
                 a += func(head.next)
-            head.val = a%10
+            head.val = a % 10
             return a // 10
         a = func(head)
         if a:
