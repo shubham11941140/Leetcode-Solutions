a6027fef74a8f667327ffa3cc112bcd81cf2e6bb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-08 14:48:20 +0530

    Time: 63 ms (69.98%), Space: 17.7 MB (50.24%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.py
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -1,5 +1,3 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        sort = sorted(score)[::-1]
-        rank = ["Gold Medal", "Silver Medal", "Bronze Medal"] + list(map(str, range(4, len(score) + 1)))
-        return list(map(dict(zip(sort, rank)).get, score))        
+        return list(map(dict(zip(sorted(score)[::-1], ["Gold Medal", "Silver Medal", "Bronze Medal"] + list(map(str, range(4, len(score) + 1))))).get, score))        
