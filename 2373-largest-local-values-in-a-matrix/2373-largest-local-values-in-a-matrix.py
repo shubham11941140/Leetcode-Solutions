3e43d09358dbf9f7b6cf9901ab948637650f02d5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-13 00:09:03 +0530

    Time: 122 ms (70.44%), Space: 17.3 MB (13.77%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -6,8 +6,7 @@ class Solution:
         for i in range(n - 2):
             row = []
             for j in range(n - 2):
-                submatrix = [grid[x][j:j+3] for x in range(i, i+3)]
-                max_val = max(max(sub) for sub in submatrix)
+                max_val = max(max(sub) for sub in [grid[x][j : j + 3] for x in range(i, i + 3)])
                 row.append(max_val)
             result.append(row)
 
