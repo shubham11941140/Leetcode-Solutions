10bf983e91212f787204a2ef78e57e566f6ad864
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-13 00:11:03 +0530

    Time: 124 ms (67.58%), Space: 16.7 MB (100.00%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -1,13 +1,4 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        n = len(grid)
-        result = []
-
-        for i in range(n - 2):
-            row = []
-            for j in range(n - 2):
-                row.append(max(max(s) for s in [grid[x][j : j + 3] for x in range(i, i + 3)]))
-            result.append(row)
-
-        return result
+        return [[max(max(s) for s in [grid[x][j : j + 3] for x in range(i, i + 3)]) for j in range(len(grid) - 2)] for i in range(len(grid) - 2)]
                 
