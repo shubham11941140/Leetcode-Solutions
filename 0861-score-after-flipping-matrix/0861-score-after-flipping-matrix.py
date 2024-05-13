9c89a66bf75251e6c249fc5533e75390844b3331
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-13 16:55:46 +0530

    Time: 46 ms (23.28%), Space: 16.4 MB (92.67%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -1,9 +1,8 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
-        A = grid.copy()
-        M, N = len(A), len(A[0])
+        M, N = len(grid), len(grid[0])
         res = (1 << (N - 1)) * M
         for j in range(1, N):
-            ones = sum(A[i][j] == A[i][0] for i in range(M))
+            ones = sum(grid[i][j] == grid[i][0] for i in range(M))
             res += max(ones, M - ones) * (1 << (N - 1 - j))
         return res        
