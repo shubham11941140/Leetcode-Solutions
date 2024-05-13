6da08aa30fd993257e10632b2a6fe6ae5adf14f5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-13 16:55:20 +0530

    Time: 39 ms (72.41%), Space: 16.4 MB (92.67%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -2,12 +2,8 @@ class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         A = grid.copy()
         M, N = len(A), len(A[0])
-        res = (1 << (N - 1)) * M  # The left-most column is all 1s
-
+        res = (1 << (N - 1)) * M
         for j in range(1, N):
-            # Count the number of 1s in the j-th column
             ones = sum(A[i][j] == A[i][0] for i in range(M))
-            # Flip the column if the number of 1s is less than M / 2
             res += max(ones, M - ones) * (1 << (N - 1 - j))
-
         return res        
