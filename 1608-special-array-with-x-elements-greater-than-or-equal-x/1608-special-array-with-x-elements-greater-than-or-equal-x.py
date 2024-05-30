0139140b3d6d7fe2da67c8c5a21f3617c68f1f83
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-05-30 17:43:50 +0530

    Time: 45 ms (25.36%), Space: 16.5 MB (60.37%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,8 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        nums.append(-1)
+        for i in range(1, len(nums)):
+            if nums[i-1] >= i > nums[i]:
+                return i
+        return -1        
