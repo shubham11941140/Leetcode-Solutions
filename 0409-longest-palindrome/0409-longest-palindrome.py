d332403a8e6259fbb3669bb57b89bb7c9efc7781
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-06-04 19:56:20 +0530

    Time: 35 ms (70.41%), Space: 16.5 MB (80.97%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        char_freq = Counter(s)
+        palindrome_length = 0
+        for freq in char_freq.values():
+            palindrome_length += freq // 2 * 2
+            if palindrome_length % 2 == 0 and freq % 2 == 1:
+                palindrome_length += 1
+        return palindrome_length        
