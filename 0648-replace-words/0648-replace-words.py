84ddc681f965737db7811408d547a5d7d6eacc44
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-06-07 13:01:44 +0530

    Time: 353 ms (13.56%), Space: 24.8 MB (94.66%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -2,14 +2,10 @@ class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         
         root_dict = {root: True for root in dictionary}
-        words = sentence.split()
-
         def find_root(word):
             for i in range(1, len(word)):
                 if word[:i] in root_dict:
                     return word[:i]
             return word
-
-        modified_words = [find_root(word) for word in words]
-        return ' '.join(modified_words)
+        return ' '.join([find_root(word) for word in sentence.split()])
         
