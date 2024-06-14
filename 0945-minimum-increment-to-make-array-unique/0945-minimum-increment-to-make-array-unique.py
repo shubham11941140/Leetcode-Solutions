807f8f288710f79753f339a1b6ff147ca4a0cee1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-06-15 00:59:29 +0530

    Time: 623 ms (75.45%), Space: 30.8 MB (39.86%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        A = nums.copy()
+        A.sort()
+        move = 0
+        for i in range(1, len(A)):
+            if A[i] <= A[i - 1]:
+                move += A[i - 1] - A[i] + 1
+                A[i] = A[i - 1] + 1
+        return move        
