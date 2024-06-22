8b74fedc7452786dcf8d84657f0803b7cefbdb53
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-06-22 13:41:46 +0530

    Time: 659 ms (22.12%), Space: 23.9 MB (25.23%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -2,10 +2,8 @@ class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         counter = Counter({0: 1})  # Initialize with zero odd numbers (valid starting point)
         ans, t = 0, 0
-
         for v in nums:
             t += v & 1  # Check if v is odd
             ans += counter[t - k]  # Add count of valid subarrays
             counter[t] += 1  # Update counter for the current count of odd numbers
-
         return ans        
