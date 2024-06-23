372ec066d1a07c438e8db31d19cfc0093d846d79
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-06-23 10:26:03 +0530

    Time: 437 ms (75.00%), Space: 26.4 MB (45.01%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        max_deque, min_deque = deque(), deque()
+        left = 0
+        max_length = 0
+
+        for right, num in enumerate(nums):
+            # Maintain a decreasing monotonic queue for maximum values
+            while max_deque and nums[max_deque[-1]] < num:
+                max_deque.pop()
+            max_deque.append(right)
+
+            # Maintain an increasing monotonic queue for minimum values
+            while min_deque and nums[min_deque[-1]] > num:
+                min_deque.pop()
+            min_deque.append(right)
+            while nums[max_deque[0]] - nums[min_deque[0]] > limit:
+                if max_deque[0] == left:
+                    max_deque.popleft()
+                if min_deque[0] == left:
+                    min_deque.popleft()
+                left += 1
+
+            # Update the maximum subarray length
+            max_length = max(max_length, right - left + 1)
+
+        return max_length        
