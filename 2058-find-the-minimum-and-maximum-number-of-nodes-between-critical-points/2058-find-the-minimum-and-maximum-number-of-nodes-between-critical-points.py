ae69116aa107b496047b1601b53f467f238186ec
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-05 14:02:38 +0530

    Time: 469 ms (5.32%), Space: 44.8 MB (6.08%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -9,20 +9,12 @@ class Solution:
         while head:
             a.append(head.val)
             head = head.next
-        print(a)
         n = len(a)
         if n < 3:
             return [-1, -1]
-        cp = []
-        for i in range(1, n - 1):
-            if a[i] > max(a[i - 1], a[i + 1]) or a[i] < min(a[i - 1], a[i + 1]):
-                cp.append(i)
-        print("CP", cp)
-        if len(cp) < 2:
-            return [-1, -1]
-        maxd = cp[-1] - cp[0]
-        mind = min([cp[i] - cp[i - 1] for i in range(1, len(cp))])
-        return [mind, maxd]
+        cp = [i for i in range(1, n - 1) if a[i] > max(a[i - 1], a[i + 1]) or a[i] < min(a[i - 1], a[i + 1])]
+        return [-1, -1] if len(cp) < 2 else [min([cp[i] - cp[i - 1] for i in range(1, len(cp))]), cp[-1] - cp[0]]
+
         
             
         
