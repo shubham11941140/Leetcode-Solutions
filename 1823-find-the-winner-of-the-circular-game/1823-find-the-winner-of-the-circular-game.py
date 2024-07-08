f69ee8c48ea6560a4aa910f8da29ba5c549df46f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-08 21:59:47 +0530

    Time: 30 ms (93.48%), Space: 16.6 MB (49.47%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -3,4 +3,4 @@ class Solution:
         if n == 1:
             return 1
         ans = (k + self.findTheWinner(n - 1, k)) % n
-        return n if ans == 0 else ans        
+        return n if not ans else ans        
