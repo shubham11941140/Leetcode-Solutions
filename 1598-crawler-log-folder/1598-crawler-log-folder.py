c127f279e5573f210c55de53aba60704b60ff748
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-10 22:39:30 +0530

    Time: 44 ms (85.36%), Space: 16.7 MB (36.19%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        ans = 0
+        for log in logs:
+            if log == "./":
+                continue
+            elif log == "../":
+                ans = max(0, ans - 1)
+            else:
+                ans += 1
+        return ans
+        
