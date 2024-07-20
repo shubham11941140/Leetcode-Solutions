61a5b529abdc79c783930fc274a5fe23d8a8686f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-20 15:22:19 +0530

    Time: 565 ms (59.57%), Space: 21.6 MB (79.06%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -7,6 +7,5 @@ class Solution:
                 min_value = min(rowSum[i], colSum[j])
                 matrix[i][j] = min_value
                 rowSum[i] -= min_value
-                colSum[j] -= min_value
-        
+                colSum[j] -= min_value        
         return matrix        
