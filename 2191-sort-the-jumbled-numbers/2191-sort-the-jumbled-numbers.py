1e76584ae25c0e6e4d61582e69ab10eee1a80f82
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-24 14:18:14 +0530

    Time: 1083 ms (58.69%), Space: 24.6 MB (60.87%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -1,30 +1,8 @@
 class Solution:
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        mapped_cache = {}
-
+        mc = {}
         def get_mapped_value(num):
-            if num not in mapped_cache:
-                mapped_str = ''.join(str(mapping[int(digit)]) for digit in str(num))
-                mapped_cache[num] = int(mapped_str)
-            return mapped_cache[num]
-
-        # Sort nums based on their mapped values
-        nums.sort(key=get_mapped_value)
-
-        return nums        
-
-        
-        
-        def get_mapped_value(num):
-            return int(''.join(str(mapping[int(digit)]) for digit in str(num)))
-
-        # Create a list of tuples (mapped_value, original_value)
-        mapped_nums = [(get_mapped_value(num), num) for num in nums]
-
-        # Sort based on the mapped values, and if they are the same, maintain the original order
-        mapped_nums.sort(key=lambda x: (x[0], nums.index(x[1])))
-
-        # Extract the original values in the new sorted order
-        sorted_nums = [num for _, num in mapped_nums]
-
-        return sorted_nums        
+            if num not in mc:
+                mc[num] = int(''.join(str(mapping[int(d)]) for d in str(num)))
+            return mc[num]
+        return sorted(nums, key = get_mapped_value)         
