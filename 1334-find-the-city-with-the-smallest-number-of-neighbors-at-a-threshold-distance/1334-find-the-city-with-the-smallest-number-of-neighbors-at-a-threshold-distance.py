1dc9b368b125e328eee122b7d70ed1b91f74d920
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-26 18:01:18 +0530

    Time: 468 ms (31.90%), Space: 17.8 MB (60.27%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -2,8 +2,7 @@ class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         ans = -1
         minCitiesCount = n
-        dist = self.floydWarshall(n, edges, distanceThreshold)
-        
+        dist = self.floydWarshall(n, edges, distanceThreshold)        
         for i in range(n):
             citiesCount = 0
             for j in range(n):
@@ -11,23 +10,18 @@ class Solution:
                     citiesCount += 1
             if citiesCount <= minCitiesCount:
                 ans = i
-                minCitiesCount = citiesCount
-        
+                minCitiesCount = citiesCount        
         return ans
     
     def floydWarshall(self, n, edges, distanceThreshold):
         dist = [[distanceThreshold + 1] * n for _ in range(n)]
         for i in range(n):
-            dist[i][i] = 0
-        
-        for edge in edges:
-            u, v, w = edge
+            dist[i][i] = 0        
+        for u, v, w in edges:
             dist[u][v] = w
-            dist[v][u] = w
-        
+            dist[v][u] = w        
         for k in range(n):
             for i in range(n):
                 for j in range(n):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-        
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])        
         return dist        
