46394b2b49e4b4505d6039943d43a37d0f9e5589
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-07-26 18:00:25 +0530

    Time: 462 ms (36.40%), Space: 17.6 MB (80.04%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        ans = -1
+        minCitiesCount = n
+        dist = self.floydWarshall(n, edges, distanceThreshold)
+        
+        for i in range(n):
+            citiesCount = 0
+            for j in range(n):
+                if dist[i][j] <= distanceThreshold:
+                    citiesCount += 1
+            if citiesCount <= minCitiesCount:
+                ans = i
+                minCitiesCount = citiesCount
+        
+        return ans
+    
+    def floydWarshall(self, n, edges, distanceThreshold):
+        dist = [[distanceThreshold + 1] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+        
+        for edge in edges:
+            u, v, w = edge
+            dist[u][v] = w
+            dist[v][u] = w
+        
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        
+        return dist        
