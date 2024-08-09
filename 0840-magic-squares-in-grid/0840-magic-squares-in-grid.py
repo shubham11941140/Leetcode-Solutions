da54d787b54c2e2035d6dcb120516b128706805a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-08-09 12:52:09 +0530

    Time: 31 ms (97.44%), Space: 16.6 MB (22.56%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        def isMagic(i, j):
+            s = set()
+            for x in range(3):
+                for y in range(3):
+                    if grid[i + x][j + y] < 1 or grid[i + x][j + y] > 9:
+                        return False
+                    s.add(grid[i + x][j + y])
+            if len(s) != 9:
+                return False
+            return (grid[i][j] + grid[i][j + 1] + grid[i][j + 2] == 15 and
+                    grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] == 15 and
+                    grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] == 15 and
+                    grid[i][j] + grid[i + 1][j] + grid[i + 2][j] == 15 and
+                    grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] == 15 and
+                    grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2] == 15 and
+                    grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] == 15 and
+                    grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] == 15)
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+        for i in range(rows - 2):
+            for j in range(cols - 2):
+                if isMagic(i, j):
+                    count += 1
+        return count
+        
