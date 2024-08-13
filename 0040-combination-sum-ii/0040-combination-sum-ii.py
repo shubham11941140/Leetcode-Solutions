3f1a8243f3a7491debfc89fc1accabf34ff5d55b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-08-13 17:13:53 +0530

    Time: 37 ms (97.98%), Space: 16.6 MB (63.92%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,30 +1,4 @@
-from collections import Counter
-
-class Solution:
-    
-    def rec(self, cursum, target, a, cand, n, ans, idx):
-        if cursum == target:
-            b = sorted(a.copy())
-            if b not in ans:
-                ans.append(b)
-            return
-        else:
-            for i in range(idx, n):
-                a.append(cand[i])
-                cursum += cand[i]
-                if cursum <= target:
-                    self.rec(cursum, target, a, cand, n, ans, idx + 1)
-                cursum -= cand[i]
-                a.pop()
-                
-    def check(self, a, b):
-        d1 = Counter(a)
-        d2 = Counter(b)
-        for i in d2:
-            if d2[i] > d1[i]:
-                return False
-        return True
-            
+class Solution:                
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def backtrack(start, target, path):
             if target == 0:
@@ -35,6 +9,8 @@ class Solution:
             for i in range(start, len(candidates)):
                 if i > start and candidates[i] == candidates[i - 1]:
                     continue
+                if candidates[i] > target:
+                    return
                 backtrack(i + 1, target - candidates[i], path + [candidates[i]])
 
         candidates.sort()
