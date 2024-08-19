98c53a41ed32571c67894703a368f4e83d0ac56a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-08-19 19:00:04 +0530

    Time: 209 ms (36.44%), Space: 16.3 MB (100.00%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,27 +1,12 @@
-class Solution:
-    
-    def __init__(self):
-        self.d = [1000 for i in range(1015)]
-        self.d[1] = 0
-        for i in range(2, 1010):
-            for j in range(2, i + 1):
-                if i == j:
-                    self.d[i] = min(self.d[i], i)
-                if i % j == 0:
-                    self.d[i] = min(self.d[i], self.d[j] + (i // j))        
-        
+class Solution:    
     def minSteps(self, n: int) -> int:     
         if n == 1:
             return 0
-
         dp = [0] * (n + 1)
-
         for i in range(2, n + 1):
             dp[i] = i  # Initialize with the maximum steps (all 'Paste' operations)
             for j in range(1, i // 2 + 1):
-                if i % j == 0:
+                if not (i % j):
                     dp[i] = min(dp[i], dp[j] + i // j)
-
-        return dp[n]        
-        return self.d[n]                    
+        return dp[n]                      
         
