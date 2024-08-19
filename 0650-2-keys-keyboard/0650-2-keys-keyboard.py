cdbb3fccebea7e902d17f1bb0de2451fbdab5b7c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-08-19 18:59:29 +0530

    Time: 2264 ms (5.01%), Space: 16.5 MB (61.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -10,6 +10,18 @@ class Solution:
                 if i % j == 0:
                     self.d[i] = min(self.d[i], self.d[j] + (i // j))        
         
-    def minSteps(self, n: int) -> int:        
+    def minSteps(self, n: int) -> int:     
+        if n == 1:
+            return 0
+
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = i  # Initialize with the maximum steps (all 'Paste' operations)
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = min(dp[i], dp[j] + i // j)
+
+        return dp[n]        
         return self.d[n]                    
         
