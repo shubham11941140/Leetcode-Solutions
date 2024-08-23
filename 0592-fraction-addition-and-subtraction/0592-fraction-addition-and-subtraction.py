e07f4779f9b48b8856981d54eb3c95b3dd38f7c1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-08-23 16:13:07 +0530

    Time: 39 ms (34.11%), Space: 16.5 MB (89.53%) - LeetHub

---
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -0,0 +1,7 @@
+from fractions import Fraction
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        fractions = expression.replace('-', '+-').split('+')
+        result = sum(Fraction(f) for f in fractions if f)
+        return f"{result.numerator}/{result.denominator}"        
