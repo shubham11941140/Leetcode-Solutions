c02998a94724a172a287470b8f66fbef123cb83a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-02 00:45:42 +0530

    Time: 886 ms (5.18%), Space: 36 MB (6.97%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -1,6 +1,4 @@
 import numpy
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        if len(original) != m * n:
-            return []  # Return an empty array if it's impossible to reshape
-        return numpy.reshape(original, (m, n)).tolist()        
+        return [] if len(original) != m * n else numpy.reshape(original, (m, n)).tolist() 
