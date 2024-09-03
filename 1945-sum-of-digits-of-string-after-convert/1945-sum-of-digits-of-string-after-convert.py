6cd99602192b6a513fd6877416ac2b26758f8f0e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-03 23:30:05 +0530

    Time: 45 ms (11.32%), Space: 16.5 MB (37.00%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -3,15 +3,11 @@ class Solution:
         # Convert the string to a number formed by the position in the alphabet
         num_str = ''.join(str(ord(char) - ord('a') + 1) for char in s)
 
-        # Function to sum digits of a number in string form
-        def sum_of_digits(n: str) -> int:
-            return sum(int(char) for char in n)
-
         # Start with the massive number string
         curr_str = num_str
 
         # Perform the sum of digits process k times
         for _ in range(k):
-            curr_str = str(sum_of_digits(curr_str))
+            curr_str = str(sum(int(char) for char in curr_str))
 
         return int(curr_str)        
