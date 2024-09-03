de3331fe8cec291f75dd6b4848ebb4741157ce30
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-03 23:30:45 +0530

    Time: 36 ms (73.05%), Space: 16.5 MB (37.00%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -1,13 +1,6 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
-        # Convert the string to a number formed by the position in the alphabet
-        num_str = ''.join(str(ord(char) - ord('a') + 1) for char in s)
-
-        # Start with the massive number string
-        curr_str = num_str
-
-        # Perform the sum of digits process k times
+        curr_str = ''.join(str(ord(char) - ord('a') + 1) for char in s)
         for _ in range(k):
             curr_str = str(sum(int(char) for char in curr_str))
-
         return int(curr_str)        
