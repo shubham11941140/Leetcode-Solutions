c21372022386f02d550fbb13562af4a1088bbaa8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-05 22:19:34 +0530

    Time: 995 ms (53.74%), Space: 26.4 MB (80.48%) - LeetHub

---
+++ 2028-find-missing-observations/2028-find-missing-observations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        total_sum = mean * (n + m)
+        current_sum = sum(rolls)
+        missing_sum = total_sum - current_sum
+
+        if missing_sum < n or missing_sum > 6 * n:
+            return []
+
+        quotient, remainder = divmod(missing_sum, n)
+        result = [quotient] * n
+
+        for i in range(remainder):
+            result[i] += 1
+
+        return result        
