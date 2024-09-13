613ec3d5fbe98b2246b56a5466c2ac7638536e4a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-13 16:09:01 +0530

    Time: 329 ms (6.93%), Space: 31.4 MB (34.46%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -3,10 +3,4 @@ class Solution:
         prefix_xor = [0] * (len(arr) + 1)
         for i in range(len(arr)):
             prefix_xor[i + 1] = prefix_xor[i] ^ arr[i]
-        
-        # Answer each query
-        result = []
-        for left, right in queries:
-            result.append(prefix_xor[right + 1] ^ prefix_xor[left])
-        
-        return result        
+        return [prefix_xor[right + 1] ^ prefix_xor[left] for left, right in queries]      
