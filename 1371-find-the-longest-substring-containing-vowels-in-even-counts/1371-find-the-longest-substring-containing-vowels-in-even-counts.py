70efe9f08d2efe8092c9c8fe9b9e7923b4f840b1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-15 12:58:57 +0530

    Time: 308 ms (45.12%), Space: 22.1 MB (79.11%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        state_to_index = {0: -1}
+        vowels = 'aeiou'
+        state = 0
+        max_length = 0
+
+        for i, char in enumerate(s):
+            if char in vowels:
+                # Toggle the bit corresponding to the vowel
+                state ^= 1 << vowels.index(char)
+
+            if state in state_to_index:
+                # Calculate the length of the substring
+                max_length = max(max_length, i - state_to_index[state])
+            else:
+                # Store the first occurrence of this state
+                state_to_index[state] = i
+
+        return max_length        
