e3f2a9de40c47f2876e3ab0b2851b26d13839e1e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-16 19:54:03 +0530

    Time: 74 ms (40.18%), Space: 19.6 MB (36.73%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -4,18 +4,5 @@ class Solution:
         for time in timePoints:
             h, m = map(int, time.split(':'))
             minutes.append(h * 60 + m)
-
-        # Sort the minutes
         minutes.sort()
-
-        # Initialize the minimum difference to a large number
-        min_diff = float('inf')
-
-        # Find the minimum difference between consecutive times
-        for i in range(1, len(minutes)):
-            min_diff = min(min_diff, minutes[i] - minutes[i - 1])
-
-        # Check the difference between the first and last time points (circular difference)
-        min_diff = min(min_diff, 1440 + minutes[0] - minutes[-1])
-
-        return min_diff        
+        return min(min([minutes[i] - minutes[i - 1] for i in range(1, len(minutes))]), 1440 + minutes[0] - minutes[-1])       
