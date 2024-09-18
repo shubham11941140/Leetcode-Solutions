74db4776d41a3baf0f0fcc8ef600b350e489374a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-18 12:34:27 +0530

    Time: 40 ms (62.12%), Space: 16.6 MB (32.99%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -7,15 +7,6 @@ class Solution:
             elif x + y < y + x:
                 return 1
             else:
-                return 0
-
-        # Convert all numbers to strings for easy concatenation
-        nums = list(map(str, nums))
-        # Sort the numbers using the custom comparator
-        nums.sort(key=cmp_to_key(compare))
-
-        # Join the sorted numbers into a single string
-        largest_num = ''.join(nums)
-
-        # Edge case: if the largest number is '0', return '0'
+                return 0     
+        largest_num = ''.join(sorted(list(map(str, nums)), key = cmp_to_key(compare)))
         return '0' if largest_num[0] == '0' else largest_num        
