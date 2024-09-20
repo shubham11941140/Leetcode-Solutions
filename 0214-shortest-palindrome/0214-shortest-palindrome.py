98f0b4638b73a763a751825813b328bc4d72ab6b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-20 12:08:36 +0530

    Time: 65 ms (64.09%), Space: 20.7 MB (19.39%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,18 +2,14 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         rev_s = s[::-1]
         new_s = s + "#" + rev_s
-
         # Compute the KMP table (partial match table)
         n = len(new_s)
         lps = [0] * n
         j = 0  # length of the previous longest prefix suffix
-
         for i in range(1, n):
             while (j > 0 and new_s[i] != new_s[j]):
                 j = lps[j - 1]
-
             if new_s[i] == new_s[j]:
                 j += 1
                 lps[i] = j
-
         return rev_s[:len(s) - lps[-1]] + s        
