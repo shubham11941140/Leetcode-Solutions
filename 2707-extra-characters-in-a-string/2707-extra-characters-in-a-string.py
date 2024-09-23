3553f5969c47d6c690ec2a764fbacfef9c8d69bb
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-23 12:33:02 +0530

    Time: 192 ms (68.48%), Space: 16.5 MB (83.01%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -4,10 +4,8 @@ class Solution:
         dp = [0] + [n] * (n - 1)        
         w = set(dictionary)        
         for i in range(1, n):
-            dp[i] = dp[i - 1] + 1
-            
+            dp[i] = dp[i - 1] + 1            
             for l in range(1, i + 1):
                 if s[i - l:i] in w:
-                    dp[i] = min(dp[i], dp[i - l])
-                    
+                    dp[i] = min(dp[i], dp[i - l])                    
         return dp[-1]
