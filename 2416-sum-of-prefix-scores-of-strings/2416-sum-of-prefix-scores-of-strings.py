193322edf87f08e4c7b8b399c1c01b6b6e0c4c66
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-09-25 12:46:36 +0530

    Time: 1966 ms (45.86%), Space: 305.4 MB (80.34%) - LeetHub

---
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.count += 1
+
+    def get_prefix_score(self, word):
+        node = self.root
+        score = 0
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+                score += node.count
+            else:
+                break
+        return score
+
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        
+        result = []
+        for word in words:
+            result.append(trie.get_prefix_score(word))
+        
+        return result        
