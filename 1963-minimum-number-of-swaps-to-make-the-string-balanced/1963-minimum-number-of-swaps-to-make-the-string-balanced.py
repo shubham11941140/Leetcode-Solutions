28b6a090d916a808728cbc24500b48c416398369
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-08 19:25:50 +0530

    Time: 159 ms (97.14%), Space: 28 MB (45.06%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        unbalanced = 0
+        for char in s:
+            if char == '[':
+                unbalanced += 1
+            elif unbalanced > 0:
+                unbalanced -= 1
+        return (unbalanced + 1) // 2        
