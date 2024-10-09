7cd42151441cdedc239a0c1d52a066fcc9833501
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-09 11:36:23 +0530

    Time: 24 ms (97.70%), Space: 16.5 MB (71.14%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        additions_needed = unmatched_open = 0
+        for char in s:
+            if char == '(':
+                unmatched_open += 1
+            else:
+                if unmatched_open > 0:
+                    unmatched_open -= 1
+                else:
+                    additions_needed += 1
+        return additions_needed + unmatched_open        
