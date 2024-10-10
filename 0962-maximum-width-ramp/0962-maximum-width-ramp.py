9cfd0308c90a761121314ee27d63180d0d1df866
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-10 13:31:47 +0530

    Time: 359 ms (38.45%), Space: 23.7 MB (40.43%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -1,17 +1,12 @@
 class Solution:
-    def maxWidthRamp(self, nums: List[int]) -> int:
-        A = nums.copy()
+    def maxWidthRamp(self, nums: List[int]) -> int:        
         stack = []
         max_width = 0
-        
-        # Build a stack of indices where A[i] is in decreasing order
-        for i in range(len(A)):
-            if not stack or A[stack[-1]] > A[i]:
+        n = len(nums)
+        for i in range(n):
+            if not stack or nums[stack[-1]] > nums[i]:
                 stack.append(i)
-        
-        # Traverse the array from the end to the start
-        for j in range(len(A) - 1, -1, -1):
-            while stack and A[stack[-1]] <= A[j]:
-                max_width = max(max_width, j - stack.pop())
-        
+        for j in reversed(range(n)):
+            while stack and nums[stack[-1]] <= nums[j]:
+                max_width = max(max_width, j - stack.pop())        
         return max_width        
