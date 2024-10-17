4b22fd22ccfd5e2d78548702ea6cf2ab33cc8f5e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-17 13:24:53 +0530

    Time: 37 ms (38.86%), Space: 16.4 MB (79.76%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        digits = list(str(num))
+        max_num = num
+        # Try swapping each pair of digits and keep track of the max number
+        for i in range(len(digits)):
+            for j in range(i + 1, len(digits)):
+                # Swap digits
+                digits[i], digits[j] = digits[j], digits[i]
+                # Convert list of digits back to number
+                new_num = int(''.join(digits))
+                # Update max number if new number is greater
+                if new_num > max_num:
+                    max_num = new_num
+                # Swap back to original positions
+                digits[i], digits[j] = digits[j], digits[i]
+        return max_num    
