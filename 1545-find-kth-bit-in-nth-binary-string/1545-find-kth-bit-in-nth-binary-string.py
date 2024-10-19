658c539f3d7a8fa9da8a47e84e774616ff2e24c5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-19 15:55:47 +0530

    Time: 386 ms (36.49%), Space: 25.7 MB (17.16%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -4,10 +4,7 @@ class Solution:
             if n == 1:
                 return "0"
             prev = build_string(n - 1)
-            inverted = ''.join('1' if c == '0' else '0' for c in prev)
-            return prev + "1" + inverted[::-1]
-
-        binary_string = build_string(n)
-        return binary_string[k - 1]
+            return prev + "1" + ''.join('1' if c == '0' else '0' for c in prev)[::-1]
+        return build_string(n)[k - 1]
 
         
