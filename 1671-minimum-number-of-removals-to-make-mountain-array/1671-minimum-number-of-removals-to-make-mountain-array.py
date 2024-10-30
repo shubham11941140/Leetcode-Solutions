51587b07f1b04b2639cb719317a8616f6bd09ea3
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-30 11:24:25 +0530

    Time: 1885 ms (32.56%), Space: 16.8 MB (31.03%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -3,24 +3,17 @@ class Solution:
         n = len(nums)
         left = [1] * n
         right = [1] * n
-
-        # Calculate the longest increasing subsequence ending at each index
         for i in range(1, n):
             for j in range(i):
                 if nums[i] > nums[j]:
                     left[i] = max(left[i], left[j] + 1)
-
-        # Calculate the longest decreasing subsequence starting at each index
-        for i in range(n-2, -1, -1):
-            for j in range(n-1, i, -1):
+        for i in reversed(range(n - 1)):
+            for j in range(n - 1, i, -1):
                 if nums[i] > nums[j]:
                     right[i] = max(right[i], right[j] + 1)
-
-        # Find the maximum length of mountain array
         max_length = 0
-        for i in range(1, n-1):
+        for i in range(1, n - 1):
             if left[i] > 1 and right[i] > 1:
                 max_length = max(max_length, left[i] + right[i] - 1)
-
         return n - max_length
         
