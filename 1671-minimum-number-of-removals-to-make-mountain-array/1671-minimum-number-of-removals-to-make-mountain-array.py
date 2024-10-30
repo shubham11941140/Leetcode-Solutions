39a6dfe7879f55328301917a91566d727d975dda
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-30 11:26:14 +0530

    Time: 1950 ms (16.28%), Space: 17 MB (13.17%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,15 +1,15 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         n = len(nums)
-        left = [1] * n
-        right = [1] * n
+        l = [1] * n
+        r = [1] * n
         for i in range(1, n):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    left[i] = max(left[i], left[j] + 1)
+                    l[i] = max(l[i], l[j] + 1)
         for i in reversed(range(n - 1)):
             for j in range(n - 1, i, -1):
                 if nums[i] > nums[j]:
-                    right[i] = max(right[i], right[j] + 1)        
-        return n - max(left[i] + right[i] - 1 for i in range(1, n - 1) if left[i] > 1 and right[i] > 1)
+                    r[i] = max(r[i], r[j] + 1)        
+        return n - max(l[i] + r[i] - 1 for i in range(1, n - 1) if l[i] > 1 and r[i] > 1)
         
