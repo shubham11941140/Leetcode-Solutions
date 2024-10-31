50a0c69aa2fe2979e0fefdf6a1a7d9afd2003c6a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-10-31 17:55:49 +0530

    Time: 869 ms (84.00%), Space: 25 MB (42.86%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -3,25 +3,22 @@ class Solution:
         robot.sort()
         factory.sort()
         fp = [f[0] for f in factory]
-        factory_limits = [f[1] for f in factory]
+        fl = [f[1] for f in factory]
         
         # Memoized recursive function
         @lru_cache(None)
-        def dp(robot_idx: int, factory_idx: int) -> int:
-            if robot_idx == len(robot):
+        def dp(ri: int, fi: int) -> int:
+            if ri == len(robot):
                 return 0
-            # If we run out of factories, return an arbitrarily large number (infeasible path)
-            if factory_idx == len(fp):
+            if fi == len(fp):
                 return float('inf')
-            min_distance = dp(robot_idx, factory_idx + 1)
-            
-            # Option 2: Assign robots to this factory up to its limit
-            total_distance = 0
-            for i in range(factory_limits[factory_idx]):
-                if robot_idx + i < len(robot):
-                    total_distance += abs(robot[robot_idx + i] - fp[factory_idx])
-                    min_distance = min(min_distance, total_distance + dp(robot_idx + i + 1, factory_idx + 1))
+            m = dp(ri, fi + 1)
+            t = 0
+            for i in range(fl[fi]):
+                if ri + i < len(robot):
+                    t += abs(robot[ri + i] - fp[fi])
+                    m = min(m, t + dp(ri + i + 1, fi + 1))
                 else:
                     break            
-            return min_distance
+            return m
         return dp(0, 0)        
