fd45b5ce8f0bb126c5f31abf2fc3af20f1a96d3d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-01 16:03:59 +0530

    Time: 383 ms (20.72%), Space: 18.6 MB (55.17%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,8 +1,5 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        result = []
-        for i, char in enumerate(s):
-            if not (i > 1 and s[i] == s[i - 1] == s[i - 2]):
-                result.append(char)
-        return ''.join(result)
+        return ''.join([c for i, c in enumerate(s) if not (i > 1 and s[i] == s[i - 1] == s[i - 2])])
+
         
