01e368209824615ceaba62a295527995e26f4471
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-08 12:21:00 +0530

    Time: 59 ms (53.57%), Space: 32.1 MB (92.44%) - LeetHub

---
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        max_val = (1 << maximumBit) - 1
+        current_xor = 0
+        result = []
+        for num in nums:
+            current_xor ^= num
+        for i in range(len(nums)):
+            result.append(current_xor ^ max_val)
+            current_xor ^= nums[-(i + 1)]
+        return result
+        
