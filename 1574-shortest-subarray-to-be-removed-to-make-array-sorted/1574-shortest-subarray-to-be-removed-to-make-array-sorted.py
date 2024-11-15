84c129b569168c3adb6edaf1bea45b1467f7ed7f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-15 11:19:07 +0530

    Time: 8 ms (93.69%), Space: 30.4 MB (90.65%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -4,16 +4,12 @@ class Solution:
         left = 0
         while left + 1 < n and arr[left] <= arr[left + 1]:
             left += 1
-
         if left == n - 1:
             return 0
-
         right = n - 1
         while right > left and arr[right - 1] <= arr[right]:
             right -= 1
-
         result = min(n - left - 1, right)
-
         i = 0
         j = right
         while i <= left and j < n:
@@ -22,5 +18,4 @@ class Solution:
                 i += 1
             else:
                 j += 1
-
         return result        
