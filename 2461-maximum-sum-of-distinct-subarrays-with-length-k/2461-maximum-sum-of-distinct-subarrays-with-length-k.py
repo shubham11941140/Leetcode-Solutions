557148b35ed68aea8f4ba4cb21f454fb9b462d5b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-19 23:06:45 +0530

    Time: 99 ms (82.81%), Space: 36.6 MB (10.47%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -2,25 +2,19 @@ class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         if len(nums) < k:
             return 0
-
         max_sum = 0
         window_sum = 0
-        element_count = {}
-
+        ec = {}
         for i in range(len(nums)):
-            # Add the current element to the window
-            if nums[i] in element_count:
-                element_count[nums[i]] += 1
-            else:
-                element_count[nums[i]] = 1
+            ec[nums[i]] = ec[nums[i]] + 1 if nums[i] in ec else 1
             window_sum += nums[i]
             if i >= k:
                 left_elem = nums[i - k]
                 window_sum -= left_elem
-                if element_count[left_elem] == 1:
-                    del element_count[left_elem]
+                if ec[left_elem] == 1:
+                    del ec[left_elem]
                 else:
-                    element_count[left_elem] -= 1
-            if len(element_count) == k:
+                    ec[left_elem] -= 1
+            if len(ec) == k:
                 max_sum = max(max_sum, window_sum)
         return max_sum        
