8e0021f548813f0f9a3f9df4eecc36aef4e9756f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-19 23:05:15 +0530

    Time: 106 ms (72.16%), Space: 36.2 MB (38.35%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,32 +1,26 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        arr = nums
-        if len(arr) < k:
+        if len(nums) < k:
             return 0
 
         max_sum = 0
         window_sum = 0
         element_count = {}
 
-        for i in range(len(arr)):
+        for i in range(len(nums)):
             # Add the current element to the window
-            if arr[i] in element_count:
-                element_count[arr[i]] += 1
+            if nums[i] in element_count:
+                element_count[nums[i]] += 1
             else:
-                element_count[arr[i]] = 1
-            window_sum += arr[i]
-
-            # If the window size exceeds k, remove the leftmost element
+                element_count[nums[i]] = 1
+            window_sum += nums[i]
             if i >= k:
-                left_elem = arr[i - k]
+                left_elem = nums[i - k]
                 window_sum -= left_elem
                 if element_count[left_elem] == 1:
                     del element_count[left_elem]
                 else:
                     element_count[left_elem] -= 1
-
-            # Update max_sum if current window has k distinct elements
             if len(element_count) == k:
                 max_sum = max(max_sum, window_sum)
-
         return max_sum        
