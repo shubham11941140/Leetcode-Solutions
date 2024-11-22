ad67f2862384c81af059fb3ae6e9373782008e9a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-22 12:43:11 +0530

    Time: 59 ms (71.30%), Space: 19.3 MB (49.75%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,17 +1,7 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        from collections import defaultdict
-
-        def rowToPattern(row):
-            # Create a pattern string where flipping columns will result in the same row
-            first_val = row[0]
-            return ''.join('1' if x == first_val else '0' for x in row)
-
         pattern_count = defaultdict(int)
-
         for row in matrix:
-            pattern = rowToPattern(row)
-            pattern_count[pattern] += 1
-
+            pattern_count[''.join('1' if x == row[0] else '0' for x in row)] += 1
         return max(pattern_count.values())
         
