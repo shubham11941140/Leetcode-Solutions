7f0d341a679b28acdbe6b4e62aa03f0a258af56b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-28 12:19:50 +0530

    Time: 976 ms (88.07%), Space: 39.7 MB (92.80%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,21 +1,16 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         pq = [(0, 0, 0)]  # (obstacles_removed, x, y)
         visited = [[False] * n for _ in range(m)]
         visited[0][0] = True
-
         while pq:
-            obstacles_removed, x, y = heapq.heappop(pq)
-
+            obstacles_removed, x, y = heappop(pq)
             if x == m - 1 and y == n - 1:
                 return obstacles_removed
-
-            for dx, dy in directions:
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny]:
                     visited[nx][ny] = True
-                    heapq.heappush(pq, (obstacles_removed + grid[nx][ny], nx, ny))
-
+                    heappush(pq, (obstacles_removed + grid[nx][ny], nx, ny))
         return -1        
