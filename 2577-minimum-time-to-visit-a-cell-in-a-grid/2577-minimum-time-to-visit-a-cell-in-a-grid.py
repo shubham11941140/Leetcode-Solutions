c7bda915655cc1c704196f6fcfac8ac7165884c9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-11-29 12:34:14 +0530

    Time: 975 ms (57.08%), Space: 44.2 MB (16.06%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,30 +1,21 @@
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         heap = [(grid[0][0], 0, 0)]
         visit = set()
         visit.add((0, 0))
-
         if grid[0][1] > 1 and grid[1][0] > 1:
             return -1
-
         while heap:
-            time, r, c = heapq.heappop(heap)
-
+            time, r, c = heappop(heap)
             if r == ROWS - 1 and c == COLS - 1:
                 return time
-
-            for dr, dc in directions:
+            for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < ROWS and 0 <= nc < COLS and (nr, nc) not in visit:
                     visit.add((nr, nc))
                     if grid[nr][nc] <= time + 1:
-                        heapq.heappush(heap, [time + 1, nr, nc])
+                        heappush(heap, [time + 1, nr, nc])
                     else:
-                        if (grid[nr][nc] - time) % 2 == 0:
-                            heapq.heappush(heap, [grid[nr][nc] + 1, nr, nc])
-                        else:
-                            heapq.heappush(heap, [grid[nr][nc], nr, nc])
-                        
+                        heappush(heap, [grid[nr][nc] + (0 if (grid[nr][nc] - time) % 2 else 1), nr, nc])                                                    
         return -1        
