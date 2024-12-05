1f421bf46d291f8348422b1664c82b4833aecb84
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-05 17:59:25 +0530

    Time: 169 ms (38.71%), Space: 20 MB (34.13%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        s = [ch for ch in start if ch != '_']
+        t = [ch for ch in target if ch != '_']
+        if s != t:
+            return False
+        # Check L's positions
+        j = 0
+        for i in range(len(start)):
+            if start[i] == 'L':
+                while target[j] != 'L':
+                    j += 1
+                if i < j:
+                    return False
+                j += 1
+        # Check R's positions
+        j = 0
+        for i in range(len(start)):
+            if start[i] == 'R':
+                while target[j] != 'R':
+                    j += 1
+                if i > j:
+                    return False
+                j += 1
+        return True
