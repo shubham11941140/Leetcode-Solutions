af921dbef3c775ea5199a6f854fae7a7a9142db4
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-10 00:05:13 +0530

    Time: 39 ms (77.78%), Space: 46.5 MB (53.04%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        n = len(nums)
+        parity = [num % 2 for num in nums]
+
+        # Compute prefix sum of parity changes
+        prefix_sum = [0] * (n + 1)
+        for i in range(1, n):
+            prefix_sum[i] = prefix_sum[i - 1] + (parity[i] != parity[i - 1])
+
+        result = []
+        for from_i, to_i in queries:
+            if prefix_sum[to_i] - prefix_sum[from_i] == to_i - from_i:
+                result.append(True)
+            else:
+                result.append(False)
+
+        return result
