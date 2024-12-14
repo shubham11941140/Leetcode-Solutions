eaed6f0ba4f7366881a8f6f79a92862b0ffcb061
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-14 17:15:06 +0530

    Time: 1381 ms (5.12%), Space: 28.1 MB (14.61%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.py
@@ -0,0 +1,19 @@
+from sortedcontainers import SortedList
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        sl = SortedList([])
+        nums.append(-5)
+        start = 0
+        sl.add(nums[start])
+        count = 0
+        for i in range(1, len(nums)):
+            added = nums[i]
+            sl.add(added)
+            while abs(added - sl[0]) > 2 or abs(added - sl[-1]) > 2:
+                subArrays = len(sl) - 1
+                count += subArrays
+                # Remove the leftmost element from the window
+                sl.remove(nums[start])
+                # Move the sliding window start position
+                start += 1
+        return count        
