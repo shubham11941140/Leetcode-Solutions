44562ca15cd7ef707264780e1484a757b23d055e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-15 22:27:48 +0530

    Time: 1058 ms (66.67%), Space: 54.3 MB (37.04%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -1,10 +1,7 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        # Function to calculate the improvement in pass ratio if an additional student is added
-        def improvement(passed, total):
-            return (passed + 1) / (total + 1) - passed / total
         # Initialize a max-heap with the negative improvement and the corresponding class data
-        heap = [(-improvement(p, t), p, t) for p, t in classes]
+        heap = [(-((p + 1) / (t + 1) - (p / t)), p, t) for p, t in classes]
         heapify(heap)
         # Distribute the extra students to maximize the overall pass ratio
         for _ in range(extraStudents):
