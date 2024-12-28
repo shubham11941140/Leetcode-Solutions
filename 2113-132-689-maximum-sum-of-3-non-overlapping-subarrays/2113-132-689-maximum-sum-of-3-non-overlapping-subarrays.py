16d9a499c258fa2a0f4a171459dff0d1ea2dd536
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-28 12:03:11 +0530

    Time: 27 ms, Memory: 20.3 MB - LeetHub

---
+++ 2113-132-689-maximum-sum-of-3-non-overlapping-subarrays/2113-132-689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        sums = [0] * (n + 1)
+        for i in range(n):
+            sums[i + 1] = sums[i] + nums[i]
+        
+        left = [0] * n
+        total = sums[k] - sums[0]
+        for i in range(k, n):
+            if sums[i + 1] - sums[i + 1 - k] > total:
+                total = sums[i + 1] - sums[i + 1 - k]
+                left[i] = i + 1 - k
+            else:
+                left[i] = left[i - 1]
+
+        right = [0] * n
+        total = sums[n] - sums[n - k]
+        right[n - k] = n - k
+        for i in range(n - k - 1, -1, -1):
+            if sums[i + k] - sums[i] >= total:
+                total = sums[i + k] - sums[i]
+                right[i] = i
+            else:
+                right[i] = right[i + 1]
+        
+        max_sum = 0
+        result = [-1, -1, -1]
+        for i in range(k, n - 2 * k + 1):
+            l, r = left[i - 1], right[i + k]
+            total = (sums[i + k] - sums[i]) + (sums[l + k] - sums[l]) + (sums[r + k] - sums[r])
+            if total > max_sum:
+                max_sum = total
+                result = [l, i, r]
+        
+        return result      
