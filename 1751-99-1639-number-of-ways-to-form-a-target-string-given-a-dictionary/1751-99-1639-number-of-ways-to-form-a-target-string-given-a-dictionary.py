e949568f37318337522f42e36ccb432395cf03cd
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2024-12-29 16:26:55 +0530

    Time: 810 ms, Memory: 23.4 MB - LeetHub

---
+++ 1751-99-1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1751-99-1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        m, n = len(words), len(words[0])
+        dp = [0] * (len(target) + 1)
+        dp[0] = 1
+        for i in range(n):
+            cnt = Counter([word[i] for word in words])
+            for j in reversed(range(len(target))):
+                dp[j + 1] += dp[j] * cnt[target[j]] % (10**9 + 7)
+        return dp[-1] % (10**9 + 7)    
