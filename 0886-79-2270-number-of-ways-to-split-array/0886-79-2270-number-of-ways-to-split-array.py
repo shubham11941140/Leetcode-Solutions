42701b0a0d44d329771caee0488c9ae290646fb2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-01-03 17:25:54 +0530

    Time: 83 ms, Memory: 35 MB - LeetHub

--- 0886-79-2270-number-of-ways-to-split-array/0886-79-2270-number-of-ways-to-split-array.py
+++ 0886-79-2270-number-of-ways-to-split-array/0886-79-2270-number-of-ways-to-split-array.py
@@ -6,10 +6,4 @@ class Solution:
         # Calculate the prefix sum array
         for i in range(1, n):
             prefix_sum[i] = prefix_sum[i - 1] + nums[i]        
-        total_sum = prefix_sum[-1]
-        count = 0        
-        # Iterate through the array to find the valid splits
-        for i in range(n - 1):
-            if 2 * prefix_sum[i] >= total_sum:
-                count += 1        
-        return count   
+        return len([i for i in range(n - 1) if 2 * prefix_sum[i] >= prefix_sum[-1]])  
