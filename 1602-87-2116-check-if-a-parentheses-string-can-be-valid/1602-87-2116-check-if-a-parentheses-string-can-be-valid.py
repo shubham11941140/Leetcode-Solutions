b0bae771a35c8d67182aa6648237f5e2c2e3ba27
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-01-12 18:48:01 +0530

    Time: 112 ms, Memory: 18.5 MB - LeetHub

---
+++ 1602-87-2116-check-if-a-parentheses-string-can-be-valid/1602-87-2116-check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        n = len(s)
+        if n % 2:
+            # If the length of s is odd, it's impossible to have a valid parentheses string
+            return False
+        # Check the balance of the parentheses from left to right
+        oc = 0
+        for i in range(n):
+            oc += (1 if s[i] == '(' or locked[i] == '0' else -1)
+            if oc < 0:
+                return False
+        # Check the balance of the parentheses from right to left
+        cc = 0
+        for i in reversed(range(n)):
+            cc += (1 if s[i] == ')' or locked[i] == '0' else -1)
+            if cc < 0:
+                return False
+        return True 
