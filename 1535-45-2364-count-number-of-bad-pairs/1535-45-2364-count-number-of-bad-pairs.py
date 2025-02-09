3178b6f1342500e8ad1799086bfbc77f817bd8d7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-02-09 22:41:50 +0530

    Time: 89 ms, Memory: 38.9 MB - LeetHub

---
+++ 1535-45-2364-count-number-of-bad-pairs/1535-45-2364-count-number-of-bad-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
+        total_pairs = 0
+        good_pairs = 0
+        for i, num in enumerate(nums):
+            diff = i - num
+            total_pairs += i
+            good_pairs += freq[diff]
+            freq[diff] += 1
+        return total_pairs - good_pairs   
