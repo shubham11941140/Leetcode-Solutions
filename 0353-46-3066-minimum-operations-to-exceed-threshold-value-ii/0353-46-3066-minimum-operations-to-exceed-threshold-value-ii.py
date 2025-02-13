3eeccfbe8be2ff91a9b0dc2550119f9633c1c517
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-02-13 15:42:41 +0530

    Time: 210 ms, Memory: 35.5 MB - LeetHub

---
+++ 0353-46-3066-minimum-operations-to-exceed-threshold-value-ii/0353-46-3066-minimum-operations-to-exceed-threshold-value-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapify(nums)
+        res = 0
+        for i in range(len(nums)):
+            x = heappop(nums)
+            if x < k:
+                res += 1
+                y = heappop(nums)
+                val = x * 2 + y if x < y else y * 2 + x
+                heappush(nums, val)
+            else:
+                break
+        return res   
