bc4e8321c526f0b622bb82ab9fb35ab5979bc8f8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-02-17 22:43:48 +0530

    Time: 67 ms, Memory: 19.2 MB - LeetHub

---
+++ 2920-80-1079-letter-tile-possibilities/2920-80-1079-letter-tile-possibilities.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        def backtrack(tiles, path, used, result):
+            if path:
+                result.add(path)
+            for i in range(len(tiles)):
+                if used[i]:
+                    continue
+                used[i] = True
+                backtrack(tiles, path + tiles[i], used, result)
+                used[i] = False        
+        result = set()
+        backtrack(tiles, "", [False] * len(tiles), result)
+        return len(result)
