de9d72fb5a67ac0a9e197a55effc0da74d624d9d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-03-07 10:35:33 +0530

    Time: 11 ms, Memory: 17.7 MB - LeetHub

---
+++ 0485-43-2523-closest-prime-numbers-in-range/0485-43-2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,27 @@
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        def primeFind(val: int)->bool:
+            if val == 1: 
+                return False
+            elif val == 2: 
+                return True
+            for i in range(2, int(val ** 0.5) + 1):
+                if not (val % i):
+                    return False
+            return True
+        res = [-1,-1]
+        prime = []
+        for p in range(left, right+1):
+            if primeFind(p):
+                # if prime like [2,3], [5,7], [11,13], [17,19] and so on ... found return immediately
+                if len(prime) >= 1 and p <= prime[-1] + 2:
+                    return [prime[-1], p]
+                prime.append(p)        
+        if len(prime) < 2:
+            return res
+        minVal = 10 ** 20
+        for i in range(1, len(prime)):
+            if minVal > prime[i] - prime[i - 1]:
+                minVal = prime[i] - prime[i - 1]
+                res = [prime[i - 1], prime[i]] 
+        return res  
