2fd77f1856206206eb05b262b335d53e975585dd
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-03-10 14:08:10 +0530

    Time: 1103 ms, Memory: 18.9 MB - LeetHub

---
+++ 0407-56-3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/0407-56-3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        frequencies = [{}, {}]
+        for v in "aeiou":
+            frequencies[0][v] = 1        
+        response, currentK, vowels, extraLeft, left = 0, 0, 0, 0, 0
+        for right, rightChar in enumerate(word):
+            if rightChar in frequencies[0]:
+                frequencies[1][rightChar] = frequencies[1].get(rightChar, 0) + 1
+                if frequencies[1][rightChar] == 1:
+                    vowels += 1
+            else:
+                currentK += 1
+            while currentK > k:
+                leftChar = word[left]
+                if leftChar in frequencies[0]:
+                    frequencies[1][leftChar] -= 1
+                    if frequencies[1][leftChar] == 0:
+                        vowels -= 1
+                else:
+                    currentK -= 1
+                left += 1
+                extraLeft = 0
+
+            while vowels == 5 and currentK == k and left < right and word[left] in frequencies[0] and frequencies[1][word[left]] > 1:
+                extraLeft += 1
+                frequencies[1][word[left]] -= 1
+                left += 1
+
+            if currentK == k and vowels == 5:
+                response += (1 + extraLeft)
+
+        return response    
