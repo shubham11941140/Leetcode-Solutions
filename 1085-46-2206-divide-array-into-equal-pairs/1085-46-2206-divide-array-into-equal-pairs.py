be8700a7b2c21054a0b2515ed147c7e0ad47cd27
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-03-17 23:44:57 +0530

    Time: 2 ms, Memory: 17.8 MB - LeetHub

--- 1085-46-2206-divide-array-into-equal-pairs/1085-46-2206-divide-array-into-equal-pairs.py
+++ 1085-46-2206-divide-array-into-equal-pairs/1085-46-2206-divide-array-into-equal-pairs.py
@@ -1,3 +1,3 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        return 1 not in [v % 2 for k, v in Counter(nums).items()]        
+        return 1 not in [v % 2 for v in Counter(nums).values()]        
