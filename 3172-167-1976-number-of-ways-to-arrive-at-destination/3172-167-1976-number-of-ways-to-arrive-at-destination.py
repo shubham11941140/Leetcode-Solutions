068d369e50a310a4985cc316b082cd26e259c44a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-03-23 12:58:16 +0530

    Time: 16 ms, Memory: 25 MB - LeetHub

---
+++ 3172-167-1976-number-of-ways-to-arrive-at-destination/3172-167-1976-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        mod = 10**9 + 7
+        adj = defaultdict(list)
+        for u, v, t in roads:
+            adj[u].append((v, t))
+            adj[v].append((u, t))
+        shortesttime = [10 ** 20] * n
+        cnt = [0] * n
+        pq = [(0, 0)]  # (time, node)
+        
+        shortesttime[0] = 0
+        cnt[0] = 1
+
+        while pq:
+            time, node = heappop(pq)
+            if time > shortesttime[node]:
+                continue
+            for nbr, rtime in adj[node]:
+                if time + rtime < shortesttime[nbr]:
+                    shortesttime[nbr] = time + rtime
+                    cnt[nbr] = cnt[node]
+                    heappush(pq, (shortesttime[nbr], nbr))
+                elif time + rtime == shortesttime[nbr]:
+                    cnt[nbr] = (cnt[nbr] + cnt[node]) % mod
+        return cnt[-1]   
