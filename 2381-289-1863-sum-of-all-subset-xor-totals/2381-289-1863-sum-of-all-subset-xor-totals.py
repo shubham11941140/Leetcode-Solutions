f88d7d4f95c48a1b7ca5953269485fe5f2da54e1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-04-05 14:25:54 +0530

    Time: 59 ms, Memory: 17.6 MB - LeetHub

---
+++ 2381-289-1863-sum-of-all-subset-xor-totals/2381-289-1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def xor(binary_string):
+            total = 0
+            for i, char in enumerate(binary_string):
+                if char == '1':
+                    total = total ^ nums[i]
+            return total
+        return sum(xor(bin(i)[2:].zfill(len(nums))) for i in range(1, 1 << len(nums)))
+ 
