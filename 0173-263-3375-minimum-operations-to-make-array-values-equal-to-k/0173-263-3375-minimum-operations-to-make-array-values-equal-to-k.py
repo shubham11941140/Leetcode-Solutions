b1e31efccca2af87a42c6b2934b8f9a190f5bce4
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-04-09 14:07:03 +0530

    Time: 66 ms, Memory: 17.8 MB - LeetHub

---
+++ 0173-263-3375-minimum-operations-to-make-array-values-equal-to-k/0173-263-3375-minimum-operations-to-make-array-values-equal-to-k.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        hasX = 0
+        xMin = 101
+        for x in nums:
+            hasX |= 1 << x
+            xMin = min(x, xMin)        
+        return -1 if xMin < k else (hasX.bit_count() + (-1 if xMin == k else 0))
