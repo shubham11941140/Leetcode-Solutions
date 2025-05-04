56eddd659b2c415bac4c67fb5e5a6b187da83a8e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-05-04 23:39:05 +0530

    Time: 9 ms, Memory: 26 MB - LeetHub

---
+++ 1004-362-1128-number-of-equivalent-domino-pairs/1004-362-1128-number-of-equivalent-domino-pairs.py
@@ -0,0 +1,3 @@
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        return sum([(i * (i - 1)) // 2 for i in Counter([tuple(sorted(j)) for j in dominoes]).values()])        
