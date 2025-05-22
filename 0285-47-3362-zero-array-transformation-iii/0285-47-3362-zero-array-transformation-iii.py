0582a25b2ccbffd0c95f7d84e41800851210fc57
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-05-22 11:58:47 +0530

    Time: 286 ms, Memory: 61.9 MB - LeetHub

---
+++ 0285-47-3362-zero-array-transformation-iii/0285-47-3362-zero-array-transformation-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        queries.sort()  # Sort queries by starting index
+        available = []  # Max heap for available `r` values
+        running = []  # Min heap for running `r` values
+        query_index = 0
+
+        for i in range(len(nums)):
+            while query_index < len(queries) and queries[query_index][0] <= i:
+                heappush(available, -queries[query_index][1])  # Push negative for max heap
+                query_index += 1
+
+            while running and running[0] < i:
+                heappop(running)
+
+            while nums[i] > len(running):
+                if not available or -available[0] < i:
+                    return -1
+                heappush(running, -heappop(available))
+
+        return len(available)   
