56ed75307336e04e7b79a1983cbb1127473fdbad
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-05-30 17:58:55 +0530

    Time: 464 ms, Memory: 70 MB - LeetHub

---
+++ 1913-440-2359-find-closest-node-to-given-two-nodes/1913-440-2359-find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,28 @@
+class Solution:    
+    def bfs(self, node1):
+        queue = deque()
+        queue.append((node1, 0))
+        visited = set()
+        visited.add(node1)
+        a = defaultdict(int)
+        while queue:
+            node, dist = queue.popleft()
+            a[node] = dist
+            for neighbor in self.g[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append((neighbor, dist + 1))
+        return a
+    def process(self, a, b):
+        v = [max(a[i], b[i]) for i in a if i in b]
+        if not v:
+            return -1
+        mv = min(v)
+        return min([i for i in a if i in b and max(a[i], b[i]) == mv])
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        self.g = defaultdict(list)
+        n = len(edges)
+        for i in range(n):
+            if edges[i] != -1:
+                self.g[i].append(edges[i])
+        return self.process(self.bfs(node1), self.bfs(node2))
