c23b03b95c9414299e0e6bc826276d463920693f
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-05-31 22:09:20 +0530

    Time: 35 ms, Memory: 18.1 MB - LeetHub

---
+++ 3373-1243-909-snakes-and-ladders/3373-1243-909-snakes-and-ladders.py
@@ -0,0 +1,27 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        self.n = len(board)
+        visited = set()
+        queue = deque()
+        queue.append((1, 0))
+        visited.add(1)
+        while queue:
+            node, steps = queue.popleft()
+            if node == self.n ** 2:
+                return steps
+            for i in range(node + 1, node + 7):
+                if i > self.n ** 2:
+                    break
+                x, y = self.get_coordinates(i)
+                if board[x][y] != -1:
+                    i = board[x][y]
+                if i not in visited:
+                    queue.append((i, steps + 1))
+                    visited.add(i)
+        return -1
+
+    def get_coordinates(self, i):
+        quot, rem = divmod(i - 1, self.n)
+        row = self.n - 1 - quot
+        col = rem if row % 2 != self.n % 2 else self.n - 1 - rem
+        return row, col       
