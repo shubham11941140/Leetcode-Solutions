94f6265fa7a1cc10b00532a9be4ceacbbc2649cf
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-06-05 17:57:20 +0530

    Time: 1 ms, Memory: 18 MB - LeetHub

---
+++ 2644-173-1061-lexicographically-smallest-equivalent-string/2644-173-1061-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    def dfs(self, g, node, v, cc):
+        for i in g[node]:
+            if i not in v:
+                v.add(i)
+                cc.append(i)
+                self.dfs(g, i, v, cc)
+    
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        g = collections.defaultdict(list)
+        n = len(s1)
+        for i in range(n):
+            if s1[i] != s2[i]:
+                g[s1[i]].append(s2[i])
+                g[s2[i]].append(s1[i])
+        v = set()
+        a = []
+        for i in g:
+            if i not in v:
+                v.add(i)
+                cc = [i]
+                self.dfs(g, i, v, cc)
+                a.append(cc)
+        sa = [sorted(i) for i in a]
+        for j in baseStr:
+            for i in sa:
+                if j in i:
+                    baseStr = baseStr.replace(j, i[0])
+        return baseStr
+                
