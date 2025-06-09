b9ca92375ccd60bfdab94c87b341fa168df94b49
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-06-09 21:58:01 +0530

    Time: 0 ms, Memory: 17.7 MB - LeetHub

---
+++ 1518-136-440-k-th-smallest-in-lexicographical-order/1518-136-440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        def count_steps(curr, n):
+            steps = 0
+            first = curr
+            last = curr
+            while first <= n:
+                steps += min(last, n) - first + 1
+                first *= 10
+                last = last * 10 + 9
+            return steps
+
+        curr = 1
+        k -= 1  # We start with the first number, so we need k-1 steps
+        while k > 0:
+            steps = count_steps(curr, n)
+            if steps <= k:
+                k -= steps
+                curr += 1
+            else:
+                curr *= 10
+                k -= 1
+        return curr        
