e5691e87a208a155aac726a431bca80b3e3479e1
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-06-17 00:19:38 +0530

    Time: 4 ms, Memory: 17.9 MB - LeetHub

---
+++ 1398-42-2016-maximum-difference-between-increasing-elements/1398-42-2016-maximum-difference-between-increasing-elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        min_val = nums[0]  # Track the smallest value seen so far
+        max_diff = -1  # Default to -1 if no valid pair is found
+        for num in nums[1:]:  # Start from the second element
+            if num > min_val:
+                max_diff = max(max_diff, num - min_val)
+            min_val = min(min_val, num)  # Update min_val
+        return max_diff     
