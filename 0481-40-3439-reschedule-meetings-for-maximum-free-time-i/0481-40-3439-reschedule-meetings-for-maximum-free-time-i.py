8cccf1c415ff515479d5f8c43a8878d41e05b718
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-07-10 00:16:36 +0530

    Time: 52 ms, Memory: 36.2 MB - LeetHub

--- 0481-40-3439-reschedule-meetings-for-maximum-free-time-i/0481-40-3439-reschedule-meetings-for-maximum-free-time-i.py
+++ 0481-40-3439-reschedule-meetings-for-maximum-free-time-i/0481-40-3439-reschedule-meetings-for-maximum-free-time-i.py
@@ -8,10 +8,8 @@ class Solution:
             prefixSum[i + 1] = prefixSum[i] + (endTime[i] - startTime[i])
 
         for i in range(k - 1, count):
-            occupied = prefixSum[i + 1] - prefixSum[i - k + 1]
             windowEnd = eventTime if i == count - 1 else startTime[i + 1]
             windowStart = 0 if i == k - 1 else endTime[i - k]
-            freeTime = windowEnd - windowStart - occupied
-            maxFree = max(maxFree, freeTime)
+            maxFree = max(maxFree, windowEnd - windowStart - (prefixSum[i + 1] - prefixSum[i - k + 1]))
 
         return maxFree        
