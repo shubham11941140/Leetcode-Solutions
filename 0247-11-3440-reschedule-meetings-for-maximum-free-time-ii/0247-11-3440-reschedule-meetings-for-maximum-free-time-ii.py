dc10b96125b21fc2a4744b1ebc8329445fdbc39e
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-07-10 16:25:00 +0530

    Time: 270 ms, Memory: 38.6 MB - LeetHub

--- 0247-11-3440-reschedule-meetings-for-maximum-free-time-ii/0247-11-3440-reschedule-meetings-for-maximum-free-time-ii.py
+++ 0247-11-3440-reschedule-meetings-for-maximum-free-time-ii/0247-11-3440-reschedule-meetings-for-maximum-free-time-ii.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
-        gap = [startTime[0]]
-        for i in range(1, len(startTime)):
-            gap.append(startTime[i] - endTime[i - 1])
-        gap.append(eventTime - endTime[-1])
-
+        gap = [startTime[0]] + [startTime[i] - endTime[i - 1] for i in range(1, len(startTime))] + [eventTime - endTime[-1]]
         largestRight = [0] * len(gap)
-        for i in range(len(gap) - 2, -1, -1):
+        for i in reversed(range(len(gap) - 1)):
             largestRight[i] = max(largestRight[i + 1], gap[i + 1])
 
         ans = 0
@@ -17,5 +13,4 @@ class Solution:
                 ans = max(ans, gap[i - 1] + gap[i] + curGap)
             ans = max(ans, gap[i - 1] + gap[i])
             largestLeft = max(largestLeft, gap[i - 1])
-
-        return ans        
+        return ans
