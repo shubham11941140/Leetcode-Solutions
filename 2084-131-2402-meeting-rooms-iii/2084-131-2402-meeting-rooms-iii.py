c10b236a5b47e8e9510141c58ad3246fff5dd1aa
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-07-12 01:14:12 +0530

    Time: 1473 ms, Memory: 52.5 MB - LeetHub

---
+++ 2084-131-2402-meeting-rooms-iii/2084-131-2402-meeting-rooms-iii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key=lambda x: x[0])
+        d = {i:[] for i in range(n)}
+        e = {i:0 for i in range(n)}
+        l = {i:0 for i in range(n)}
+        for i in meetings:
+            f = False
+            for j in d:
+                # Unused case
+                if not d[j]:
+                    d[j] = i
+                    l[j] += 1
+                    e[j] = i[1]
+                    f = True
+                    break
+                # If the meeting is not overlapping
+                if i[0] >= d[j][1] and e[j] <= i[0]:
+                    d[j] = i
+                    e[j] = i[1]
+                    l[j] += 1
+                    f = True
+                    break
+            if not f:
+                m = min(e.values())
+                for j in d:
+                    if e[j] == m:
+                        e[j] += (i[1] - i[0])
+                        d[j] = i
+                        l[j] += 1
+                        break
+        m = max(l.values())
+        for i in l:
+            if l[i] == m:
+                return i        
