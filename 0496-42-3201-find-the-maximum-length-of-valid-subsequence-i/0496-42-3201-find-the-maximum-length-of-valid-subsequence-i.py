ccd31a1bd35e98655863c460a0219382704fae52
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-07-16 22:58:57 +0530

    Time: 164 ms, Memory: 39.1 MB - LeetHub

---
+++ 0496-42-3201-find-the-maximum-length-of-valid-subsequence-i/0496-42-3201-find-the-maximum-length-of-valid-subsequence-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        cnt1 = sum(1 for x in nums if x % 2 == 0)
+        cnt2 = sum(1 for x in nums if x % 2 == 1)
+
+        eve = odd = 0
+        for x in nums:
+            if x % 2 == 0:
+                eve = max(eve, odd + 1)
+            else:
+                odd = max(odd, eve + 1)
+
+        return max(cnt1, cnt2, eve, odd)        
