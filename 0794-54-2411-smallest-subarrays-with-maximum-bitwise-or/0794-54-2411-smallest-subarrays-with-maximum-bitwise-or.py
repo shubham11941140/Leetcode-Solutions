e6a41f501a3f2198407a54d5cb781afa0aef1e9d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-07-29 16:13:43 +0530

    Time: 962 ms, Memory: 31 MB - LeetHub

--- 0794-54-2411-smallest-subarrays-with-maximum-bitwise-or/0794-54-2411-smallest-subarrays-with-maximum-bitwise-or.py
+++ 0794-54-2411-smallest-subarrays-with-maximum-bitwise-or/0794-54-2411-smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,4 +1,3 @@
 class Solution:
     def smallestSubarrays(self, nums: List[int]) -> List[int]:
-        return [*map(lambda d:max(d.values()) - d[-1] + 1, accumulate(reversed(range(len(nums))),
-            lambda d, i:d|{q:i for q in range(32) if nums[i] & 1 << q}|{-1:i}, initial = {-1:0}))][:0:-1]        
+        return [*map(lambda d:max(d.values()) - d[-1] + 1, accumulate(reversed(range(len(nums))), lambda d, i:d|{q:i for q in range(32) if nums[i] & 1 << q}|{-1:i}, initial = {-1:0}))][:0:-1]        
