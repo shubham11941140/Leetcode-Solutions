4d7939b1a6d23fc9bdfed6053844f6b461034ce9
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-02 20:40:10 +0530

    Time: 99 ms, Memory: 48 MB - LeetHub

--- 0710-44-2561-rearranging-fruits/0710-44-2561-rearranging-fruits.py
+++ 0710-44-2561-rearranging-fruits/0710-44-2561-rearranging-fruits.py
@@ -1,13 +1,10 @@
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
-        total_counts = Counter(basket1) + Counter(basket2)
-        
+        total_counts = Counter(basket1) + Counter(basket2)        
         for count in total_counts.values():
             if count % 2:
-                return -1
-        
+                return -1        
         count1 = Counter(basket1)
-        fruits_to_swap = sorted([fruit for fruit, t in total_counts.items() for _ in range(abs(count1.get(fruit, 0) - t // 2))])
-        
+        fr = sorted([fruit for fruit, t in total_counts.items() for _ in range(abs(count1.get(fruit, 0) - t // 2))])        
         min_val = min(total_counts.keys())
-        return sum([min(fruits_to_swap[i], 2 * min_val) for i in range(len(fruits_to_swap) // 2)])     
+        return sum([min(fr[i], 2 * min_val) for i in range(len(fr) // 2)])     
