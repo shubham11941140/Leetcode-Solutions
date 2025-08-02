b6ea4cf8ab107b5b09069abf8ba06d750e955ce2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-02 20:38:56 +0530

    Time: 94 ms, Memory: 47.3 MB - LeetHub

--- 0710-44-2561-rearranging-fruits/0710-44-2561-rearranging-fruits.py
+++ 0710-44-2561-rearranging-fruits/0710-44-2561-rearranging-fruits.py
@@ -6,12 +6,8 @@ class Solution:
             if count % 2:
                 return -1
         
-        fruits_to_swap = []
         count1 = Counter(basket1)
-        for fruit, total_count in total_counts.items():
-            for _ in range(abs(count1.get(fruit, 0) - total_count // 2)):
-                fruits_to_swap.append(fruit)
-
+        fruits_to_swap = [fruit for fruit, total_count in total_counts.items() for _ in range(abs(count1.get(fruit, 0) - total_count // 2))]
         fruits_to_swap.sort()
         
         min_val = min(total_counts.keys())
