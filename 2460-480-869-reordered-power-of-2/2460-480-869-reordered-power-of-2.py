ab988119fc21d359945f912f083fd769e1b35a74
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-11 00:48:59 +0530

    Time: 7 ms, Memory: 17.8 MB - LeetHub

---
+++ 2460-480-869-reordered-power-of-2/2460-480-869-reordered-power-of-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        for i in range(30):
+            if Counter(str(n)) == Counter(str(2 ** i)):
+                return True
+        return False
+        
