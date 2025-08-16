772cf3141e66d2258d753a1229914678edb73e34
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-17 01:25:36 +0530

    Time: 3 ms, Memory: 17.7 MB - LeetHub

--- 3208-235-1323-maximum-69-number/3208-235-1323-maximum-69-number.py
+++ 3208-235-1323-maximum-69-number/3208-235-1323-maximum-69-number.py
@@ -3,6 +3,10 @@ class Solution:
         s = list(str((num)))
         n = len(s)
         f = []
+        if '6' in s:
+            s[s.index('6')] = '9'
+            return int(''.join(s))
+        return num
         for i in range(n):
             if s[i] == '6':
                 t = s.copy()
