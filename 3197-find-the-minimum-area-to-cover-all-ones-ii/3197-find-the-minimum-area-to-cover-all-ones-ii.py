2dad77bcb930e556c6cd613ca71e078f5833111c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-23 17:25:47 +0530

    Time: 4612 ms (66.67%), Space: 18 MB (64.29%) - LeetHub

--- 3197-find-the-minimum-area-to-cover-all-ones-ii/3197-find-the-minimum-area-to-cover-all-ones-ii.py
+++ 3197-find-the-minimum-area-to-cover-all-ones-ii/3197-find-the-minimum-area-to-cover-all-ones-ii.py
@@ -5,35 +5,31 @@ class Solution:
         for _ in range(4):
             n, m = len(A), len(A[0])
             for i in range(1, n):
-                a1 = self.minimumArea(A[:i])
+                a1 = self.minar(A[:i])
                 for j in range(1, m):
                     part2 = [row[:j] for row in A[i:]]
                     part3 = [row[j:] for row in A[i:]]
-                    a2 = self.minimumArea(part2)
-                    a3 = self.minimumArea(part3)
+                    a2 = self.minar(part2)
+                    a3 = self.minar(part3)
                     res = min(res, a1 + a2 + a3)
                 for i2 in range(i + 1, n):
                     part2 = A[i:i2]
                     part3 = A[i2:]
-                    a2 = self.minimumArea(part2)
-                    a3 = self.minimumArea(part3)
+                    a2 = self.minar(part2)
+                    a3 = self.minar(part3)
                     res = min(res, a1 + a2 + a3)
             A = [[A[i][j] for i in reversed(range(len(A)))] for j in range(len(A[0]))] 
         return res
 
-    def minimumArea(self, A: List[List[int]]) -> int:
+    def minar(self, A: List[List[int]]) -> int:
         if not A or not A[0]:
             return 0
-        n, m = len(A), len(A[0])
         left, top, right, bottom = float("inf"), float("inf"), -1, -1
-        for i in range(n):
-            for j in range(m):
+        for i in range(len(A)):
+            for j in range(len(A[0])):
                 if A[i][j]:
                     left = min(left, j)
                     top = min(top, i)
                     right = max(right, j)
                     bottom = max(bottom, i)
-        return 0 if right == -1 else (right - left + 1) * (bottom - top + 1)
-
-    def rotate(self, A: List[List[int]]) -> List[List[int]]:
-        return        
+        return 0 if right == -1 else (right - left + 1) * (bottom - top + 1)  
