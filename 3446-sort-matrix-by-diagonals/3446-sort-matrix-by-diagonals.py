ba607bd01606cd5761633e182cdc3671dcb9c654
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-28 22:33:42 +0530

    Time: 15 ms (37.92%), Space: 17.9 MB (52.08%) - LeetHub

--- 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.py
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.py
@@ -1,12 +1,11 @@
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n, m = len(grid), len(grid[0])
-        diags = {}
+        diags = defaultdict(list)
         for i in range(n):
             for j in range(m):
                 key = i - j
-                if key not in diags: 
-                    diags[key] = []
+                #if key not in diags:                     diags[key] = []
                 heappush(diags[key], grid[i][j] if key < 0 else -grid[i][j])
         for i in range(n):
             for j in range(m):
