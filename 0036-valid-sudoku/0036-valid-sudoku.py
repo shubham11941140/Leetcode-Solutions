56944f41acc14da5f140e0f50ac11e899868d72d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-08-30 15:46:40 +0530

    Time: 17 ms (5.48%), Space: 17.7 MB (69.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -12,9 +12,9 @@ class Solution:
         return len([1 for i in board if self.check(i)]) == 9
     
     def grid(self, board, i, j):
-        return [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]      
+        return [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]   
+
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         c = [[i[j] for i in board] for j in range(9)]
         g = [self.grid(board, i, j) for i in [0, 3, 6] for j in [0, 3, 6]]
         return self.rows(board) and self.rows(c) and self.rows(g)
-    
