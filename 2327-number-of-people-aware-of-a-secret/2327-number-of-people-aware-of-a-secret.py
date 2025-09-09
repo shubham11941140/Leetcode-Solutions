e47e517e50e0c96fff7e8793cdcfab3ac5cead1b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-09-09 12:10:52 +0530

    Time: 3 ms (91.24%), Space: 18.2 MB (11.68%) - LeetHub

---
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        share = 0
+        dp = [0] * n
+        dp[0] = 1
+        for i in range(1, n):
+            if i - delay >= 0:
+                share += dp[i - delay]
+            if i - forget >= 0:
+                share -= dp[i - forget]
+            dp[i] = share
+        return sum([dp[i] for i in range(n - forget, n) if i >= 0]) % (10 ** 9 + 7)   
