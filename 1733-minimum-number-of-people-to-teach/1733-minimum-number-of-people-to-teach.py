2fc5a42799a30774bef3973ff3363dc95f6ec7d8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-09-10 11:57:20 +0530

    Time: 151 ms (24.39%), Space: 27.3 MB (87.81%) - LeetHub

---
+++ 1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        need = set()
+        for u, v in friendships:
+            u -= 1
+            v -= 1
+            ok = False
+            for x in languages[u]:
+                if x in languages[v]:
+                    ok = True
+                    break
+            if not ok:
+                need.add(u)
+                need.add(v)
+
+        ans = len(languages) + 1
+        for i in range(1, n + 1):
+            cans = 0
+            for v in need:
+                if i not in languages[v]:
+                    cans += 1
+            ans = min(ans, cans)
+        return ans        
