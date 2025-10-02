d9cb6d7f1a2303ad53b60fff09f4d0d4ed8cabf5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-10-02 15:18:08 +0530

    Time: 45 ms (19.44%), Space: 17.7 MB (43.33%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        bottleDrunk = numBottles
+        emptyBottles = numBottles
+        while emptyBottles >= numExchange:
+            emptyBottles -= numExchange
+            numExchange += 1
+            bottleDrunk += 1
+            emptyBottles += 1
+        return bottleDrunk        
