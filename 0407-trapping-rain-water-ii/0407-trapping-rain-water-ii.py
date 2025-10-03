8c861af7e15958107e6f7f3fb0ded0bf452dd833
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-10-03 23:35:52 +0530

    Time: 119 ms (84.75%), Space: 20.1 MB (81.75%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        if not heightMap or not heightMap[0]:
+            return 0
+        n, m = len(heightMap), len(heightMap[0])
+        visited = [[False] * m for _ in range(n)]
+        heap = []        
+        # Push all the boundary cells into the heap
+        for i in range(n):
+            for j in [0, m - 1]:
+                heappush(heap, (heightMap[i][j], i, j))                
+                visited[i][j] = True
+        for j in range(m):
+            for i in [0, n - 1]:
+                heappush(heap, (heightMap[i][j], i, j))
+                visited[i][j] = True
+        water_trapped = 0        
+        while heap:
+            height, x, y = heappop(heap)
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                    water_trapped += max(0, height - heightMap[nx][ny])
+                    heappush(heap, (max(height, heightMap[nx][ny]), nx, ny))
+                    visited[nx][ny] = True
+        return water_trapped
