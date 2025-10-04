3e28c3287d13e453a7619c0a0aa18acd38fa87c2
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-10-04 13:04:47 +0530

    Time: 114 ms (12.2%), Space: 28.6 MB (23.74%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,14 @@
+class Solution:           
+    def maxArea(self, height: List[int]) -> int:
+        # Find farthest greater element on left and right
+        l = 0
+        r = len(height) - 1
+        ans = -1
+        while l <= r:
+            area = (r - l) * min(height[l], height[r])
+            ans = max(ans, area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans        
