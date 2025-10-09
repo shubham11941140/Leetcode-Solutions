d2dfbf43859a38348496ecb1632546579f48c746
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-10-09 12:05:05 +0530

    Time: 9234 ms (69.23%), Space: 18.8 MB (39.42%) - LeetHub

--- 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
+++ 3494-find-the-minimum-amount-of-time-to-brew-potions/3494-find-the-minimum-amount-of-time-to-brew-potions.py
@@ -4,15 +4,10 @@ class Solution:
         p = [0] * n
         for i in range(1, n):
             p[i] = p[i - 1] + skills[i]
-
         total_time = skills[0] * energy[0]
-
         for j in range(1, m):
-            max_time = skills[0] * energy[j]
+            mt = skills[0] * energy[j]
             for i in range(1, n):
-                diff_time = p[i] * energy[j - 1] - p[i - 1] * energy[j]
-                if diff_time > max_time:
-                    max_time = diff_time
-            total_time += max_time
-
+                mt = max(mt, p[i] * energy[j - 1] - p[i - 1] * energy[j])
+            total_time += mt
         return total_time + p[-1] * energy[-1]        
