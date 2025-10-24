2887208bb90b5b560a3fd974374c2948355f8e55
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-10-24 14:13:49 +0530

    Time: 4116 ms (21.51%), Space: 17.7 MB (64.53%) - LeetHub

---
+++ 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        i = n + 1
+        while True:
+            c = Counter(str(i))
+            f = True
+            for j in c:
+                if int(j) != c[j]:
+                    f = False
+                    break
+            if f:
+                return i
+            i += 1
+
+        
