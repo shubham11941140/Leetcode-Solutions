4bbe12f4f5b97107fb782b6bd20607091004aed5
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-11-06 14:06:48 +0530

    Time: 327 ms (84.31%), Space: 101.4 MB (96.32%) - LeetHub

---
+++ 3607-power-grid-maintenance/3607-power-grid-maintenance.py
@@ -0,0 +1,42 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+
+    def find(self, x):
+        if self.parent[x] != x: self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py: return False
+        self.parent[py] = px
+        return True
+
+class Solution:
+    def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
+        dsu = DSU(c)
+        online = [True] * (c + 1)
+
+        for u, v in connections: dsu.union(u, v)
+
+        component_heap = defaultdict(list)
+        for station in range(1, c + 1):
+            root = dsu.find(station)
+            heapq.heappush(component_heap[root], station)
+
+        result = []
+
+        for typ, x in queries:
+            if typ == 2:
+                online[x] = False
+            else:
+                if online[x]:
+                    result.append(x)
+                else:
+                    root = dsu.find(x)
+                    heap = component_heap[root]
+                    while heap and not online[heap[0]]:
+                        heapq.heappop(heap)
+                    result.append(heap[0] if heap else -1)
+
+        return result        
