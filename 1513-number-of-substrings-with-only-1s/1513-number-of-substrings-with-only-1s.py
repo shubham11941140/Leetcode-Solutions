d5eafb8ce7583eca65d46e900f3b1e723d71639a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-11-16 17:30:05 +0530

    Time: 15 ms (66.67%), Space: 18.3 MB (40.82%) - LeetHub

---
+++ 1513-number-of-substrings-with-only-1s/1513-number-of-substrings-with-only-1s.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        ans = 0
+        count1 = 0
+        for ch in s:
+            if ch == '1':
+                count1 += 1
+                ans += count1
+            else:
+                count1 = 0        
+        return ans % (10 ** 9 + 7)       
