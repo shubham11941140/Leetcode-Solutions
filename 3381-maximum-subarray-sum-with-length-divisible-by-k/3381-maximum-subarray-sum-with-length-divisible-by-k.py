cc485d4ea01b871e52018a2b438dc1ffc310e7d0
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-11-27 14:23:33 +0530

    Time: 320 ms (72.41%), Space: 40.2 MB (86.9%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        INF = 10 ** 30
+        minPrefix = [INF] * k
+        minPrefix[0] = 0
+        prefix = 0
+        answer = -INF
+        for i, x in enumerate(nums):
+            prefix += x
+            mod = (i + 1) % k
+            answer = max(answer, prefix - minPrefix[mod])
+            minPrefix[mod] = min(minPrefix[mod], prefix)
+        return answer        
