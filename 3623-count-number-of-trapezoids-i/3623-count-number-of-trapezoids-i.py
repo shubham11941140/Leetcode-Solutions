3dc988a567eb496fb8a5a8af57eb2a7df2a51513
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-02 22:50:22 +0530

    Time: 82 ms (58.26%), Space: 65.9 MB (15.31%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        mod = int(10 ** 9 + 7)
+
+        freq_dict = Counter([p[1] for p in points])
+
+
+        
+        for y, freq in freq_dict.items():
+            freq_dict[y] = comb(freq, 2)
+        
+        ans = 0
+        ps = 0
+
+        for e in freq_dict.values():
+            ans = (ans + e * ps) % mod 
+            ps = (ps + e) % mod 
+        
+        return ans         
