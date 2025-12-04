e746042f523951e188315bf86f2dcb4c3d2396f8
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-04 12:40:28 +0530

    Time: 71 ms (75.09%), Space: 21.9 MB (5.95%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        n = len(directions)
+        l, r = 0, n - 1
+        while l < n and directions[l] == 'L': 
+            l += 1
+        while r >= l and directions[r] == 'R': 
+            r -= 1
+        return len([i for i in range(l, r + 1) if directions[i] != 'S'])      
