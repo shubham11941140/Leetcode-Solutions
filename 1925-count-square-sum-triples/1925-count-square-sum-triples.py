6d4194bf2453bd3e2d3149fcca25f7de3cf32dff
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-08 16:13:18 +0530

    Time: 167 ms (54.89%), Space: 17.9 MB (11.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -4,10 +4,8 @@ class Solution:
         # Iterate a from 1 to n
         for a in range(1, n):
             # Iterate b from a + 1 to n
-            for b in range(a + 1, n):
-                c = int((a * a + b * b) ** 0.5)
-                
+            for b in range(a + 1, n):                
                 # Check if c is a perfect square and within range
-                if c * c == (a * a + b * b) and c <= n:
+                if int((a * a + b * b) ** 0.5) ** 2 == (a * a + b * b) and int((a * a + b * b) ** 0.5) <= n:
                     cnt += 2 # Count both (a, b, c) and (b, a, c)
         return cnt
