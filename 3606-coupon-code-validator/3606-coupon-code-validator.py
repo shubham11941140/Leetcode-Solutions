1992f7e5ae6a1d6cbfde1fdcfce4a28462622b9c
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-14 01:44:47 +0530

    Time: 5 ms (78.73%), Space: 18.2 MB (13.97%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -0,0 +1,3 @@
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        return [c for c, _ in sorted(((c, b) for c, b, a in zip(code, businessLine, isActive) if a and c and bool(re.fullmatch(r"[A-Za-z0-9_]+", c)) and b in ["electronics", "grocery", "pharmacy", "restaurant"]), key = lambda x: (x[1], x[0]))]        
