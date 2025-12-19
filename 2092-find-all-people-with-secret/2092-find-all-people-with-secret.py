dae40dec9fd8a6e500135559113e57b01eff00ef
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-20 01:55:08 +0530

    Time: 693 ms (8.02%), Space: 71.6 MB (30.25%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,12 +1,10 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
         q = [(0, 0), (0, firstPerson)]
-
         g = collections.defaultdict(list)
         for p, p1, t in meetings:
             g[p].append((p1, t))
             g[p1].append((p, t))
-
         a = set()
         while q:
             t, p = heappop(q)
