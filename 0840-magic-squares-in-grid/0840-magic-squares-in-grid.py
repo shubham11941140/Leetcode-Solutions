2ccd8dcc5126f9189a3370c1430901aef2ff6283
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2025-12-30 22:28:38 +0530

    Time: 0 ms (100%), Space: 17.4 MB (90.91%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,6 +1,6 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        def isMagic(i, j):
+        def mg(i, j):
             s = set()
             for x in range(3):
                 for y in range(3):
@@ -9,13 +9,13 @@ class Solution:
                     s.add(grid[i + x][j + y])
             if len(s) != 9:
                 return False
-            return (grid[i][j] + grid[i][j + 1] + grid[i][j + 2] == 15 and
-                    grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] == 15 and
-                    grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] == 15 and
-                    grid[i][j] + grid[i + 1][j] + grid[i + 2][j] == 15 and
-                    grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] == 15 and
-                    grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2] == 15 and
-                    grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] == 15 and
-                    grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] == 15)
-        rows, cols = len(grid), len(grid[0])
-        return len([1 for i in range(rows - 2) for j in range(cols - 2) if isMagic(i, j)])
+            v1 = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] == 15
+            v2 = grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] == 15
+            v3 = grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] == 15
+            v4 = grid[i][j] + grid[i + 1][j] + grid[i + 2][j] == 15
+            v5 = grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] == 15
+            v6 = grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2] == 15
+            v7 = grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] == 15
+            v8 = grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] == 15
+            return (v1 and v2 and v3 and v4 and v5 and v6 and v7 and v8)
+        return len([1 for i in range(len(grid) - 2) for j in range(len(grid[0]) - 2) if mg(i, j)])
