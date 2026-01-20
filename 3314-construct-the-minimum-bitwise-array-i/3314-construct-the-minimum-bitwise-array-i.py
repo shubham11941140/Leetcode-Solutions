f04dd89d11e5f0f82705ad2d78676ff7a2749e62
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-01-20 09:35:49 +0530

    Time: 0 ms (100%), Space: 19.5 MB (15.44%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for n in nums:
+            ans.append(n - ((n + 1) & (-n - 1)) // 2 if n != 2 else -1)
+        return ans
