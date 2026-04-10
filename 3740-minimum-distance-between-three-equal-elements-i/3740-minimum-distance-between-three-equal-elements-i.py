d230a2b194163c6a413abf85e03eacad48340935
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-04-11 01:06:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -1,17 +1,14 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        def compute(a):
+        def comp(a):
             return abs(a[0] - a[1]) + abs(a[1] - a[2]) + abs(a[2] - a[0])
         d = defaultdict(list)
         for i in range(len(nums)):
             d[nums[i]].append(i)
-        print(d)
         if max([len(d[i]) for i in d]) <= 2:
             return -1
         ans = 10 ** 9
         for i in d:
-            di = d[i]
-            if len(di) > 2:
-                for j in range(len(di) - 2):
-                    ans = min(ans, compute(di[j:j + 3]))
+            if len(d[i]) > 2:
+                ans = min(ans, min([comp(d[i][j:j + 3]) for j in range(len(d[i]) - 2)]))
         return ans
