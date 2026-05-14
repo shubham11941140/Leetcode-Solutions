2a71974ff501879d1b036607f732d656a82d735d
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-05-14 11:39:53 +0530

    Time: 4 ms (11.03%), Space: 19.2 MB (59.43%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -4,12 +4,9 @@ class Solution:
         if len(nums) != m + 1:
             return False
         nums.sort()
-        print(nums)
         j = 1
         for i in range(m):
             if nums[i] != j:
                 return False
             j += 1
-        return nums[-1] == m
-
-        
+        return True      
