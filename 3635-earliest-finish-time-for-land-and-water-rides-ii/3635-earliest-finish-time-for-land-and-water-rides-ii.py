65edcea5ffcb8438629cd4b54ede70ed5628f09b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-06-03 18:35:23 +0530

    Time: 101 ms (92.73%), Space: 34.9 MB (50.91%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -1,16 +1,9 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        def calculatemin(start1,dur1,start2,dur2):
-            
+        def calc(start1, dur1, start2, dur2):            
             ans = min([(start1[r] + dur1[r]) for r in range(len(start1))])
-            ansfinal = float("inf")
+            return min([(max(ans, start2[r1]) + dur2[r1]) for r1 in range(len(start2))])
 
-            for r1 in range(len(start2)):
-                finish2=max(ans,start2[r1]) + dur2[r1]
-                ansfinal=min(ansfinal,finish2)
-            return ansfinal
-
-        firstlandsecondwater = calculatemin(landStartTime,landDuration,waterStartTime,waterDuration)
-        firstwatersecondland = calculatemin(waterStartTime,waterDuration,landStartTime,landDuration)
-
-        return min(firstlandsecondwater,firstwatersecondland)        
+        f1 = calc(landStartTime, landDuration, waterStartTime, waterDuration)
+        f2 = calc(waterStartTime, waterDuration, landStartTime, landDuration)
+        return min(f1, f2)
