4f85dd3cd90356366d6bc643456980339156594b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-06-11 20:45:35 +0530

    Time: 290 ms (82.8%), Space: 84.3 MB (44.09%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -1,17 +1,13 @@
 class Solution:
 
     def dfs(self, adj, node, par):
-        depth = 0
         a = [1 + self.dfs(adj, child, node) for child in adj[node] if child != par]
         return max(a) if a else 0
 
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
         n = len(edges) + 1
         adj = [[] for _ in range(n + 1)]
-
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-
-        depth = self.dfs(adj, 1, -1)
-        return pow(2, depth - 1, 10 ** 9 + 7)       
+        return pow(2, self.dfs(adj, 1, -1) - 1, 10 ** 9 + 7)       
