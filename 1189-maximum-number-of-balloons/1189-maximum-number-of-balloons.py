d64b654fc18e8b14b53594ec2601a515921a5b88
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-06-22 18:25:17 +0530

    Time: 1 ms (73.88%), Space: 19.4 MB (38.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         c = Counter(text)
-        for i in ['b', 'a', 'l', 'o', 'n']:
-            if i not in c:
-                return 0
+        print(c['b'])
+        #for i in ['b', 'a', 'l', 'o', 'n']:
+            #if i not in c:
+                #return 0
         return min([c['b'], c['a'], c['l'] // 2, c['o'] // 2, c['n']])
         
