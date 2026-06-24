702868a58a229b150ebc66bdfe78e1a036cec970
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-06-25 03:35:17 +0530

    Time: 14188 ms (5.48%), Space: 21.5 MB (23.29%) - LeetHub

---
+++ 3700-number-of-zigzag-arrays-ii/3700-number-of-zigzag-arrays-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def multiply(self, A, B):
+        sz = len(A)
+        C = [[0] * sz for _ in range(sz)]
+        for i in range(sz):
+            for k in range(sz):
+                if A[i][k] == 0:
+                    continue
+                for j in range(sz):
+                    C[i][j] += (A[i][k] * B[k][j]) % (10 ** 9 + 7)
+        return C
+
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        if n == 1:
+            return r - l + 1
+
+        k = r - l + 1
+        sz = 2 * k
+        M = [[0] * sz for _ in range(sz)]
+
+        for i in range(k):
+            for j in range(i):
+                M[i][k + j] = 1
+            for j in range(i + 1, k):
+                M[k + i][j] = 1
+
+        res = [[0] * sz for _ in range(sz)]
+        for i in range(sz):
+            res[i][i] = 1
+
+        p = n - 1
+        while p > 0:
+            if p % 2 == 1:
+                res = self.multiply(res, M)
+            M = self.multiply(M, M)
+            p //= 2
+
+        return sum(sum(i) for i in res) % (10 ** 9 + 7)      
