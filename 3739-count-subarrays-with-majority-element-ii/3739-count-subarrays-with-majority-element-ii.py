800dbee4e55e2c6956ad39a8e91a03c26a11cd50
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-06-26 16:37:27 +0530

    Time: 57 ms (96.4%), Space: 33.6 MB (37.84%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        pref = n
+        freq = [0] * (2 * n + 1)
+        freq[n] = 1
+        less = 0
+        ans = 0
+        for num in nums:
+            if num == target:
+                less += freq[pref]
+                pref += 1
+            else:
+                pref -= 1
+                less -= freq[pref]
+            freq[pref] += 1
+            ans += less
+        return ans        
