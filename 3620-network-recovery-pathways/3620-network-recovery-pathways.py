786f09100dc571cbb3e1b5a1e4fb5fa2e20abb72
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-07-03 15:51:57 +0530

    Time: 945 ms (48.86%), Space: 56.4 MB (96.59%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.py
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.py
@@ -8,28 +8,20 @@ class Solution:
             mxx = max(mxx, w)
 
         def check(target):
-
             heap = []
             heappush(heap, (0, 0))
             seen = defaultdict(int)
-
             while heap:
-
-                sm, node = heapq.heappop(heap)
-
+                sm, node = heappop(heap)
                 if node == len(online) - 1:
                     return True
-
-                if node in seen and seen[node] <= sm: continue
-    
+                if node in seen and seen[node] <= sm: 
+                    continue    
                 seen[node] = sm
-
                 for n, w in hs[node]:
-                    if sm + w > k: continue
-                    if not online[n]: continue
-                    if w < target: continue
-                    heapq.heappush(heap, (sm + w, n))
-
+                    if sm + w > k or not online[n] or w < target: 
+                        continue
+                    heappush(heap, (sm + w, n))
             return False
 
         l = -1
@@ -39,7 +31,5 @@ class Solution:
             if check(mid):
                 l = mid + 1
             else:
-                r = mid - 1
-        
-
+                r = mid - 1        
         return max(r, -1)
