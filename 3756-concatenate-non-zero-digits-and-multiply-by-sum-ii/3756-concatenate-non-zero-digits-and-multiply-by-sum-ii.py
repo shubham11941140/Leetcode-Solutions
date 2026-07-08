679ae1bc206a709ef16597ee6fa0493e0cb50765
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-07-08 19:54:20 +0530

    Time: 485 ms (62.96%), Space: 57.4 MB (37.04%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
+        n = len(s)
+        m = len(queries)
+        MOD = 10 ** 9 + 7
+        prefSum = [0] * (n + 1)
+        prefVal = [0] * (n + 1)
+        prefCnt = [0] * (n + 1)
+        power = [1] * (n + 1)
+        result = [0] * m
+
+        for i in range(1, n + 1):
+            power[i] = (power[i - 1] * 10) % MOD
+
+        for i in range(n):
+            d = int(s[i])
+            prefSum[i + 1] = prefSum[i] + d
+            prefCnt[i + 1] = prefCnt[i] + (1 if d != 0 else 0)
+            prefVal[i + 1] = (prefVal[i] * 10 + d) % MOD if d else prefVal[i] 
+
+        for i in range(m):
+            l, r = queries[i]
+            length = prefCnt[r + 1] - prefCnt[l]
+            x = (prefVal[r + 1] - (prefVal[l] * power[length]) % MOD + MOD) % MOD
+            result[i] = (x * (prefSum[r + 1] - prefSum[l])) % MOD
+        return result
