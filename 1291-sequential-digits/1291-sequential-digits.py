eb52641a0269e97358e5ade53ef51dcf7e1c6c2b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-07-13 19:57:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.59%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -3,4 +3,4 @@ class Solution:
         a = list(range(1, 10))
         b = [a[i:j] for i in range(9) for j in range(i, 10) if len(a[i:j]) > 1]        
         d = [int(i) for i in [''.join([str(k) for k in a]) for a in b]]
-        return sorted([i for i in d if low <= i <= high])                
+        return sorted([i for i in d if low <= i <= high])
