fe16fc5ddfdd847fc427dec31d6b6f0a87b4e933
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-07-13 19:56:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.59%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,16 +1,6 @@
-class Solution:
-
-    @staticmethod
-    def gen():
-        a = list(range(1, 10))
-        return [
-            a[i:j] for i in range(9) for j in range(i, 10) if len(a[i:j]) > 1
-        ]
-
-    @staticmethod
-    def process(b):
-        return [int(i) for i in ["".join([str(k) for k in a]) for a in b]]
-
+class Solution:                         
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        d = self.process(self.gen())
-        return sorted([d[i] for i in range(len(d)) if low <= d[i] <= high])
+        a = list(range(1, 10))
+        b = [a[i:j] for i in range(9) for j in range(i, 10) if len(a[i:j]) > 1]        
+        d = [int(i) for i in [''.join([str(k) for k in a]) for a in b]]
+        return sorted([i for i in d if low <= i <= high])                
