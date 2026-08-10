368db0311829bf5243e6eb0e6f52ac37583aea01
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-08-10 19:45:10 +0530

    Time: 450 ms (77.58%), Space: 20 MB (67.88%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,19 +1,13 @@
-from math import ceil, sqrt
-
-
-class Solution:
-
-    @staticmethod
-    def winnerSquareGame(n: int) -> bool:
+class Solution:            
+    def winnerSquareGame(self, n: int) -> bool:        
         a = [(i * i) for i in range(1, ceil(sqrt(n)) + 1)]
         k = len(a)
         dp = [False] * (n + 1)
         for i in range(n + 1):
             for j in range(k):
-                val = i - a[j]
-                if val < 0:
+                if i < a[j]:
                     break
-                if not dp[val]:
+                if not dp[i - a[j]]:
                     dp[i] = True
                     break
-        return dp[n]
+        return dp[n]        
