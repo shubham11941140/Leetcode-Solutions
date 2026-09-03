c6fc3bb4eff36049dc96b601c281228599334ab7
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-09-03 13:48:18 +0530

    Time: 53 ms (65.24%), Space: 35 MB (95.12%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,13 +1,3 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        n = len(nums1)
-        ce = len([i for i in range(n) if nums1[i] % 2 == 0])
-        co = n - ce
-        if ce == n or co == n:
-            return True
-        nums1.sort()
-        if nums1[0] % 2 == 0:
-            return False
-        return True
-
-        
+        return True if len([i for i in nums1 if i % 2]) in [0, len(nums1)] else bool(min(nums1) % 2)
