6f54fea6b7287be5fdb05909ed138eee704cc8a4
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-09-09 23:02:17 +0530

    Time: 3 ms (19.73%), Space: 19.5 MB (13.61%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,4 +1,5 @@
 class Solution:
     def countCommas(self, n: int) -> int:
+        return sum([max(n - int(str(999) * i), 0) for i in range(1, 6)])
         return max(n - 999, 0) + max(n - 999999, 0) + max(n - 999999999, 0) + max(n - 999999999999, 0) + max(n - 999999999999999, 0)
         
