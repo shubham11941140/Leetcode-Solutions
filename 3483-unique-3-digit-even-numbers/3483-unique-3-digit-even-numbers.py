34b0f6f4d6b95e06549ffac8430c5b52d323370b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-09-12 01:22:57 +0530

    Time: 145 ms (14.47%), Space: 19.2 MB (71.93%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,15 +1,14 @@
 class Solution:
-    def check(self, num, dig):
-        v = [int(i) for i in str(num)]
-        c = len(v)
+    def check(self, num, dig): 
+        c = 3
         digits = dig.copy()
-        for i in v:
-            if i in digits:
-                digits.remove(i)
+        for i in num:
+            if int(i) in digits:
+                digits.remove(int(i))
                 c -= 1
             else:
                 return False
-        return c == 0
+        return not c
 
     def totalNumbers(self, digits: List[int]) -> int:
-        return len([i for i in range(100, 1000, 2) if self.check(i, digits)])
+        return len([i for i in range(100, 1000, 2) if self.check(str(i), digits)])
