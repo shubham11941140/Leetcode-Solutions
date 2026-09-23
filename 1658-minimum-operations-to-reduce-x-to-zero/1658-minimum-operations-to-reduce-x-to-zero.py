b73feb9e13a969fd3c09a2ab28f2b33273d6bd5a
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2026-09-23 23:38:33 +0530

    Time: 137 ms (17.74%), Space: 43.7 MB (6.92%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,7 +1,6 @@
-class Solution:
-
-    @staticmethod
-    def long(arr, n, k):
+class Solution:  
+    
+    def long(self, arr, n, k):
         d = {}
         s = 0
         m = 0
@@ -13,17 +12,14 @@ class Solution:
                 m = max(m, i - d[s - k])
             if s not in d:
                 d[s] = i
-        return m
-
+        return m    
+            
     def minOperations(self, nums: List[int], x: int) -> int:
         s = sum(nums)
         if x > s:
             return -1
         n = len(nums)
         if x == s:
-            return n
-        val = s - x
-        l = self.long(nums, n, val)
-        if not l:
-            return -1
-        return n - l
+            return n        
+        l = self.long(nums, n, s - x)
+        return -1 if not l else n - l        
